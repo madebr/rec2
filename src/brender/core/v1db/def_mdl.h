09ae46ea1087385358b3f9b9a56da75c6c8b7a7b
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 00:36:22 +0200

    Add v1db default model

---
+++ src/brender/core/v1db/def_mdl.h
@@ -0,0 +1,10 @@
+#ifndef REC2_DEF_MDL_H
+#define REC2_DEF_MDL_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(br_model, _BrDefaultModel);
+
+#endif // REC2_DEF_MDL_H
