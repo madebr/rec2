d874f2824711c3dc3322f104abcd9d91bebb253c
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 00:59:01 +0200

    Hook ctype.h functions

--- src/carma2/hooks/c2_ctype.c
+++ src/carma2/hooks/c2_ctype.c
@@ -11,3 +11,23 @@ int C2_HOOK_CDECL c2_toupper(int character) {
     return toupper(character);
 }
 C2_HOOK_FUNCTION(0x005771a0, c2_toupper);
+
+int C2_HOOK_CDECL c2_isalpha(int c) {
+    return isalpha(c);
+}
+C2_HOOK_FUNCTION(0x005843e0, c2_isalpha)
+
+int C2_HOOK_CDECL c2_isdigit(int c) {
+    return isdigit(c);
+}
+C2_HOOK_FUNCTION(0x00584240, c2_isdigit)
+
+int C2_HOOK_CDECL c2_isspace(int c) {
+    return isspace(c);
+}
+C2_HOOK_FUNCTION(0x005842a0, c2_isspace)
+
+int C2_HOOK_CDECL c2_isprint(int c) {
+    return isprint(c);
+}
+C2_HOOK_FUNCTION(0x00584330, c2_isprint)
