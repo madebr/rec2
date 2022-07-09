28b0421c9eeaad89c952f8786031e678b27e4c33
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 00:55:33 +0200

    Patch malloc/calloc/free

--- src/carma2/hooks/c2_stdlib.h
+++ src/carma2/hooks/c2_stdlib.h
@@ -13,6 +13,24 @@ void C2_HOOK_CDECL c2_srand(int seed);
 
 void C2_HOOK_CDECL c2_abort(void);
 
+void* C2_HOOK_CDECL c2_malloc(size_t size);
+
+void* C2_HOOK_CDECL c2_calloc (size_t num, size_t size);
+
+void C2_HOOK_CDECL c2_free(void* ptr);
+
+char* C2_HOOK_CDECL c2_getenv(const char* name);
+
+float C2_HOOK_CDECL c2_strtof(const char* str, char** endptr);
+
+double C2_HOOK_CDECL c2_strtod(const char* str, char** endptr);
+
+long int C2_HOOK_CDECL c2_strtol(const char* str, char** endptr, int base);
+
+unsigned long int C2_HOOK_CDECL c2_strtoul(const char* str, char** endptr, int base);
+
+void C2_HOOK_CDECL c2_exit(int status);
+
 #else
 
 #include <stdlib.h>
@@ -20,6 +38,24 @@ void C2_HOOK_CDECL c2_abort(void);
 #define c2_srand srand
 
 #define c2_abort abort
+
+#define c2_malloc malloc
+
+#define c2_calloc calloc
+
+#define c2_free free
+
+#define c2_getenv getenv
+
+#define c2_strtof strtof
+
+#define c2_strtod strtod
+
+#define c2_strtol strtol
+
+#define c2_strtoul strtoul
+
+#define c2_exit exit
 
 #endif
 
