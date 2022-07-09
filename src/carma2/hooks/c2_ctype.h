6fe7da7a1059d5fe353da2a1fd2be585279409f7
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 00:59:01 +0200

    Hook ctype.h functions

--- src/carma2/hooks/c2_ctype.h
+++ src/carma2/hooks/c2_ctype.h
@@ -9,6 +9,14 @@ int C2_HOOK_CDECL c2_tolower(int character);
 
 int C2_HOOK_CDECL c2_toupper(int character);
 
+int C2_HOOK_CDECL c2_isalpha(int c);
+
+int C2_HOOK_CDECL c2_isdigit(int c);
+
+int C2_HOOK_CDECL c2_isspace(int c);
+
+int C2_HOOK_CDECL c2_isprint(int c);
+
 #else
 
 #include <ctype.h>
@@ -16,6 +24,14 @@ int C2_HOOK_CDECL c2_toupper(int character);
 #define c2_tolower tolower
 
 #define c2_toupper toupper
+
+#define c2_isalpha isalpha
+
+#define c2_isdigit isdigit
+
+#define c2_isspace isspace
+
+#define c2_isprint isprint
 
 #endif
 
