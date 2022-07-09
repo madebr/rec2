0358306e4d6460d28e35e2c0baf6a618a20766fa
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 00:33:55 +0200

    Add BrNamePatternMatch

---
+++ src/brender/core/fw/pattern.c
@@ -0,0 +1,47 @@
+#include "pattern.h"
+
+#include "c2_ctype.h"
+
+br_boolean C2_HOOK_CDECL BrNamePatternMatch(char* p, char* s) {
+    char* cp;
+
+    if (p == NULL) {
+        return 1;
+    }
+    if (s == NULL) {
+        return 0;
+    }
+
+    for (;; s++, p++) {
+
+        if (*p == '\0') {
+            // if pattern string ends at the same point as search string, we match
+            return *s == 0;
+        }
+
+        if (*p == '*') {
+            cp = s;
+            do {
+                if (BrNamePatternMatch(p + 1, cp)) {
+                    return 1;
+                }
+            } while (*cp++);
+            return 0;
+        }
+        if (*p == '/') {
+            return *s == 0;
+        }
+        if (*p == '?') {
+            if (*s == '\0') {
+                return 0;
+            }
+            continue;
+        }
+
+        if (c2_toupper(*p) != c2_toupper(*s)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+C2_HOOK_FUNCTION(0x00529860, BrNamePatternMatch)
---
+++ src/brender/core/fw/pattern.h
@@ -0,0 +1,10 @@
+#ifndef REC2_PATTERN_H
+#define REC2_PATTERN_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+br_boolean C2_HOOK_CDECL BrNamePatternMatch(char* p, char* s);
+
+#endif // REC2_PATTERN_H
