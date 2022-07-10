f63dc4bd2552e33e89712748a13ef1db2ae9b003
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 02:35:30 +0200

    Hook brbegin functions

---
+++ src/brender/core/fw/brbegin.h
@@ -0,0 +1,12 @@
+#ifndef REC2_BRBEGIN_H
+#define REC2_BRBEGIN_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+br_error C2_HOOK_CDECL BrBegin(void);
+
+br_error C2_HOOK_CDECL BrEnd(void);
+
+#endif // REC2_BRBEGIN_H
