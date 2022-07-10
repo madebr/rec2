c1f641b0262291a80164e2498716feb43057ecf3
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 04:12:03 +0200

    Hook def_mat function

---
+++ src/brender/core/v1db/def_mat.c
@@ -0,0 +1,31 @@
+#include "def_mat.h"
+
+#include "core/fw/resource.h"
+#include "core/math/matrix23.h"
+#include "core/v1db/dbsetup.h"
+
+br_material* (C2_HOOK_CDECL * SetupDefaultMaterial_original)(void);
+br_material* C2_HOOK_CDECL SetupDefaultMaterial(void) {
+#if defined(C2_HOOKS_ENABLED)
+    return SetupDefaultMaterial_original();
+#else
+    static br_material* default_mat;
+
+    default_mat = BrResAllocate(C2V(v1db).res, sizeof(br_material), BR_MEMORY_MATERIAL);
+    if (default_mat == NULL) {
+        return NULL;
+    }
+    default_mat->identifier = "default_mat";
+    default_mat->colour = BR_COLOUR_RGB(255, 255, 255);
+    default_mat->ka = 0.1f;
+    default_mat->kd = 0.7f;
+    default_mat->ks = 0.0f;
+    default_mat->power = 20;
+    default_mat->flags = BR_MATF_LIGHT;
+    BrMatrix23Identity(&default_mat->map_transform);
+    default_mat->index_base = 10;
+    default_mat->index_range = 31;
+    return default_mat;
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00526940, SetupDefaultMaterial, SetupDefaultMaterial_original)
---
+++ src/brender/core/v1db/def_mat.h
@@ -0,0 +1,10 @@
+#ifndef REC2_DEF_MAT_H
+#define REC2_DEF_MAT_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+br_material* C2_HOOK_CDECL SetupDefaultMaterial(void);
+
+#endif // REC2_DEF_MAT_H
