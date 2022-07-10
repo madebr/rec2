3922ec6897e82a3e2c8424e875ef4bf94cad9375
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 21:57:28 +0200

    Hook all diag functions

--- src/brender/core/fw/diag.c
+++ src/brender/core/fw/diag.c
@@ -29,3 +29,56 @@ void C2_HOOK_CDECL BrFailure(const char* s, ...) {
     C2_HOOK_FINISH();
 }
 C2_HOOK_FUNCTION(0x00527c80, BrFailure)
+
+void BrWarning(const char* s, ...) {
+    va_list args;
+    const char warning_header[10] = "Warning: ";
+
+    va_start(args, s);
+    BrStrCpy(C2V(_diag_scratch), warning_header);
+    BrVSprintf(&C2V(_diag_scratch)[sizeof(warning_header) - 1], s, args);
+
+    if (C2V(fw).diag->warning == NULL) {
+        BrAbort();
+    }
+
+    C2V(fw).diag->warning(C2V(_diag_scratch));
+    va_end(args);
+}
+C2_HOOK_FUNCTION(0x00527cd0, BrWarning)
+
+void BrFatal(const char* name, int line, const char* s, ...) {
+    va_list args;
+    int n;
+
+    va_start(args, s);
+    n = BrSprintf(C2V(_diag_scratch), "FATAL %s:%d\n", name, line);
+    BrVSprintf(&C2V(_diag_scratch)[n], s, args);
+    if (C2V(fw).diag->failure == NULL) {
+        BrAbort();
+    }
+
+    C2V(fw).diag->failure(C2V(_diag_scratch));
+    va_end(args);
+}
+C2_HOOK_FUNCTION(0x00527d20, BrFatal)
+
+void _BrAssert(const char* condition, const char* file, unsigned int line) {
+    if (C2V(fw).diag->failure == NULL) {
+        BrAbort();
+    }
+
+    BrSprintf(C2V(_diag_scratch), "ASSERTION FAILED %s:%d: \"%s\"\n", file, line, condition);
+    C2V(fw).diag->failure(C2V(_diag_scratch));
+}
+C2_HOOK_FUNCTION(0x00527d80, _BrAssert)
+
+void _BrUAssert(const char* condition, const char* file, unsigned int line) {
+    if (C2V(fw).diag->failure == NULL) {
+        BrAbort();
+    }
+
+    BrSprintf(C2V(_diag_scratch), "ASSERTION FAILED %s:%d: \"%s\"\n", file, line, condition);
+    C2V(fw).diag->failure(C2V(_diag_scratch));
+}
+C2_HOOK_FUNCTION(0x00527dd0, _BrUAssert)
