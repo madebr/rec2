596138790552713d499f5567865651dfc270a43f
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 21:57:28 +0200

    Hook all diag functions

--- src/brender/core/fw/diag.h
+++ src/brender/core/fw/diag.h
@@ -5,4 +5,12 @@
 
 void C2_HOOK_CDECL BrFailure (const char* format, ...);
 
-#endif //REC2_DIAG_H
+void C2_HOOK_CDECL BrWarning(const char* s, ...);
+
+void C2_HOOK_CDECL BrFatal(const char* name, int line, const char* s, ...);
+
+void C2_HOOK_CDECL _BrAssert(const char* condition, const char* file, unsigned int line);
+
+void C2_HOOK_CDECL _BrUAssert(const char* condition, const char* file, unsigned int line);
+
+#endif // REC2_DIAG_H
