17fa7bb91b934a28779a349242136d1d9fa6ddc8
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 02:04:56 +0200

    Hook a few more stdio functions

--- src/carma2/hooks/c2_stdio.h
+++ src/carma2/hooks/c2_stdio.h
@@ -17,6 +17,7 @@ C2_HOOK_VARIABLE_DECLARE(FILE, c2_stderr_value);
 
 FILE* C2_HOOK_CDECL c2_fopen(const char* path, const char* mode);
 void C2_HOOK_CDECL c2_fclose(FILE* file);
+int C2_HOOK_CDECL c2_feof(FILE* file);
 
 int C2_HOOK_CDECL c2_fflush(FILE* file);
 
@@ -24,24 +25,55 @@ int C2_HOOK_CDECL c2_ftell(FILE* file);
 int C2_HOOK_CDECL c2_fseek(FILE *stream, int offset, int whence);
 void C2_HOOK_CDECL c2_rewind(FILE *stream);
 
+int C2_HOOK_CDECL c2_fgetc(FILE * stream);
+char* C2_HOOK_CDECL c2_fgets(char* str, int num, FILE* stream);
 int C2_HOOK_CDECL c2_fputc(int character, FILE* file);
 int C2_HOOK_CDECL c2_fputs(const char* text, FILE* file);
 
 int C2_HOOK_CDECL c2_printf(const char* format, ...);
 int C2_HOOK_CDECL c2_fprintf(FILE* file, const char* format, ...);
-int C2_HOOK_CDECL c2_sprintf(char* str, const char* format, ...);
 int C2_HOOK_CDECL c2_vfprintf(FILE* file, const char* format, va_list ap);
+
+int C2_HOOK_CDECL c2_sprintf(char* str, const char* format, ...);
+int C2_HOOK_CDECL c2_vsprintf(char* str, const char* format, va_list ap);
 
 size_t C2_HOOK_CDECL c2_fwrite(const void* ptr, size_t size, size_t count, FILE* stream);
 size_t C2_HOOK_CDECL c2_fread(void *ptr, size_t size, size_t nmemb, FILE *stream);
 
 int C2_HOOK_CDECL c2_sscanf(const char* str, const char* format, ...);
+int C2_HOOK_CDECL c2_vsscanf(const char* str, const char* format, va_list ap);
 
 #else
 
 #include <stdio.h>
 
+#define c2_fopen fopen
+#define c2_fclose fclose
+#define c2_feof feof
+
+#define c2_fflush fflush
+
+#define c2_ftell ftell
+#define c2_fseek fseek
+#define c2_rewind rewind
+
+#define c2_fgetc fgetc
+#define c2_fgets fgets
+#define c2_fputc fpuc
+#define c2_fputs fputs
+
+#define c2_printf printf
+#define c2_fprintf fprintf
+#define c2_vfprintf vfprintf
+
 #define c2_sprintf sprintf
+#define c2_vsprintf vsprintf
+
+#define c2_fwrite fwrite
+#define c2_fread fread
+
+#define c2_sscanf sscanf
+#define c2_vsscanf vsscanf
 
 #endif
 
