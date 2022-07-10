b116d343d04e1a5637ca6c9e27074302a2132852
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 16:17:19 +0200

    Hook plane functions

---
+++ src/brender/core/math/plane.h
@@ -0,0 +1,10 @@
+#ifndef REC2_PLANE_H
+#define REC2_PLANE_H
+
+#include "C2_hooks.h"
+
+#include "brender/br_types.h"
+
+br_int_32 C2_HOOK_CDECL BrPlaneEquation(br_vector4* eqn, br_vector3* v0, br_vector3* v1, br_vector3* v2);
+
+#endif // REC2_PLANE_H
