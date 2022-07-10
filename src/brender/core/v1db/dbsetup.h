d1408108d415eb5b934f1c7ff893bf1393b70238
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-10 14:22:24 +0200

    Hook dbsetup functions

--- src/brender/core/v1db/dbsetup.h
+++ src/brender/core/v1db/dbsetup.h
@@ -1,48 +1,49 @@
 #ifndef REC2_DBSETUP_H
-#define ReC2_DBSETUP_H
+#define REC2_DBSETUP_H
 
 #include "c2_hooks.h"
 
-//#include "brender/br_types.h"
-//
-//extern br_v1db_state v1db;
-//
-//br_error BrV1dbBegin();
-//
-//br_error BrV1dbEnd();
-//
-//br_uint_32 updateTable(br_pixelmap* item, void* arg);
-//
-//br_uint_32 updateMap(br_pixelmap* item, void* arg);
-//
-//br_uint_32 updateMaterial(br_material* item, void* arg);
-//
-//br_uint_32 updateModel(br_model* item, void* arg);
-//
-//br_uint_32 clearTable(br_pixelmap* item, void* arg);
-//
-//br_uint_32 clearMap(br_pixelmap* item, void* arg);
-//
-//br_uint_32 clearMaterial(br_material* item, void* arg);
-//
-//br_uint_32 clearModel(br_model* item, void* arg);
-//
-//br_error BrV1dbRendererBegin(br_device_pixelmap* destination, br_renderer* renderer);
-//
-//br_renderer* BrV1dbRendererQuery();
-//
-//br_error BrV1dbRendererEnd();
-//
-//void BrZbBegin(br_uint_8 colour_type, br_uint_8 depth_type);
-//
-//void BrZsBegin(br_uint_8 colour_type, void* primitive, br_uint_32 size);
-//
-//void BrZbEnd();
-//
-//void BrZsEnd();
-//
+#include "brender/br_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(br_v1db_state, v1db);
+
+br_error C2_HOOK_CDECL BrV1dbBegin(void);
+
+br_error C2_HOOK_CDECL BrV1dbEnd(void);
+
+br_uint_32 C2_HOOK_CDECL updateTable(br_pixelmap* item, void* arg);
+
+br_uint_32 C2_HOOK_CDECL updateMap(br_pixelmap* item, void* arg);
+
+br_uint_32 C2_HOOK_CDECL updateMaterial(br_material* item, void* arg);
+
+br_uint_32 C2_HOOK_CDECL updateModel(br_model* item, void* arg);
+
+br_uint_32 C2_HOOK_CDECL clearTable(br_pixelmap* item, void* arg);
+
+br_uint_32 C2_HOOK_CDECL clearMap(br_pixelmap* item, void* arg);
+
+br_uint_32 C2_HOOK_CDECL clearMaterial(br_material* item, void* arg);
+
+br_uint_32 C2_HOOK_CDECL clearModel(br_model* item, void* arg);
+
+br_error C2_HOOK_CDECL BrV1dbRendererBegin(br_device_pixelmap* destination, br_renderer* renderer);
+
+br_renderer* C2_HOOK_CDECL BrV1dbRendererQuery(void);
+
+br_error C2_HOOK_CDECL BrV1dbRendererEnd(void);
+
+void C2_HOOK_CDECL BrZbBegin(br_uint_8 colour_type, br_uint_8 depth_type);
+
+void C2_HOOK_CDECL BrZsBegin(br_uint_8 colour_type, void* primitive, br_uint_32 size);
+
+void C2_HOOK_CDECL BrZbEnd(void);
+
+void C2_HOOK_CDECL BrZsEnd(void);
+
 void C2_HOOK_CDECL BrV1dbBeginWrapper_Float(void);
-//
-//void BrV1dbEndWrapper();
+
+void C2_HOOK_CDECL BrV1dbEndWrapper_Float
+(void);
 
 #endif // REC2_DBSETUP_H
