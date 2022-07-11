fbbd2d2929bc09debb53d830c6af0a8254a7a5c4
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-11 14:36:41 +0200

    Add hooks for def_otab variable

---
+++ src/brender/core/v1db/def_otab.h
@@ -0,0 +1,10 @@
+#ifndef REC2_DEF_OTAB_H
+#define REC2_DEF_OTAB_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(br_order_table, _BrDefaultOrderTable);
+
+#endif // REC2_DEF_OTAB_H
