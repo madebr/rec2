5c98cbd5e6c2c0b8316f01b479b8859920e995b5
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-12 22:30:10 +0200

    Add hooks for preptab functions

---
+++ src/brender/core/v1db/preptab.h
@@ -0,0 +1,10 @@
+#ifndef REC2_PREPTAB_H
+#define REC2_PREPTAB_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+void C2_HOOK_CDECL BrTableUpdate(br_pixelmap* table, br_uint_16 flags);
+
+#endif // REC2_PREPTAB_H
