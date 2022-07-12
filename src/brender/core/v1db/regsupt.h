acf120cd63f337b3dc3bd43948978bb92500eb3d
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-12 22:52:29 +0200

    Add hooks for BrMaterial* functions in regsupt

--- src/brender/core/v1db/regsupt.h
+++ src/brender/core/v1db/regsupt.h
@@ -3,8 +3,24 @@
 
 #include "c2_hooks.h"
 
-#include <brender/br_types.h>
+#include "brender/br_types.h"
+
+br_material* C2_HOOK_CDECL BrMaterialAdd(br_material* material);
+
+br_material* C2_HOOK_CDECL BrMaterialRemove(br_material* material);
 
 br_material* C2_HOOK_CDECL BrMaterialFind(char* pattern);
 
-#endif //REC2_REGSUPT_H
+br_material_find_cbfn* C2_HOOK_CDECL BrMaterialFindHook(br_material_find_cbfn* hook);
+
+br_uint_32 C2_HOOK_CDECL BrMaterialAddMany(br_material** items, int n);
+
+br_uint_32 C2_HOOK_CDECL BrMaterialRemoveMany(br_material** items, int n);
+
+br_uint_32 C2_HOOK_CDECL BrMaterialFindMany(char* pattern, br_material** items, int max);
+
+br_uint_32 C2_HOOK_CDECL BrMaterialCount(char* pattern);
+
+br_uint_32 C2_HOOK_CDECL BrMaterialEnum(char* pattern, br_material_enum_cbfn* callback, void* arg);
+
+#endif // REC2_REGSUPT_H
