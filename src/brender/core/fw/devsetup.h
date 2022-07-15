47bc2ca002d0150772d1a9893ab2b546f95f4d68
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-16 00:33:33 +0200

    Hook more functions in devsetup

--- src/brender/core/fw/devsetup.h
+++ src/brender/core/fw/devsetup.h
@@ -5,7 +5,14 @@
 
 #include "c2_hooks.h"
 
+br_pixelmap* C2_HOOK_CDECL BrDevLastBeginQuery(void);
+
+void C2_HOOK_CDECL BrDevLastBeginSet(br_pixelmap* pm);
+
 br_error C2_HOOK_CDECL BrDevBeginVar(br_pixelmap** ppm, char* setup_string, ...);
+
 br_error C2_HOOK_CDECL BrDevBeginTV(br_pixelmap** ppm, char* setup_string, br_token_value* setup_tv);
+
+br_error C2_HOOK_CDECL BrDevBegin(br_pixelmap** ppm, char* setup_string);
 
 #endif //REC2_DEVSETUP_H
