4fd4d01425d778e86290d8302ec75d788fe70e94
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-16 17:48:50 +0200

    Add pixelmap fontptrs

---
+++ src/brender/core/pixelmap/fontptrs.h
@@ -0,0 +1,12 @@
+#ifndef REC2_FONTPTRS_H
+#define REC2_FONTPTRS_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(br_font*, BrFontProp4x6);
+C2_HOOK_VARIABLE_DECLARE(br_font*, BrFontProp7x9);
+C2_HOOK_VARIABLE_DECLARE(br_font*, BrFontFixed3x5);
+
+#endif // REC2_FONTPTRS_H
