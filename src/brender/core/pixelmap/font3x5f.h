b7733c285a7861c2e10b940915657dff4f5cf2f1
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-16 17:45:09 +0200

    Add 3x5 fixed font

---
+++ src/brender/core/pixelmap/font3x5f.h
@@ -0,0 +1,10 @@
+#ifndef REC2_FONT3X5F_H
+#define REC2_FONT3X5F_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(br_font, br_font3x5f);
+
+#endif // REC2_FONT3X5F_H
