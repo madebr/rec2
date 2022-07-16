e4517122e75acd941062d2c658d7cfb4929cd418
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-16 19:40:20 +0200

    Hook DRPixelmapAllocate

--- src/carma2/common/utility.c
+++ src/carma2/common/utility.c
@@ -3,9 +3,10 @@
 #include "globvars.h"
 #include "platform.h"
 
-#include "c2_ctype.h"
+#include "brender/brender.h"
 
-#include <string.h>
+#include "c2_ctype.h"
+#include "c2_string.h"
 
 static br_error (C2_HOOK_FASTCALL * RemoveAllBrenderDevices_original)(void);
 br_error C2_HOOK_FASTCALL RemoveAllBrenderDevices(void) {
@@ -35,14 +36,14 @@ char* C2_HOOK_FASTCALL GetALineAndDontArgue(tTWTFILE* pF, char* pS) {
 }
 C2_HOOK_FUNCTION(0x00491090, GetALineAndDontArgue)
 
-void C2_HOOK_FASTCALL PathCat(char* pDestn_str, char* pStr_1, char* pStr_2) {
+void C2_HOOK_FASTCALL PathCat(char* pDestn_str, char* pStr_1, const char* pStr_2) {
 
     if (pDestn_str != pStr_1) { // Added to avoid strcpy overlap checks
-        strcpy(pDestn_str, pStr_1);
+        c2_strcpy(pDestn_str, pStr_1);
     }
-    if (strlen(pStr_2) != 0) {
-        strcat(pDestn_str, C2V(gDir_separator));
-        strcat(pDestn_str, pStr_2);
+    if (c2_strlen(pStr_2) != 0) {
+        c2_strcat(pDestn_str, C2V(gDir_separator));
+        c2_strcat(pDestn_str, pStr_2);
     }
 }
 C2_HOOK_FUNCTION(0x00513690, PathCat)
@@ -70,3 +71,15 @@ void C2_HOOK_FASTCALL PossibleService(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x005155d0, PossibleService, PossibleService_original)
+
+br_pixelmap* C2_HOOK_FASTCALL DRPixelmapAllocate(br_uint_8 pType, br_uint_16 pW, br_uint_16 pH, void* pPixels, int pFlags) {
+    br_pixelmap* pm;
+
+    pm = BrPixelmapAllocate(pType, pW, pH, pPixels, pFlags);
+    if (pm != NULL) {
+        pm->origin_x = 0;
+        pm->origin_y = 0;
+    }
+    return pm;
+}
+C2_HOOK_FUNCTION(0x005137d0, DRPixelmapAllocate)
