d407eb803f42569e10b7687fe49ab04d67de6b16
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-16 17:45:49 +0200

    Add 4x6 proportional font

---
+++ src/brender/core/pixelmap/font4x6p.h
@@ -0,0 +1,10 @@
+#ifndef REC2_FONT4x6P_H
+#define REC2_FONT4x6P_H
+
+#include "c2_hooks.h"
+
+#include "brender/br_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(br_font, br_font4x6p);
+
+#endif // REC2_FONT4x6P_H
