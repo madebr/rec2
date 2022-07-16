9dd56beb7117a575c108cda435a058b43886f8ef
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-16 19:43:59 +0200

    Hook AnyKeyDown

--- src/carma2/common/input.c
+++ src/carma2/common/input.c
@@ -12,3 +12,16 @@ int C2_HOOK_FASTCALL LoadJoystickPreferences() {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0045c0b0, LoadJoystickPreferences, LoadJoystickPreferences_original)
+
+int (C2_HOOK_FASTCALL * AnyKeyDown_original)(void);
+int C2_HOOK_FASTCALL AnyKeyDown(void) {
+#if defined(C2_HOOKS_ENABLED)
+    C2_HOOK_START();
+    int res = AnyKeyDown_original();
+    C2_HOOK_FINISH();
+    return res;
+#else
+#error "not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00482d70, AnyKeyDown, AnyKeyDown_original)
