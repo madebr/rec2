b3b5f91018d0763bd83db5bc7b322a5694047348
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-17 18:19:18 +0200

    Sprinkle 'const' around brender math functions

--- src/brender/core/math/quat.h
+++ src/brender/core/math/quat.h
@@ -5,20 +5,20 @@
 
 #include "brender/br_types.h"
 
-br_quat* C2_HOOK_CDECL BrQuatMul(br_quat* q, br_quat* l, br_quat* r);
+br_quat* C2_HOOK_CDECL BrQuatMul(br_quat* q, const br_quat* l, const br_quat* r);
 
-br_quat* C2_HOOK_CDECL BrQuatNormalise(br_quat* q, br_quat* qq);
+br_quat* C2_HOOK_CDECL BrQuatNormalise(br_quat* q, const br_quat* qq);
 
-br_quat* C2_HOOK_CDECL BrQuatInvert(br_quat* q, br_quat* qq);
+br_quat* C2_HOOK_CDECL BrQuatInvert(br_quat* q, const br_quat* qq);
 
-br_quat* C2_HOOK_CDECL BrQuatSlerp(br_quat* q, br_quat* l, br_quat* r, br_scalar a, br_int_16 spins);
+br_quat* C2_HOOK_CDECL BrQuatSlerp(br_quat* q, const br_quat* l, const br_quat* r, br_scalar a, br_int_16 spins);
 
-br_matrix34* C2_HOOK_CDECL BrQuatToMatrix34(br_matrix34* mat, br_quat* q);
+br_matrix34* C2_HOOK_CDECL BrQuatToMatrix34(br_matrix34* mat, const br_quat* q);
 
-br_quat* C2_HOOK_CDECL BrMatrix34ToQuat(br_quat* q, br_matrix34* mat);
+br_quat* C2_HOOK_CDECL BrMatrix34ToQuat(br_quat* q, const br_matrix34* mat);
 
-br_matrix4* C2_HOOK_CDECL BrQuatToMatrix4(br_matrix4* mat, br_quat* q);
+br_matrix4* C2_HOOK_CDECL BrQuatToMatrix4(br_matrix4* mat, const br_quat* q);
 
-br_quat* C2_HOOK_CDECL BrMatrix4ToQuat(br_quat* q, br_matrix4* mat);
+br_quat* C2_HOOK_CDECL BrMatrix4ToQuat(br_quat* q, const br_matrix4* mat);
 
 #endif // REC2_QUAT_H
