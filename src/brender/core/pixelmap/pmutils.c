60e4c4529386f67bb89e93bcca92f6db750e36f5
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-07-19 02:30:21 +0200

    Make functions parsable

--- src/brender/core/pixelmap/pmutils.c
+++ src/brender/core/pixelmap/pmutils.c
@@ -1,7 +1,10 @@
 #include "pmutils.h"
 
 #if defined(__I86__) || defined(_M_IX86)
-C2_NAKED br_uint_16 C2_HOOK_CDECL GetSysQual(void) {
+C2_NAKED
+#endif
+br_uint_16 C2_HOOK_CDECL GetSysQual(void) {
+#if defined(__I86__) || defined(_M_IX86)
 #if defined(_MSC_VER)
     __asm {
         mov ax, ds;
@@ -13,10 +16,8 @@ C2_NAKED br_uint_16 C2_HOOK_CDECL GetSysQual(void) {
         "ret\n"
     );
 #endif
-}
 #else
-br_uint_16 C2_HOOK_CDECL GetSysQual(void) {
     return 0;
-}
 #endif
+}
 C2_NO_HOOK_FUNCTION(0x0053e682, GetSysQual)
