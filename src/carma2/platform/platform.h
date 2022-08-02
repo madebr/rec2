3aeb2c0dd8657e63aa42fa9cf523f9e803f6e309
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-03 01:33:47 +0200

    Add function and variable declarations to platform.h header

--- src/carma2/platform/platform.h
+++ src/carma2/platform/platform.h
@@ -6,11 +6,15 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(int, gGraf_spec_index);
+C2_HOOK_VARIABLE_DECLARE(int, gNbPixelBits);
 C2_HOOK_VARIABLE_DECLARE_ARRAY(tGraf_spec, gGraf_specs, 2);
+C2_HOOK_VARIABLE_DECLARE(br_pixelmap*, gReal_back_screen);
 
+void C2_HOOK_FASTCALL PDServiceInput(void);
 void C2_HOOK_FASTCALL KeyBegin(void);
 
 void C2_HOOK_FASTCALL PDBuildAppPath(char* pThe_path);
+void C2_HOOK_FASTCALL PDEnterDebugger(const char* pStr);
 void C2_HOOK_FASTCALL PDFatalError(char* pThe_str);
 void C2_HOOK_FASTCALL PDSetFileVariables(void);
 C2_NORETURN void C2_HOOK_FASTCALL PDShutdownSystem(void);
@@ -21,5 +25,13 @@ int C2_HOOK_FASTCALL PDCheckDriveExists2(const char* pThe_path, const char* pFil
 int C2_HOOK_FASTCALL PDReadSourceLocation(tPath_name pPath);
 
 int C2_HOOK_FASTCALL PDDoWeLeadAnAustereExistance(void);
+
+int C2_HOOK_FASTCALL PDGetTotalTime(void);
+
+void C2_HOOK_FASTCALL SwapBackScreen(void);
+
+void C2_HOOK_FASTCALL PDSetPaletteEntries(br_pixelmap* pPalette, int pFirst_colour, int pCount);
+
+void C2_HOOK_FASTCALL PDSetPalette(br_pixelmap *pixelmap);
 
 #endif // C2_PLATFORM_H
