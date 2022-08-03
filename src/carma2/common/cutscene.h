d152593f6156e04137ceef4be890da761641d6b7
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-03 18:53:03 +0200

    Implement DoSCILogo and DoStainlessLogo

--- src/carma2/common/cutscene.h
+++ src/carma2/common/cutscene.h
@@ -3,34 +3,24 @@
 
 #include "c2_hooks.h"
 
-//extern tS32 gLast_demo_end_anim;
-//
-//void ShowCutScene(int pIndex, int pWait_end, int pSound_ID, br_scalar pDelay);
-//
-//void DoSCILogo();
-//
-//void DoStainlessLogo();
-//
+void C2_HOOK_FASTCALL DoSCILogo(void);
+
+void C2_HOOK_FASTCALL DoStainlessLogo(void);
+
 void C2_HOOK_FASTCALL PlaySmackerFile(const char* pSmack_name);
-//
-//void DoOpeningAnimation();
-//
-//void DoNewGameAnimation();
-//
-//void DoGoToRaceAnimation();
-//
-//void DoEndRaceAnimation();
-//
-//void DoGameOverAnimation();
-//
-//void DoGameCompletedAnimation();
-//
-//void DoFeatureUnavailableInDemo();
-//
-//void DoFullVersionPowerpoint();
-//
-//void DoDemoGoodbye();
-//
-//void StartLoadingScreen();
+
+void C2_HOOK_FASTCALL DoOpeningAnimation(void);
+
+void C2_HOOK_FASTCALL DoNewGameAnimation(void);
+
+void C2_HOOK_FASTCALL DoGoToRaceAnimation(void);
+
+void C2_HOOK_FASTCALL DoEndRaceAnimation(void);
+
+void C2_HOOK_FASTCALL DoGameOverAnimation(void);
+
+void C2_HOOK_FASTCALL DoGameCompletedAnimation(void);
+
+void C2_HOOK_FASTCALL StartLoadingScreen(void);
 
 #endif // REC2_CUTSCENE_H
