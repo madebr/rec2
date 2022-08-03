0299579c1db2bc0c9162d9c4352964e2965c89f6
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-03 22:42:43 +0200

    Hook TintedPolyActor_SetColour

--- src/carma2/common/graphics.h
+++ src/carma2/common/graphics.h
@@ -18,6 +18,8 @@ void C2_HOOK_FASTCALL SplashScreenWith(const char* pPixmap_name);
 
 void C2_HOOK_FASTCALL DRConvertPixelmapRGB565To555(br_pixelmap* pixmap, int pixelType);
 
+void C2_HOOK_FASTCALL TintedPolyActor_SetColour(int index, int red, int green, int blue);
+
 int C2_HOOK_FASTCALL CreateTintedPolyActor(int x, int y, int width, int height, int type, int arg1, int arg2, int arg3);
 
 void C2_HOOK_FASTCALL AllocateCursorActors(void);
