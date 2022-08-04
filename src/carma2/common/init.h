2b835abf4fc70164669e27da0b44aef4a92ba7aa
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-04 21:50:44 +0200

    Implement GetScreenSize/SetScreenSize

--- src/carma2/common/init.h
+++ src/carma2/common/init.h
@@ -7,4 +7,8 @@ C2_HOOK_VARIABLE_DECLARE(int, gBr_initialized);
 
 void C2_HOOK_FASTCALL InitialiseApplication(int pArgc, const char** pArgv);
 
+int C2_HOOK_FASTCALL GetScreenSize(void);
+
+void C2_HOOK_FASTCALL SetScreenSize(int pNew_size);
+
 #endif //REC2_INIT_H
