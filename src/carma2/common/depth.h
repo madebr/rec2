d36d66db146e0c1ac32f04be5e118996022dd68b
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-04 17:44:37 +0200

    Implement GetSkYTextureOn + Hook SetSkyTextureOn

---
+++ src/carma2/common/depth.h
@@ -0,0 +1,10 @@
+#ifndef REC2_DEPTH_H
+#define REC2_DEPTH_H
+
+#include "c2_hooks.h"
+
+int C2_HOOK_FASTCALL GetSkyTextureOn(void);
+
+void C2_HOOK_FASTCALL SetSkyTextureOn(int skyTextureOn);
+
+#endif //REC2_DEPTH_H
