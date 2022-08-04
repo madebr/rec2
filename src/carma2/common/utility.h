d10e0a450573ec5233d69a7bc7c370bfae04ace4
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-04 21:19:43 +0200

    Implement DRActorEnumRecurse

--- src/carma2/common/utility.h
+++ src/carma2/common/utility.h
@@ -26,4 +26,6 @@ br_pixelmap* C2_HOOK_FASTCALL DRPixelmapAllocate(br_uint_8 pType, br_uint_16 pW,
 
 void C2_HOOK_FASTCALL DRPixelmapRectangleCopy(br_pixelmap* dst, br_int_16 dx, br_int_16 dy, br_pixelmap* src, br_int_16 sx, br_int_16 sy, br_uint_16 w, br_uint_16 h);
 
+intptr_t C2_HOOK_FASTCALL DRActorEnumRecurse(br_actor* pActor, br_actor_enum_cbfn* callback, void* arg);
+
 #endif // REC2_UTILITY_H
