1b927b424051a5312c44cb9c0f4e4e24755faa40
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-04 19:59:17 +0200

    Implement GetDepthCueing + hook SetDepthCueingOn/ToggleDepthCueingQuietly/ToggleDepthCueing

--- src/carma2/common/depth.c
+++ src/carma2/common/depth.c
@@ -1,6 +1,7 @@
 #include "depth.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_INIT(int, gSky_on, 0x00591188, 1);
+C2_HOOK_VARIABLE_IMPLEMENT_INIT(int, gDepth_cueing_on, 0x0059118c, 1);
 
 int C2_HOOK_FASTCALL GetSkyTextureOn(void) {
 
@@ -17,3 +18,42 @@ void C2_HOOK_FASTCALL SetSkyTextureOn(int skyTextureOn) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00446e10, SetSkyTextureOn, SetSkyTextureOn_original)
+
+int C2_HOOK_FASTCALL GetDepthCueingOn(void) {
+
+    return C2V(gDepth_cueing_on);
+}
+C2_HOOK_FUNCTION(0x00446f90, GetDepthCueingOn)
+
+void (C2_HOOK_FASTCALL * SetDepthCueingOn_original)(int pOn);
+void C2_HOOK_FASTCALL SetDepthCueingOn(int pOn) {
+
+#if defined(C2_HOOKS_ENABLED)
+    SetDepthCueingOn_original(pOn);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00446fa0, SetDepthCueingOn, SetDepthCueingOn_original)
+
+void (C2_HOOK_FASTCALL * ToggleDepthCueingQuietly_original)(void);
+void C2_HOOK_FASTCALL ToggleDepthCueingQuietly(void) {
+
+#if defined(C2_HOOKS_ENABLED)
+    ToggleDepthCueingQuietly_original();
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00447070, ToggleDepthCueingQuietly, ToggleDepthCueingQuietly_original)
+
+void (C2_HOOK_FASTCALL * ToggleDepthCueing_original)(void);
+void C2_HOOK_FASTCALL ToggleDepthCueing(void) {
+
+#if defined(C2_HOOKS_ENABLED)
+    ToggleDepthCueing_original();
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00447110, ToggleDepthCueing, ToggleDepthCueing_original)
