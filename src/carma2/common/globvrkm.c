ed45366dfed241541b79ca4395ca7883e7c9e8b1
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-04 20:43:48 +0200

    Hook gCamera_list

---
+++ src/carma2/common/globvrkm.c
@@ -0,0 +1,3 @@
+#include "globvrkm.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY_INIT(br_actor*, gCamera_list, 2, 0x0074c7b0);
---
+++ src/carma2/common/globvrkm.h
@@ -0,0 +1,10 @@
+#ifndef REC2_GLOBVRKM_H
+#define REC2_GLOBVRKM_H
+
+#include "c2_hooks.h"
+
+#include <brender/br_types.h>
+
+C2_HOOK_VARIABLE_DECLARE_ARRAY(br_actor*, gCamera_list, 2);
+
+#endif //REC2_GLOBVRKM_H
