d06f67775c58a4c2667595035ca84a5b506ccfc8
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-04 19:59:17 +0200

    Implement GetDepthCueing + hook SetDepthCueingOn/ToggleDepthCueingQuietly/ToggleDepthCueing

--- src/carma2/common/depth.h
+++ src/carma2/common/depth.h
@@ -7,4 +7,12 @@ int C2_HOOK_FASTCALL GetSkyTextureOn(void);
 
 void C2_HOOK_FASTCALL SetSkyTextureOn(int skyTextureOn);
 
+int C2_HOOK_FASTCALL GetDepthCueingOn(void);
+
+void C2_HOOK_FASTCALL SetDepthCueingOn(int pOn);
+
+void C2_HOOK_FASTCALL ToggleDepthCueingQuietly(void);
+
+void C2_HOOK_FASTCALL ToggleDepthCueing(void);
+
 #endif //REC2_DEPTH_H
