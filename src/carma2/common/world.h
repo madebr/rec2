3c39582a90c68f921d9dbd62ed6b654007dfc1f6
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-06 04:58:30 +0200

    Implement DRstrcmpi

--- src/carma2/common/world.h
+++ src/carma2/common/world.h
@@ -57,4 +57,6 @@ int C2_HOOK_FASTCALL ResolveTexturePathLink(char* realPath, const char* path);
 
 int C2_HOOK_FASTCALL CreatePathLink(const char* targetPath, const char* linkPath);
 
+int C2_HOOK_FASTCALL DRstrcmpi(const char* str1, const char* str2);
+
 #endif // REC2_WORLD_H
