d00f78217ff9b2bd457c7d4978690f430ca498b0
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-07 18:53:32 +0200

    Add libtiff

--- conanfile.py
+++ conanfile.py
@@ -11,11 +11,20 @@ class CarmaBypass(ConanFile):
     }
     default_options = {
         "hook": True,
+        "libtiff:lzma": False,
+        "libtiff:jpeg": False,
+        "libtiff:zlib": False,
+        "libtiff:libdeflate": False,
+        "libtiff:zstd": False,
+        "libtiff:jbig": False,
+        "libtiff:webp": False,
+        "libtiff:cxx":  False,
     }
 
     def requirements(self):
         if self.options.hook:
             self.requires("detours/cci.20220630")
+        self.requires("libtiff/4.4.0")
 
     def validate(self):
         assert self.settings.os == "Windows"
