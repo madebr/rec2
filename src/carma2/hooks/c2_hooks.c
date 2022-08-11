d1432d319ab34a9dbc61790950ce81a64c2c0098
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-11 16:06:17 +0200

    Add macro aborting when a function is unexpectedly used

--- src/carma2/hooks/c2_hooks.c
+++ src/carma2/hooks/c2_hooks.c
@@ -3,6 +3,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include <windows.h>
+
 static FILE* gHookFile;
 
 void c2_hook_init(void) {
@@ -28,4 +30,22 @@ void c2_hook_vdebugf(const char* function, const char* format, va_list ap) {
     vfprintf(gHookFile, format, ap);
     fputc('\n', gHookFile);
     fflush(gHookFile);
+}
+
+void c2_hook_fatal_error(const char* function, const char* format, ...) {
+    va_list ap;
+    char buffer[512];
+
+    va_start(ap, format);
+    c2_hook_vdebugf(function, format, ap);
+    va_end(ap);
+
+    sprintf(buffer, "%s: ", function);
+    va_start(ap, format);
+    vsprintf(buffer + strlen(buffer), format, ap);
+    va_end(ap);
+
+    MessageBoxA(NULL, buffer, "Hook Fatal Error", MB_ICONERROR | MB_OK);
+
+    abort();
 }
