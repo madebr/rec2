6d3a5417f49cc6fce1334f211e590c11da663685
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-11 16:07:05 +0200

    Hook (unused) DoInterfaceScreen

---
+++ src/carma2/common/intrface.h
@@ -0,0 +1,10 @@
+#ifndef REC2_INTRFACE_H
+#define REC2_INTRFACE_H
+
+#include "c2_hooks.h"
+
+#include "rec2_types.h"
+
+int C2_HOOK_FASTCALL DoInterfaceScreen(const tInterface_spec* pSpec, int pOptions, int pCurrent_choice);
+
+#endif //REC2_INTRFACE_H
