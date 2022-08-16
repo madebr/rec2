87032ebd62e628608a61b0abbbda1a8a3a889ebc
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-16 21:57:59 +0200

    Hook DRS3StartSound + gEffects_outlet

--- src/carma2/common/sound.h
+++ src/carma2/common/sound.h
@@ -7,6 +7,7 @@
 
 C2_HOOK_VARIABLE_DECLARE(int, gSound_detail_level);
 C2_HOOK_VARIABLE_DECLARE(int, gCD_fully_installed);
+C2_HOOK_VARIABLE_DECLARE(tS3_outlet_ptr, gEffects_outlet);
 
 void C2_HOOK_FASTCALL UsePathFileToDetermineIfFullInstallation(void);
 
@@ -15,5 +16,7 @@ void C2_HOOK_FASTCALL ParseSoundFxDetails(tTWTFILE* pF, tSpecial_volume_soundfx_
 void C2_HOOK_FASTCALL StopMusic(void);
 
 void C2_HOOK_FASTCALL DRS3StartCDA(tS3_sound_id pCDA_id);
+
+tS3_sound_tag C2_HOOK_FASTCALL DRS3StartSound(tS3_outlet_ptr pOutlet, tS3_sound_id pSound);
 
 #endif // REC2_SOUND_H
