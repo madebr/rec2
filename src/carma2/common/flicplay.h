05c460d61e34520536454c992dc910ee60c3360a
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-16 18:22:55 +0200

    Implement StartFlic

--- src/carma2/common/flicplay.h
+++ src/carma2/common/flicplay.h
@@ -5,4 +5,6 @@
 
 #include "rec2_types.h"
 
+int C2_HOOK_FASTCALL StartFlic(char* pFile_name, int pIndex, tFlic_descriptor_ptr pFlic_info, tU32 pSize, tS8* pData_ptr, br_pixelmap* pDest_pixelmap, int pX_offset, int pY_offset, int pFrame_rate);
+
 #endif //REC2_FLICPLAY_H
