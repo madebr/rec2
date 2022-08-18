64ef9ef762dad4b9c12ad47fce4282e6567e79a5
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-18 17:43:34 +0200

    Implement SetPolyFOntBorderColours

--- src/carma2/common/font.c
+++ src/carma2/common/font.c
@@ -1,6 +1,7 @@
 #include "font.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(br_pixelmap*, gTextureMaps, 1000, 0x0076c960);
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tPolyFontBorderColours, gPolyFontBorderColours, 27, 0x00765ec0);
 
 br_material* C2_HOOK_FASTCALL CreateFontCharacterMaterial(int textureIdx) {
     br_material* pMaterial;
@@ -15,3 +16,22 @@ br_material* C2_HOOK_FASTCALL CreateFontCharacterMaterial(int textureIdx) {
     return pMaterial;
 }
 C2_HOOK_FUNCTION(0x00464cf0, CreateFontCharacterMaterial)
+
+void C2_HOOK_FASTCALL SetPolyFontBorderColours(br_model* pModel, int fontIdx) {
+    if (pModel == NULL) {
+        return;
+    }
+    pModel->vertices[0].red = C2V(gPolyFontBorderColours)[fontIdx].tl.r;
+    pModel->vertices[0].grn = C2V(gPolyFontBorderColours)[fontIdx].tl.g;
+    pModel->vertices[0].blu = C2V(gPolyFontBorderColours)[fontIdx].tl.b;
+    pModel->vertices[1].red = C2V(gPolyFontBorderColours)[fontIdx].tr.r;
+    pModel->vertices[1].grn = C2V(gPolyFontBorderColours)[fontIdx].tr.g;
+    pModel->vertices[1].blu = C2V(gPolyFontBorderColours)[fontIdx].tr.b;
+    pModel->vertices[2].red = C2V(gPolyFontBorderColours)[fontIdx].bl.r;
+    pModel->vertices[2].grn = C2V(gPolyFontBorderColours)[fontIdx].bl.g;
+    pModel->vertices[2].blu = C2V(gPolyFontBorderColours)[fontIdx].bl.b;
+    pModel->vertices[3].red = C2V(gPolyFontBorderColours)[fontIdx].br.r;
+    pModel->vertices[3].grn = C2V(gPolyFontBorderColours)[fontIdx].br.g;
+    pModel->vertices[3].blu = C2V(gPolyFontBorderColours)[fontIdx].br.b;
+}
+C2_HOOK_FUNCTION(0x00464d40, SetPolyFontBorderColours)
