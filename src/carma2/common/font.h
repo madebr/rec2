689614444c7878a352ca978f76e132b220bb2775
Anonymous Maarten <anonymous.maarten@gmail.com>
2022-08-18 17:23:05 +0200

    Implement CreateFontCharacterModel

---
+++ src/carma2/common/font.h
@@ -0,0 +1,10 @@
+#ifndef REC2_FONT_H
+#define REC2_FONT_H
+
+#include "c2_hooks.h"
+
+#include "brender/brender.h"
+
+br_material* C2_HOOK_FASTCALL CreateFontCharacterMaterial(int textureIdx);
+
+#endif //REC2_FONT_H
