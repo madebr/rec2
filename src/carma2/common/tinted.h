33aab9b6527d42d825aa1f080cee4a61f051b69b
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-05-09 15:37:39 +0200

    Implement CreateInterpolatedQuadModel

--- src/carma2/common/tinted.h
+++ src/carma2/common/tinted.h
@@ -3,9 +3,13 @@
 
 #include "c2_hooks.h"
 
+#include <brender/brender.h>
+
 void C2_HOOK_FASTCALL InitTintedPolys(void);
 
 void C2_HOOK_FASTCALL SetTintedColour(int pTintedIndex, int pRed, int pGreen, int pBlue);
+
+br_model* C2_HOOK_FASTCALL CreateInterpolatedQuadModel(int x0, int y0, int width, int height, int nbX, int nbY);
 
 void C2_HOOK_FASTCALL FreeTintedPolyActor(int pTintedIndex);
 
