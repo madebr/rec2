afcd2d8f93d6ff248ae93f05182e7a6e660c5e47
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-05-20 03:49:29 +0200

    Implement PDConvertToASCIILessThan128

--- src/carma2/platform/platform.h
+++ src/carma2/platform/platform.h
@@ -19,6 +19,7 @@ void C2_HOOK_FASTCALL PDEnterDebugger(const char* pStr);
 void C2_HOOK_FASTCALL PDFatalError(char* pThe_str);
 void C2_HOOK_FASTCALL PDNonFatalError(char* pThe_str);
 int C2_HOOK_FASTCALL PDIsWindowActive(void);
+char C2_HOOK_FASTCALL PDConvertToASCIILessThan128(char pChar);
 void C2_HOOK_FASTCALL PDSetFileVariables(void);
 C2_NORETURN void C2_HOOK_FASTCALL PDShutdownSystem(void);
 void C2_HOOK_FASTCALL PDUnlockRealBackScreen(void);
