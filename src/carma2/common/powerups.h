480ff71d010a35016365043f6f461eb36b42950f
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-12 18:00:31 +0200

    Add gPickedup_powerups

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -1,7 +1,11 @@
 #ifndef REC2_POWERUPS_H
 #define REC2_POWERUPS_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
+
+C2_HOOK_VARIABLE_DECLARE_ARRAY(tHeadup_icon, gPickedup_powerups, 5);
 
 void C2_HOOK_FASTCALL InitPowerups(void);
 
