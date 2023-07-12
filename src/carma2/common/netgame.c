2ea01272fe69c53a108a63e99a4a429d3dae21c8
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-12 18:43:12 +0200

    Hook DefaultNetName

--- src/carma2/common/netgame.c
+++ src/carma2/common/netgame.c
@@ -3,6 +3,14 @@
 #include "globvars.h"
 #include "platform.h"
 
+void C2_HOOK_FASTCALL DefaultNetName(void) {
+
+    /* FIXME: 32 overflows the player_name field */
+    C2_HOOK_ASSERT((uintptr_t)&C2V(gProgram_state).player_name == 0x0075d590);
+    NetObtainSystemUserName(C2V(gProgram_state).player_name, 32);
+}
+C2_HOOK_FUNCTION(0x0049bd10, DefaultNetName)
+
 void C2_HOOK_FASTCALL NetObtainSystemUserName(char* pName, int pMax_length) {
 
     PDNetObtainSystemUserName(pName, pMax_length);
