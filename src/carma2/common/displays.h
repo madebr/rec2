8d4f8fe8f5e8bc3380caaece57b4cfc0773b74d3
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-12 15:32:37 +0200

    Hook InitHeadups

--- src/carma2/common/displays.h
+++ src/carma2/common/displays.h
@@ -9,4 +9,6 @@ int C2_HOOK_FASTCALL DRTextWidth(const tDR_font* pFont, const char* pText);
 
 void C2_HOOK_FASTCALL TransDRPixelmapText(br_pixelmap* pPixelmap, int pX, int pY, const tDR_font* pFont, const char* pText, int pRight_edge);
 
+void C2_HOOK_FASTCALL InitHeadups(void);
+
 #endif //REC2_DISPLAYS_H
