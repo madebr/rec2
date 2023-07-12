61c7c884031b197d22b2d9a62c0a3ca5adef64e7
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-12 18:31:19 +0200

    Hook AllocateStandardLamp

--- src/carma2/common/init.h
+++ src/carma2/common/init.h
@@ -11,6 +11,8 @@ int C2_HOOK_FASTCALL GetScreenSize(void);
 
 void C2_HOOK_FASTCALL SetScreenSize(int pNew_size);
 
+void C2_HOOK_FASTCALL AllocateStandardLamp(void);
+
 void C2_HOOK_FASTCALL InitGame(int pStart_race);
 
 #endif //REC2_INIT_H
