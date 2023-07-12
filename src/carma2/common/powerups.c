a9c30c5d39f786accb62bdc3b5a115aceb6ea3cd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-12 17:59:46 +0200

    Hook LoadPowerups

--- src/carma2/common/powerups.c
+++ src/carma2/common/powerups.c
@@ -11,3 +11,15 @@ void C2_HOOK_FASTCALL InitPowerups(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004d9ea0, InitPowerups, InitPowerups_original)
+
+void (C2_HOOK_FASTCALL * LoadPowerups_original)(void);
+void C2_HOOK_FASTCALL LoadPowerups(void) {
+
+    C2_HOOK_ASSUME_UNUSED();
+#if defined(C2_HOOKS_ENABLED)
+    LoadPowerups_original();
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004d96c0, LoadPowerups, LoadPowerups_original)
