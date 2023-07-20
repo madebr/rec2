071f3a342922f76cec6a41efe575c40a6a87af79
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-20 16:17:49 +0200

    Implement InitRayCasting

--- src/carma2/common/raycast.h
+++ src/carma2/common/raycast.h
@@ -3,6 +3,10 @@
 
 #include "c2_hooks.h"
 
+#include <brender/brender.h>
+
+C2_HOOK_VARIABLE_DECLARE(br_actor*, gY_picking_camera);
+
 void C2_HOOK_FASTCALL InitRayCasting(void);
 
 #endif //REC2_RAYCAST_H
