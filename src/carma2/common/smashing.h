85d69f977b0d87b9a2548e404cf26410542b44d5
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-20 21:52:36 +0200

    Implement InitSmashing

--- src/carma2/common/smashing.h
+++ src/carma2/common/smashing.h
@@ -1,7 +1,12 @@
 #ifndef REC2_SMASHING_H
 #define REC2_SMASHING_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
+
+C2_HOOK_VARIABLE_DECLARE_ARRAY(tSmash_vertex, gSmash_vertices, 200);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(tSmash_quad, gSmash_quads, 50);
 
 void C2_HOOK_FASTCALL InitSmashing(void);
 
