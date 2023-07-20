75dbc6d18f8a16a9ac166fbb8eb4e1b0a6edeabd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-20 23:25:39 +0200

    Implement LoadMaterial

--- src/carma2/common/loading.c
+++ src/carma2/common/loading.c
@@ -2280,3 +2280,13 @@ void C2_HOOK_FASTCALL DisableVertexColours(br_model** pModels, int pCount) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00518690, DisableVertexColours, DisableVertexColours_original)
+
+br_material* C2_HOOK_FASTCALL LoadMaterial(const char* pThe_path) {
+    tPath_name the_path;
+
+    PossibleService();
+    PathCat(the_path, C2V(gApplication_path), "MATERIAL");
+    PathCat(the_path, the_path, pThe_path);
+    return BrMaterialLoad(the_path);
+}
+C2_HOOK_FUNCTION(0x0048ef80, LoadMaterial)
