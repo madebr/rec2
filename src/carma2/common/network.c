5a1ef892f6c43c4147083a8f6ab5f5132f5848f9
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-24 04:44:06 +0200

    Implement InitNetHeadups

--- src/carma2/common/network.c
+++ src/carma2/common/network.c
@@ -1,5 +1,11 @@
 #include "network.h"
 
+#include "loading.h"
+
+#include "brender/brender.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT(br_pixelmap*, gDigits_pix, 0x0075b8f8);
+
 void (C2_HOOK_FASTCALL * NetPlayerStatusChanged_original)(tPlayer_status pNew_status);
 void C2_HOOK_FASTCALL NetPlayerStatusChanged(tPlayer_status pNew_status) {
 #if defined(C2_HOOKS_ENABLED)
@@ -9,3 +15,12 @@ NetPlayerStatusChanged_original(pNew_status);
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004a57e0, NetPlayerStatusChanged, NetPlayerStatusChanged_original)
+
+void C2_HOOK_FASTCALL InitNetHeadups(void) {
+
+    C2V(gDigits_pix) = DRLoadPixelmap("HDIGITS.PIX");
+    if (C2V(gDigits_pix) != NULL) {
+        BrMapAdd(C2V(gDigits_pix));
+    }
+}
+C2_HOOK_FUNCTION(0x0049a710, InitNetHeadups)
--- src/carma2/common/network.h
+++ src/carma2/common/network.h
@@ -5,6 +5,10 @@
 
 #include "c2_hooks.h"
 
+C2_HOOK_VARIABLE_DECLARE(br_pixelmap*, gDigits_pix);
+
+void C2_HOOK_FASTCALL InitNetHeadups(void);
+
 void C2_HOOK_FASTCALL NetPlayerStatusChanged(tPlayer_status pNew_status);
 
 #endif // REC2_NETWORK_H
