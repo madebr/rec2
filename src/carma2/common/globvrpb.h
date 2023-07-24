6304c114685c29de08db0f14cb6faf7c597363b2
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-24 04:48:45 +0200

    Implement DisposeGameIfNecessary

--- src/carma2/common/globvrpb.h
+++ src/carma2/common/globvrpb.h
@@ -7,5 +7,7 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(tNet_mode, gNet_mode);
+C2_HOOK_VARIABLE_DECLARE(int, gNumber_of_net_players);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(tNet_game_player_info, gNet_players, 12);
 
 #endif // REC2_GLOBVRPB_H
