c7e72c2e0d4385b88d92ec4abee1c0092481c254
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-24 04:45:06 +0200

    Implement DisposeNetHeadups

--- src/carma2/common/network.h
+++ src/carma2/common/network.h
@@ -9,6 +9,8 @@ C2_HOOK_VARIABLE_DECLARE(br_pixelmap*, gDigits_pix);
 
 void C2_HOOK_FASTCALL InitNetHeadups(void);
 
+void C2_HOOK_FASTCALL DisposeNetHeadups(void);
+
 void C2_HOOK_FASTCALL NetPlayerStatusChanged(tPlayer_status pNew_status);
 
 #endif // REC2_NETWORK_H
