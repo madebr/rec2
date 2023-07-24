22b30a2194c915c0a9bdf82d1f2b6b0a04bf3bd7
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-24 04:47:09 +0200

    Implement ShutdownNetIfRequired

--- src/carma2/common/network.h
+++ src/carma2/common/network.h
@@ -6,6 +6,9 @@
 #include "c2_hooks.h"
 
 C2_HOOK_VARIABLE_DECLARE(br_pixelmap*, gDigits_pix);
+C2_HOOK_VARIABLE_DECLARE(tMin_message*, gMin_messages);
+C2_HOOK_VARIABLE_DECLARE(tMid_message*, gMid_messages);
+C2_HOOK_VARIABLE_DECLARE(tMax_message*, gMax_messages);
 
 void C2_HOOK_FASTCALL InitNetHeadups(void);
 
@@ -14,5 +17,7 @@ void C2_HOOK_FASTCALL DisposeNetHeadups(void);
 void C2_HOOK_FASTCALL NetPlayerStatusChanged(tPlayer_status pNew_status);
 
 void C2_HOOK_FASTCALL NetLeaveGame(tNet_game_details* pNet_game);
+
+void C2_HOOK_FASTCALL ShutdownNetIfRequired(void);
 
 #endif // REC2_NETWORK_H
