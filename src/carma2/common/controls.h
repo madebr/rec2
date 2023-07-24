0c4dcf00c5e9ca62f0b06eadb264cf98e806509e
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-24 04:40:59 +0200

    Implement DisposeAbuseomatic

--- src/carma2/common/controls.h
+++ src/carma2/common/controls.h
@@ -3,10 +3,14 @@
 
 #include "c2_hooks.h"
 
+C2_HOOK_VARIABLE_DECLARE_ARRAY(char*, gAbuse_text, 10);
+
 void C2_HOOK_FASTCALL SetSoundDetailLevel(int pLevel);
 
 void C2_HOOK_FASTCALL ReallySetSoundDetailLevel(int pLevel);
 
 int C2_HOOK_FASTCALL GetSoundDetailLevel(void);
+
+void C2_HOOK_FASTCALL DisposeAbuseomatic(void);
 
 #endif //REC2_CONTROLS_H
