3a5073ac1c27b20032377edf4735b87922444ef9
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 00:22:08 +0200

    Implement NewTextHeadupSlot2

--- src/carma2/common/displays.c
+++ src/carma2/common/displays.c
@@ -1,5 +1,9 @@
 #include "displays.h"
 
+#include "globvars.h"
+#include "graphics.h"
+#include "utility.h"
+
 #include "platform.h"
 
 #include "c2_string.h"
@@ -9,6 +13,7 @@
 C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tHeadup, gHeadups, 37, 0x0067c500);
 C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tQueued_headup, gQueued_headups, 4, 0x0067f890);
 C2_HOOK_VARIABLE_IMPLEMENT(int, gQueued_headup_count, 0x0067f888);
+C2_HOOK_VARIABLE_IMPLEMENT(tU32, gLast_centre_headup, 0x0067fcf8);
 
 int (C2_HOOK_FASTCALL * DRTextWidth_original)(const tDR_font* pFont, const char* pText);
 int C2_HOOK_FASTCALL DRTextWidth(const tDR_font* pFont, const char* pText) {
@@ -90,3 +95,70 @@ void C2_HOOK_FASTCALL ClearQueuedHeadups(void) {
     }
 }
 C2_HOOK_FUNCTION(0x004497b0, ClearQueuedHeadups)
+
+int C2_HOOK_FASTCALL NewTextHeadupSlot2(int pSlot_index, int pFlash_rate, int pLifetime, int pFont_index, char* pText, int pQueue_it) {
+    int index;
+    tHeadup* the_headup;
+    tHeadup_slot* headup_slot;
+    tU32 time;
+
+    time = PDGetTotalTime();
+    if (pQueue_it && pSlot_index == 4 && time - C2V(gLast_centre_headup) < 1000) {
+        if (C2V(gQueued_headup_count) == 4) {
+            KillOldestQueuedHeadup();
+        }
+        C2V(gQueued_headups)[C2V(gQueued_headup_count)].flash_rate = pFlash_rate;
+        C2V(gQueued_headups)[C2V(gQueued_headup_count)].lifetime = pLifetime;
+        C2V(gQueued_headups)[C2V(gQueued_headup_count)].font_index = pFont_index;
+        c2_strcpy(C2V(gQueued_headups)[C2V(gQueued_headup_count)].text, pText);
+        C2V(gQueued_headup_count)++;
+        return -1;
+    }
+    index = FindAHeadupHoleWoofBarkSoundsABitRude(pSlot_index);
+    if (index < 0) {
+        return index;
+    }
+    if (pSlot_index == 4) {
+        C2V(gLast_centre_headup) = time;
+    }
+    headup_slot = &C2V(gProgram_state).current_car.headup_slots[C2V(gProgram_state).cockpit_on][pSlot_index];
+    the_headup = &C2V(gHeadups)[index];
+    the_headup->data.coloured_text_info.coloured_font = &C2V(gFonts)[-pFont_index];
+    if (pSlot_index == 4) {
+        the_headup->type = eHeadup_box_text;
+    } else {
+        the_headup->type = eHeadup_coloured_text;
+    }
+    c2_strcpy(the_headup->data.coloured_text_info.text, pText);
+
+    the_headup->slot_index = pSlot_index;
+    the_headup->justification = headup_slot->justification;
+    if (pSlot_index < 0) {
+        the_headup->cockpit_anchored = 0;
+    } else {
+        the_headup->cockpit_anchored = headup_slot->cockpit_anchored;
+    }
+    the_headup->field_0x3c = headup_slot->field_0x28;
+    the_headup->dimmed_background = headup_slot->dimmed_background;
+    the_headup->dim_left = headup_slot->dim_left;
+    the_headup->dim_top = headup_slot->dim_top;
+    the_headup->dim_right = headup_slot->dim_right;
+    the_headup->dim_bottom = headup_slot->dim_bottom;
+    the_headup->original_x = headup_slot->x;
+    the_headup->right_edge = MungeHeadupWidth(the_headup) + the_headup->x;
+    the_headup->y = headup_slot->y;
+    if (pFlash_rate) {
+        the_headup->flash_period = 1000 / pFlash_rate;
+    } else {
+        the_headup->flash_period = 0;
+    }
+    the_headup->last_flash = 0;
+    the_headup->flash_state = 0;
+    if (pLifetime) {
+        the_headup->end_time = GetTotalTime() + pLifetime;
+    } else {
+        the_headup->end_time = 0;
+    }
+    return index;
+}
+C2_HOOK_FUNCTION(0x00449fd0, NewTextHeadupSlot2)
