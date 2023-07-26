bae9cb4aedb4b3c22d6ee105667af0ee142a7825
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 00:22:08 +0200

    Implement NewTextHeadupSlot2

--- src/carma2/common/displays.h
+++ src/carma2/common/displays.h
@@ -19,4 +19,6 @@ void C2_HOOK_FASTCALL KillOldestQueuedHeadup(void);
 
 void C2_HOOK_FASTCALL ClearQueuedHeadups(void);
 
+int C2_HOOK_FASTCALL NewTextHeadupSlot2(int pSlot_index, int pFlash_rate, int pLifetime, int pFont_index, char* pText, int pQueue_it);
+
 #endif //REC2_DISPLAYS_H
