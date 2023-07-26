db30224952b5f00065895337271c91ef34e6409d
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-26 17:21:54 +0200

    Hook SoundService

--- src/carma2/common/sound.c
+++ src/carma2/common/sound.c
@@ -106,3 +106,13 @@ void C2_HOOK_FASTCALL SetSoundVolumes(int pCD_audio) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00456530, SetSoundVolumes, SetSoundVolumes_original)
+
+void (C2_HOOK_FASTCALL * SoundService_original)(void);
+void C2_HOOK_FASTCALL SoundService(void) {
+#if defined(C2_HOOKS_ENABLED)
+    SoundService_original();
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00455a80, SoundService, SoundService_original)
