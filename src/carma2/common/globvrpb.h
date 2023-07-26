0851fa4c0f1f56208680868f6e625e44d0d4c4ff
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-26 12:32:25 +0200

    Implement CheckKevKeys

--- src/carma2/common/globvrpb.h
+++ src/carma2/common/globvrpb.h
@@ -9,5 +9,6 @@
 C2_HOOK_VARIABLE_DECLARE(tNet_mode, gNet_mode);
 C2_HOOK_VARIABLE_DECLARE(int, gNumber_of_net_players);
 C2_HOOK_VARIABLE_DECLARE_ARRAY(tNet_game_player_info, gNet_players, 12);
+C2_HOOK_VARIABLE_DECLARE(int, gThis_net_player_index);
 
 #endif // REC2_GLOBVRPB_H
