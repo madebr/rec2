b4e590f8028c5b823c73afe7a1f9a0ceb44d3159
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 15:06:48 +0200

    Hook LoadInOppoPaths

--- src/carma2/common/opponent.h
+++ src/carma2/common/opponent.h
@@ -1,8 +1,12 @@
 #ifndef REC2_OPPONENT_H
 #define REC2_OPPONENT_H
 
+#include "c2_stdio.h"
+
 #include "c2_hooks.h"
 
 void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index);
+
+void C2_HOOK_FASTCALL LoadInOppoPaths(FILE* pF);
 
 #endif //REC2_OPPONENT_H
