1be6c84113b2ef2abad21ca2913cdc35d9a5095c
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 19:13:56 +0200

    Implement LoadPlayerCars

--- src/carma2/include/rec2_types.h
+++ src/carma2/include/rec2_types.h
@@ -887,7 +887,7 @@ typedef struct tCar_spec {
     int invulnerable1;
     int invulnerable2;
     undefined field_0x1bc[4];
-    int field_0x1c0;
+    int is_race_goal;
     int wall_climber_mode;
     int can_be_stolen;
     int has_been_stolen;
