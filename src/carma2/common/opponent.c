a3a483e4ba60e93ec523216007f0a358ffddc2bd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 02:28:00 +0200

    Move InitAIWorld from opponent.c to loading.c

--- src/carma2/common/opponent.c
+++ src/carma2/common/opponent.c
@@ -1,12 +1,1 @@
 #include "opponent.h"
-
-void (C2_HOOK_FASTCALL * InitAIWorld_original)(void);
-void C2_HOOK_FASTCALL InitAIWorld(void) {
-
-#if defined(C2_HOOKS_ENABLED)
-    InitAIWorld_original();
-#else
-#error "Not implemented"
-#endif
-}
-C2_HOOK_FUNCTION_ORIGINAL(0x00401000, InitAIWorld, InitAIWorld_original)
