8feaef0d02bb0a48352b9b48c75d6df6221e606b
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 15:06:48 +0200

    Hook LoadInOppoPaths

--- src/carma2/common/opponent.c
+++ src/carma2/common/opponent.c
@@ -7,3 +7,14 @@ void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index) {
     C2V(gOpponents)[pOpponent_index].psyche.grudge_against_player = 0;
 }
 C2_HOOK_FUNCTION(0x004aee90, InitOpponentPsyche)
+
+void (C2_HOOK_FASTCALL * LoadInOppoPaths_original)(FILE* pF);
+void C2_HOOK_FASTCALL LoadInOppoPaths(FILE* pF) {
+
+#if defined(C2_HOOKS_ENABLED)
+    LoadInOppoPaths_original(pF);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004a9610, LoadInOppoPaths, LoadInOppoPaths_original)
