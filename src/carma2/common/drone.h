d1cdaf4fb4d9622b9cfc741b86abe405af56f50c
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 03:58:04 +0200

    Implement LoadPanGameDroneInfo

--- src/carma2/common/drone.h
+++ src/carma2/common/drone.h
@@ -1,7 +1,12 @@
 #ifndef REC2_DRONE_H
 #define REC2_DRONE_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
+
+C2_HOOK_VARIABLE_DECLARE_ARRAY(tDrone, gDrones, 64);
+C2_HOOK_VARIABLE_DECLARE(int, gCount_drones);
 
 void C2_HOOK_CDECL DroneDebug(const char* message, ...);
 
