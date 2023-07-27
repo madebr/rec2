11c76b5f1bf1eaff0b180592215088fff9992164
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-07-27 02:34:38 +0200

    Implement InitOpponentPsyche

--- src/carma2/common/opponent.c
+++ src/carma2/common/opponent.c
@@ -1,1 +1,9 @@
 #include "opponent.h"
+
+#include "globvars.h"
+
+void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index) {
+
+    C2V(gOpponents)[pOpponent_index].psyche.grudge_against_player = 0;
+}
+C2_HOOK_FUNCTION(0x004aee90, InitOpponentPsyche)
--- src/carma2/common/opponent.h
+++ src/carma2/common/opponent.h
@@ -3,4 +3,6 @@
 
 #include "c2_hooks.h"
 
+void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index);
+
 #endif //REC2_OPPONENT_H
