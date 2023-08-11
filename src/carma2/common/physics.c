09b3333b38b6c2f5f16a55cf4fd20418b38517f3
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-11 23:51:29 +0200

    Hook LoadCollisionShape

--- src/carma2/common/physics.c
+++ src/carma2/common/physics.c
@@ -11,3 +11,14 @@ void C2_HOOK_FASTCALL InitPhysics(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004b5ca0, InitPhysics, InitPhysics_original)
+
+void (C2_HOOK_FASTCALL * LoadCollisionShape_original)(tCollision_shape** pShape, FILE* pF);
+void C2_HOOK_FASTCALL LoadCollisionShape(tCollision_shape** pShape, FILE* pF) {
+
+#if defined(C2_HOOKS_ENABLED)
+    LoadCollisionShape_original(pShape, pF);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00488b00, LoadCollisionShape, LoadCollisionShape_original)
