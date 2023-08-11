18770cbd2aaf5894cd188e306069e4466c9efd5d
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-11 23:51:56 +0200

    Hook UpdateCollisionBoundingBox

--- src/carma2/common/physics.c
+++ src/carma2/common/physics.c
@@ -22,3 +22,14 @@ void C2_HOOK_FASTCALL LoadCollisionShape(tCollision_shape** pShape, FILE* pF) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00488b00, LoadCollisionShape, LoadCollisionShape_original)
+
+void (C2_HOOK_FASTCALL * UpdateCollisionBoundingBox_original)(tCollision_info* pCollision_info);
+void C2_HOOK_FASTCALL UpdateCollisionBoundingBox(tCollision_info* pCollision_info) {
+
+#if defined(C2_HOOKS_ENABLED)
+    UpdateCollisionBoundingBox_original(pCollision_info);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004c60a0, UpdateCollisionBoundingBox, UpdateCollisionBoundingBox_original)
