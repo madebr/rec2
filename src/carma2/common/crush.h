6c8f74f19e34060d65ef59be13e0e929f4fd99e0
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-12 00:03:43 +0200

    Implement AllocateUserDetailLevel

--- src/carma2/common/crush.h
+++ src/carma2/common/crush.h
@@ -15,6 +15,8 @@ br_scalar C2_HOOK_FASTCALL SquaredDistanceFromLineSegment(br_vector3* pP, br_vec
 
 void C2_HOOK_FASTCALL LoadMinMax(FILE* pF, br_bounds3* pBounds);
 
+int C2_HOOK_CDECL AllocateUserDetailLevel(br_actor* pActor, void* pData);
+
 void C2_HOOK_FASTCALL PrepareCarForCrushing(tCar_spec * pCar_spec);
 
 #endif // REC2_CRUSH_H
