2d7cfe48d620f64e0b79813b9c237268ca9292eb
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-12 00:23:37 +0200

    Implement LoadSmashableLevels

--- src/carma2/common/crush.h
+++ src/carma2/common/crush.h
@@ -15,6 +15,12 @@ br_scalar C2_HOOK_FASTCALL SquaredDistanceFromLineSegment(br_vector3* pP, br_vec
 
 void C2_HOOK_FASTCALL LoadMinMax(FILE* pF, br_bounds3* pBounds);
 
+void C2_HOOK_FASTCALL LoadNoncarActivation(FILE* pF, tNoncar_activation** pNoncar_activations, int* pCount_noncar_activations);
+
+void C2_HOOK_FASTCALL LoadAward(FILE* pF, tAward_info* pAward_info);
+
+void C2_HOOK_FASTCALL LoadSmashableLevels(FILE* pF, tSmashable_level** pSmashable_levels, int* pCount_smashable_levels, int pIs_texture_change, tBrender_storage* pBrender_storage);
+
 int C2_HOOK_CDECL LinkCrushData(br_actor* pActor, void* pData);
 
 int C2_HOOK_CDECL LinkCarCrushData(br_actor* pActor, tCar_crush_buffer* pCar_crush_buffer);
