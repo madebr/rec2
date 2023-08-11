03842144cacb7841d960fc58383bbebaecda4147
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-11 23:51:29 +0200

    Hook LoadCollisionShape

--- src/carma2/common/physics.h
+++ src/carma2/common/physics.h
@@ -1,8 +1,12 @@
 #ifndef REC2_PHYSICS_H
 #define REC2_PHYSICS_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
 
 void C2_HOOK_FASTCALL InitPhysics(void);
+
+void C2_HOOK_FASTCALL LoadCollisionShape(tCollision_shape** pShape, FILE* pF);
 
 #endif //REC2_PHYSICS_H
