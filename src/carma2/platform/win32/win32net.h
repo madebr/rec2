af1bf563d3ec360f447073895b5bc37a0c4b13d8
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-13 01:02:57 +0200

    Implement NetNowIPXLocalTarget2String

--- src/carma2/platform/win32/win32net.h
+++ src/carma2/platform/win32/win32net.h
@@ -6,5 +6,8 @@
 #include "rec2_types.h"
 
 #include <brender/brender.h>
+#include <winsock.h>
+
+void C2_HOOK_FASTCALL NetNowIPXLocalTarget2String(char* pString, struct sockaddr* pSock_addr);
 
 #endif // C2_WIN32_NET_H
