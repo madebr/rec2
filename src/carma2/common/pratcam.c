641a42270e8718293a0914346da57e2b608a1190
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-12 02:09:06 +0200

    Implement DisposePratcam

--- src/carma2/common/pratcam.c
+++ src/carma2/common/pratcam.c
@@ -4,3 +4,8 @@ void C2_HOOK_FASTCALL InitPratcam(void) {
 
 }
 C2_HOOK_FUNCTION(0x004e0e50, InitPratcam)
+
+void C2_HOOK_FASTCALL DisposePratcam(void) {
+
+}
+C2_HOOK_FUNCTION(0x004e0e60, DisposePratcam)
