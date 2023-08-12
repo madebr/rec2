aac9eda5ed35c659c868ca79c79e43926c03edb3
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-12 20:59:25 +0200

    Implement ReadS8

--- src/carma2/common/loading.h
+++ src/carma2/common/loading.h
@@ -101,6 +101,8 @@ void C2_HOOK_FASTCALL SetDronesOn(int pNewDronesOn);
 
 void C2_HOOK_FASTCALL StripCRNL(char* line);
 
+tS8 C2_HOOK_FASTCALL ReadS8(FILE* pF);
+
 void C2_HOOK_FASTCALL WriteU32L(FILE* pF, tU32 pNumber);
 
 void C2_HOOK_FASTCALL WriteU16L(FILE* pF, tU16 pNumber);
