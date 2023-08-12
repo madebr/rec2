e73668b18a8ff416f1df312d7649a95167cb25b5
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-12 03:42:44 +0200

    Implement DisableCar

--- src/carma2/common/netgame.h
+++ src/carma2/common/netgame.h
@@ -1,10 +1,14 @@
 #ifndef REC2_NETGAME_H
 #define REC2_NETGAME_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
 
 void C2_HOOK_FASTCALL DefaultNetName(void);
 
 void C2_HOOK_FASTCALL NetObtainSystemUserName(char* pName, int pMax_length);
+
+void C2_HOOK_FASTCALL DisableCar(tCar_spec* pCar);
 
 #endif // REC2_NETGAME_H
