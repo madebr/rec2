79d889259b0d1bb67e75f83aedfca35f7e7328ca
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-18 23:23:59 +0200

    Implement GetPairOfScalars

--- src/carma2/common/loading.h
+++ src/carma2/common/loading.h
@@ -157,6 +157,8 @@ float C2_HOOK_FASTCALL GetAFloat(FILE* pF);
 
 float C2_HOOK_FASTCALL GetAScalar(FILE* pF);
 
+void C2_HOOK_FASTCALL GetPairOfScalars(FILE* pF, br_scalar* pS1, br_scalar* pS2);
+
 void C2_HOOK_FASTCALL GetPairOfFloats(FILE* pF, float* pF1, float* pF2);
 
 void C2_HOOK_FASTCALL GetThreeFloats(FILE * pF, float* pF1, float* pF2, float* pF3);
