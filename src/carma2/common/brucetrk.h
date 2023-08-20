995685cfa0a929a9e11d7e5f5170651da8c4b590
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-20 23:55:37 +0200

    Hook StripBlendedFaces

--- src/carma2/common/brucetrk.h
+++ src/carma2/common/brucetrk.h
@@ -1,11 +1,14 @@
 #ifndef REC2_BRUCETRK_H
 #define REC2_BRUCETRK_H
 
-#include "c2_hooks.h"
 #include <brender/brender.h>
+
+#include "c2_hooks.h"
 
 br_scalar C2_HOOK_STDCALL GetYonFactor(void);
 
 void C2_HOOK_STDCALL SetYonFactor(br_scalar pNew);
+
+void C2_HOOK_FASTCALL StripBlendedFaces(br_actor* pActor, br_model* pModel);
 
 #endif //REC2_BRUCETRK_H
