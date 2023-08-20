fd2593fd0e6b610d391b01677c7892870bac31a0
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-21 01:09:05 +0200

    Implement FixModelPointer

--- src/carma2/common/brucetrk.c
+++ src/carma2/common/brucetrk.c
@@ -103,3 +103,21 @@ void C2_HOOK_FASTCALL GetModelTextureArea(br_model* pModel, int* pArea_1, int* p
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004f5ae0, GetModelTextureArea, GetModelTextureArea_original)
+
+void C2_HOOK_FASTCALL FixModelPointer(br_model* pModel, br_uint_16 pFlags) {
+
+    C2_HOOK_BUG_ON(sizeof(v11group) != 0x24);
+
+    if (pModel->nvertices != 0 && pModel->nfaces != 0) {
+        int i;
+
+        BrModelUpdate(pModel, pFlags);
+        for (i = 0; i < V11MODEL(pModel)->ngroups; i++) {
+            v11group* v11g;
+
+            v11g = &V11MODEL(pModel)->groups[i];
+            (br_material*)v11g->face_colours = pModel->faces[*v11g->face_user].material;
+        }
+    }
+}
+C2_HOOK_FUNCTION(0x00515fa0, FixModelPointer)
