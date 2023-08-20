e4c6e749fc467cceaa6b1d696542ee97e071e7bd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-21 00:36:44 +0200

    Implement GetModelTextureArea

--- src/carma2/common/brucetrk.h
+++ src/carma2/common/brucetrk.h
@@ -14,4 +14,6 @@ void C2_HOOK_FASTCALL StripBlendedFaces(br_actor* pActor, br_model* pModel);
 
 intptr_t C2_HOOK_CDECL FindNonCarsCB(br_actor* pActor, tTrack_spec* pTrack_spec);
 
+void C2_HOOK_FASTCALL GetModelTextureArea(br_model* pModel, int* pArea_1, int* pArea_2);
+
 #endif //REC2_BRUCETRK_H
