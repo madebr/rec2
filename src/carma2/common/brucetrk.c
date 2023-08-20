887b2cc813567428daaa80a4d1499d93f061fb18
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-20 23:55:37 +0200

    Hook StripBlendedFaces

--- src/carma2/common/brucetrk.c
+++ src/carma2/common/brucetrk.c
@@ -13,3 +13,16 @@ void C2_HOOK_STDCALL SetYonFactor(br_scalar pNew) {
     C2V(gYon_factor) = pNew;
 }
 C2_HOOK_FUNCTION(0x0040dff0, SetYonFactor)
+
+C2_HOOK_VARIABLE_IMPLEMENT(br_actor*, gMr_blendy, 0x00679264);
+
+void (C2_HOOK_FASTCALL * StripBlendedFaces_original)(br_actor* pActor, br_model* pModel);
+void C2_HOOK_FASTCALL StripBlendedFaces(br_actor* pActor, br_model* pModel) {
+
+#if defined(C2_HOOKS_ENABLED)
+    StripBlendedFaces_original(pActor, pModel);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x0040d530, StripBlendedFaces, StripBlendedFaces_original)
