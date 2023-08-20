7711923b7cb4757645e85406e239bc42c8387c93
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-21 01:09:05 +0200

    Implement FixModelPointer

--- src/carma2/common/brucetrk.h
+++ src/carma2/common/brucetrk.h
@@ -16,4 +16,6 @@ intptr_t C2_HOOK_CDECL FindNonCarsCB(br_actor* pActor, tTrack_spec* pTrack_spec)
 
 void C2_HOOK_FASTCALL GetModelTextureArea(br_model* pModel, int* pArea_1, int* pArea_2);
 
+void C2_HOOK_FASTCALL FixModelPointer(br_model* pModel, br_uint_16 pFlags);
+
 #endif //REC2_BRUCETRK_H
