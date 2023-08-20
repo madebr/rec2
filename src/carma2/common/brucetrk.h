b3180060d4b029eb0139d0a08f385abfb0e7b1c6
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-20 23:58:13 +0200

    Hook FindNonCarsCB

--- src/carma2/common/brucetrk.h
+++ src/carma2/common/brucetrk.h
@@ -2,6 +2,7 @@
 #define REC2_BRUCETRK_H
 
 #include <brender/brender.h>
+#include "rec2_types.h"
 
 #include "c2_hooks.h"
 
@@ -10,5 +11,7 @@ br_scalar C2_HOOK_STDCALL GetYonFactor(void);
 void C2_HOOK_STDCALL SetYonFactor(br_scalar pNew);
 
 void C2_HOOK_FASTCALL StripBlendedFaces(br_actor* pActor, br_model* pModel);
+
+intptr_t C2_HOOK_CDECL FindNonCarsCB(br_actor* pActor, tTrack_spec* pTrack_spec);
 
 #endif //REC2_BRUCETRK_H
