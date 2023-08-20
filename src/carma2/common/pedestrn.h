30634b441fcc541a890042e873c67e79392ccf61
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-08-20 16:38:34 +0200

    Implement LoadTrackPedestrians

--- src/carma2/common/pedestrn.h
+++ src/carma2/common/pedestrn.h
@@ -8,6 +8,8 @@
 #include <brender/brender.h>
 #include "rec2_types.h"
 
+C2_HOOK_VARIABLE_DECLARE(tPedestrian*, gPedestrian_array);
+
 void C2_HOOK_FASTCALL InitPedsForm(tPedForms_vtable* pTable);
 
 void C2_HOOK_FAKE_THISCALL ScaleModelXYZ(br_model* pModel, int pArg2, float pX, float pY, float pZ);
@@ -21,5 +23,7 @@ void C2_HOOK_FASTCALL InitPeds(void);
 void C2_HOOK_FASTCALL InitBurningPeds(void);
 
 int C2_HOOK_FASTCALL ForEachMortalPedestrianInSight(tCar_spec* pCar_spec, int pOnly_alive, float pMax_distance, int pParam_4, void* pData, tForEachPedestrian_cbfn* pCallback);
+
+void C2_HOOK_FASTCALL LoadTrackPedestrians(FILE* pF);
 
 #endif //REC2_PEDESTRN_H
