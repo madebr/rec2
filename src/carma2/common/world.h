2962e8ad49655a3e36b9e10e1189cbf29c1db4c7
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-06 00:45:00 +0200

    Implement LoadSinglePixelmap

--- src/carma2/common/world.h
+++ src/carma2/common/world.h
@@ -97,6 +97,8 @@ void C2_HOOK_FASTCALL LoadSomeShadeTables(tBrender_storage* pStorage, const char
 
 void C2_HOOK_FASTCALL LoadPixelmapCallback(const char* pPath);
 
+br_pixelmap* C2_HOOK_FASTCALL LoadSinglePixelmap(tBrender_storage* pStorage, const char* pName);
+
 void C2_HOOK_FASTCALL LoadSomePixelmaps(tBrender_storage* pStorage, const char* pPath);
 
 void C2_HOOK_FASTCALL LoadMaterialCallback(const char* pPath);
