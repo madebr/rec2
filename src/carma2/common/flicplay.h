9984432371f35159a28dbb4c4f341c75fca8aa3a
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-06 04:21:46 +0200

    Implement LoadTrack

--- src/carma2/common/flicplay.h
+++ src/carma2/common/flicplay.h
@@ -5,6 +5,20 @@
 
 #include "rec2_types.h"
 
+int C2_HOOK_FASTCALL GetPanelFlicFrameIndex(int pIndex);
+
+void C2_HOOK_FASTCALL DontLetFlicFuckWithPalettes(void);
+
+void C2_HOOK_FASTCALL LetFlicFuckWithPalettes(void);
+
+void C2_HOOK_FASTCALL TurnFlicTransparencyOff(void);
+
+void C2_HOOK_FASTCALL TurnFlicTransparencyOn(void);
+
+void C2_HOOK_FASTCALL FlicPaletteAllocate(void);
+
+void C2_HOOK_FASTCALL AssertFlicPixelmap(tFlic_descriptor_ptr pFlic_info, br_pixelmap* pDest_pixelmap);
+
 int C2_HOOK_FASTCALL StartFlic(char* pFile_name, int pIndex, tFlic_descriptor_ptr pFlic_info, tU32 pSize, tS8* pData_ptr, br_pixelmap* pDest_pixelmap, int pX_offset, int pY_offset, int pFrame_rate);
 
 int C2_HOOK_FASTCALL EndFlic(tFlic_descriptor_ptr pFlic_info);
