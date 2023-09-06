6d76056270f90a927eb459285857bd7dec09b955
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-06 04:48:09 +0200

    Implement LoadInShrapnel

--- src/carma2/common/spark.h
+++ src/carma2/common/spark.h
@@ -7,6 +7,13 @@
 
 #include "c2_hooks.h"
 
+C2_HOOK_VARIABLE_DECLARE(int, gSmoke_on);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(int, gShade_list, 16);
+C2_HOOK_VARIABLE_DECLARE(int*, gShade_table); /* FIXME: rename to gDust_table*/
+C2_HOOK_VARIABLE_DECLARE(int, gNum_dust_tables);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(br_model*, gShrapnel_model, 2);
+C2_HOOK_VARIABLE_DECLARE(br_material*, gBlack_material);
+
 void C2_HOOK_FASTCALL SetSmokeOn(int pSmoke_on);
 
 int C2_HOOK_FASTCALL GetSmokeOn(void);
@@ -16,6 +23,8 @@ void C2_HOOK_FASTCALL GenerateSmokeShades(void);
 void C2_HOOK_FASTCALL GetSmokeShadeTables(FILE* f);
 
 void C2_HOOK_FASTCALL ModelScale(br_model* pModel, float pScale);
+
+void C2_HOOK_FASTCALL LoadInShrapnel(void);
 
 void C2_HOOK_FASTCALL LoadInKevStuff(FILE* pF);
 
