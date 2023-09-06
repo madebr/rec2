53232624a7733b931462d0b0ebdbda1020db3fc0
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-06 03:50:55 +0200

    Implement GetSmokeShadeTables

--- src/carma2/common/spark.h
+++ src/carma2/common/spark.h
@@ -1,6 +1,8 @@
 #ifndef REC2_SPARK_H
 #define REC2_SPARK_H
 
+#include "c2_stdio.h"
+
 #include "c2_hooks.h"
 
 void C2_HOOK_FASTCALL SetSmokeOn(int pSmoke_on);
@@ -8,5 +10,7 @@ void C2_HOOK_FASTCALL SetSmokeOn(int pSmoke_on);
 int C2_HOOK_FASTCALL GetSmokeOn(void);
 
 void C2_HOOK_FASTCALL GenerateSmokeShades(void);
+
+void C2_HOOK_FASTCALL GetSmokeShadeTables(FILE* f);
 
 #endif //REC2_SPARK_H
