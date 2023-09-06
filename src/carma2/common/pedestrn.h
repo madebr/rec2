ce18cd61a61f1c110edac27a794ef093794b7f6d
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-06 04:17:18 +0200

    Implement AllocateTrackPedestrians

--- src/carma2/common/pedestrn.h
+++ src/carma2/common/pedestrn.h
@@ -8,6 +8,8 @@
 #include <brender/brender.h>
 #include "rec2_types.h"
 
+C2_HOOK_VARIABLE_DECLARE(int, gPed_count);
+
 void C2_HOOK_FASTCALL InitPedsForm(tPedForms_vtable* pTable);
 
 void C2_HOOK_FAKE_THISCALL ScaleModelXYZ(br_model* pModel, int pArg2, float pX, float pY, float pZ);
@@ -23,5 +25,7 @@ void C2_HOOK_FASTCALL InitBurningPeds(void);
 int C2_HOOK_FASTCALL ForEachMortalPedestrianInSight(tCar_spec* pCar_spec, int pOnly_alive, float pMax_distance, int pParam_4, void* pData, tForEachPedestrian_cbfn* pCallback);
 
 void C2_HOOK_FASTCALL LoadTrackPedestrians(FILE* pF);
+
+void C2_HOOK_FASTCALL AllocateTrackPedestrians(void);
 
 #endif //REC2_PEDESTRN_H
