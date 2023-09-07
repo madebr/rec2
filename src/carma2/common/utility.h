0aa6513ec244767f4957e7c2d0a24f355126ef01
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-07 04:01:43 +0200

    Implement SRandomBetween

--- src/carma2/common/utility.h
+++ src/carma2/common/utility.h
@@ -52,6 +52,8 @@ int C2_HOOK_FASTCALL IRandomBetween(int pA, int pB);
 
 float C2_HOOK_STDCALL FRandomBetween(float pA, float pB);
 
+br_scalar C2_HOOK_STDCALL SRandomBetween(br_scalar pA, br_scalar pB);
+
 const char* C2_HOOK_FASTCALL GetMiscString(int pIndex);
 
 void C2_HOOK_FASTCALL AddLostTime(tU32 pLost_time);
