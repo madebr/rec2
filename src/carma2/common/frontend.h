61cafce69fba278d9cf27be8d19d3412722407fd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-12 03:48:47 +0200

    Implement LoadFrontendStrings

--- src/carma2/common/frontend.h
+++ src/carma2/common/frontend.h
@@ -5,6 +5,8 @@
 
 #include "c2_hooks.h"
 
+void C2_HOOK_FASTCALL LoadFrontendStrings(void);
+
 void C2_HOOK_FASTCALL CreateMenuActors(void);
 
 int C2_HOOK_FASTCALL FRONTEND_CreateMenu(tFrontend_spec* pFrontend_spec);
