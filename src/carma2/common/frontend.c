51550fc076f67599052c724f2f73df89d1ac4fff
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-12 03:48:47 +0200

    Implement LoadFrontendStrings

--- src/carma2/common/frontend.c
+++ src/carma2/common/frontend.c
@@ -1,5 +1,44 @@
 #include "frontend.h"
 
+#include "errors.h"
+#include "globvars.h"
+#include "loading.h"
+#include "utility.h"
+
+#include <brender/brender.h>
+
+#include "rec2_macros.h"
+#include "c2_string.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT(int, gCount_interface_strings, 0x0068c6e0);
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(char*, gInterface_strings, 300, 0x0068c230);
+
+void C2_HOOK_FASTCALL LoadFrontendStrings(void) {
+    tPath_name the_path;
+    FILE* f;
+    char s[256];
+    int i;
+
+    if (C2V(gCount_interface_strings) != 0) {
+        return;
+    }
+    PathCat(the_path, C2V(gApplication_path), "INTRFACE");
+    PathCat(the_path, the_path, "interface.txt");
+    f = TWT_fopen(the_path, "rt");
+    if (f == NULL) {
+        FatalError(kFatalError_CannotOpenTEXT_TXT);
+    }
+    C2V(gCount_interface_strings) = 0;
+    for (i = 0; !DRfeof(f) && i < REC2_ASIZE(C2V(gInterface_strings)); i++) {
+        GetALineAndDontArgue(f, s);
+        C2V(gInterface_strings)[i] = BrMemAllocate(c2_strlen(s) + 1, kMem_misc_string);
+        c2_strcpy(C2V(gInterface_strings)[i], s);
+        C2V(gCount_interface_strings)++;
+    }
+    DRfclose(f);
+}
+C2_HOOK_FUNCTION(0x00484fd0, LoadFrontendStrings)
+
 void (C2_HOOK_FASTCALL * CreateMenuActors_original)(void);
 void C2_HOOK_FASTCALL CreateMenuActors(void) {
 
