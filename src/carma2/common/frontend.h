071610988ed74524bf1909c1f149fee8f8879de7
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-12 03:47:10 +0200

    Hook FRONTEND_CreateMenu

--- src/carma2/common/frontend.h
+++ src/carma2/common/frontend.h
@@ -1,8 +1,12 @@
 #ifndef REC2_FRONTEND_H
 #define REC2_FRONTEND_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
 
 void C2_HOOK_FASTCALL CreateMenuActors(void);
+
+int C2_HOOK_FASTCALL FRONTEND_CreateMenu(tFrontend_spec* pFrontend_spec);
 
 #endif //REC2_FRONTEND_H
