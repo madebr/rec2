21e4adf58d00c25f0f5ddce44458889fe9260cab
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-12 03:52:58 +0200

    Implement FRONTEND_Setup

--- src/carma2/common/frontend.h
+++ src/carma2/common/frontend.h
@@ -5,10 +5,14 @@
 
 #include "c2_hooks.h"
 
+#include "rec2_types.h"
+
 void C2_HOOK_FASTCALL LoadFrontendStrings(void);
 
 void C2_HOOK_FASTCALL CreateMenuActors(void);
 
 int C2_HOOK_FASTCALL FRONTEND_CreateMenu(tFrontend_spec* pFrontend_spec);
+
+void C2_HOOK_FASTCALL FRONTEND_Setup(tFrontendMenuType pType) ;
 
 #endif //REC2_FRONTEND_H
