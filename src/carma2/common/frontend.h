30b72040ca3d79537e09ca3a3f5766a217bf8ffc
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-13 21:34:02 +0200

    Implement FRONTEND_CreateMenu

--- src/carma2/common/frontend.h
+++ src/carma2/common/frontend.h
@@ -17,6 +17,8 @@ int C2_HOOK_FASTCALL FRONTEND_CreateMenu(tFrontend_spec* pFrontend_spec);
 
 void C2_HOOK_FASTCALL FRONTEND_CreateItemBrenderObjects(tFrontend_brender_item* Frontend_brender_item, tS16 pX, tS16 pY, tS16 pWidth, tS16 pHeight, br_colour pColour, br_pixelmap* pMap, const char* pText);
 
+int C2_HOOK_FASTCALL FRONTEND_CreateMenu(tFrontend_spec* pFrontend_spec);
+
 void C2_HOOK_FASTCALL FRONTEND_Setup(tFrontendMenuType pType) ;
 
 #endif //REC2_FRONTEND_H
