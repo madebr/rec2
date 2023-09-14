97f3e431c3047d33c790a22ea2f704404de6e5f2
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-14 04:09:47 +0200

    Hook InitCarIcons

--- src/carma2/common/polyfont.h
+++ src/carma2/common/polyfont.h
@@ -1,9 +1,13 @@
 #ifndef REC2_POLYFONT_H
 #define REC2_POLYFONT_H
 
+#include <brender/brender.h>
+
 #include "c2_hooks.h"
 
 void C2_HOOK_FASTCALL LoadPolyFont(const char* pName, int pSize, int pIndex);
+
+void C2_HOOK_FASTCALL InitCarIcons(br_pixelmap* pMap);
 
 void C2_HOOK_FASTCALL InitPolyFonts(void);
 
