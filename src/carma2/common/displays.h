5d5fa482bf989c5bf55c1835b04ec17da702c3b4
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-16 22:46:08 +0200

    Implement CreateAccentPolyActor

--- src/carma2/common/displays.h
+++ src/carma2/common/displays.h
@@ -5,6 +5,12 @@
 
 #include "rec2_types.h"
 
+C2_HOOK_VARIABLE_DECLARE(br_material*, gAcc_poly_material);
+C2_HOOK_VARIABLE_DECLARE(br_actor*, gAcc_poly_actor);
+C2_HOOK_VARIABLE_DECLARE(br_model*, gAcc_poly_model);
+C2_HOOK_VARIABLE_DECLARE(int, gRender_acc_poly_actor);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(br_token_value, gAccent_poly_prims, 3);
+
 C2_HOOK_VARIABLE_DECLARE_ARRAY(int, gDRFont_to_polyfont_mapping, 24);
 
 int C2_HOOK_FASTCALL DRTextWidth(const tDR_font* pFont, const char* pText);
@@ -34,5 +40,7 @@ int C2_HOOK_FASTCALL NewTextHeadupSlot2(int pSlot_index, int pFlash_rate, int pL
 int C2_HOOK_FASTCALL NewTextHeadupSlot(int pSlot_index, int pFlash_rate, int pLifetime, int pFont_index, char* pText);
 
 void C2_HOOK_FASTCALL TransDRPixelmapCleverText(br_pixelmap* pPixelmap, int pX, int pY, tDR_font* pFont, char* pText, int pRight_edge);
+
+void C2_HOOK_STDCALL CreateAccentPolyActor(float pX, float pY, float pWidth, float pHeight);
 
 #endif //REC2_DISPLAYS_H
