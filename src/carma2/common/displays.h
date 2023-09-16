61fe30b055801c70b1277b4a3b1cc67f2eb92d0c
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-16 21:22:21 +0200

    Implement DRFontToPolyFontHandle

--- src/carma2/common/displays.h
+++ src/carma2/common/displays.h
@@ -15,6 +15,8 @@ void C2_HOOK_FASTCALL DRPixelmapCentredText(br_pixelmap* pPixelmap, int pX, int 
 
 void C2_HOOK_FASTCALL OoerrIveGotTextInMeBoxMissus(int pFont_index, const char* pText, br_pixelmap* pPixelmap, int pLeft, int pTop, int pRight, int pBottom, int pCentred);
 
+int C2_HOOK_FASTCALL DRFontToPolyFontHandle(const tDR_font* pFont);
+
 void C2_HOOK_FASTCALL TransDRPixelmapText(br_pixelmap* pPixelmap, int pX, int pY, const tDR_font* pFont, const char* pText, int pRight_edge);
 
 void C2_HOOK_FASTCALL InitHeadups(void);
