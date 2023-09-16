5c1772f1f6982c5fc46e20b005d0f3f44a907fce
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-17 00:23:07 +0200

    Implement ControlCar4

--- src/carma2/common/car.c
+++ src/carma2/common/car.c
@@ -3,7 +3,10 @@
 #include "finteray.h"
 #include "world.h"
 
+#include "brender/brender.h"
 #include "brender/br_inline_funcs.h"
+#include "rec2_macros.h"
+#include "rec2_types.h"
 
 #include "c2_string.h"
 
@@ -86,3 +89,57 @@ void C2_HOOK_FASTCALL ResetCarSpecialVolume(tCollision_info* pCollision_info) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004ff530, ResetCarSpecialVolume, ResetCarSpecialVolume_original)
+
+void C2_HOOK_FAKE_THISCALL ControlCar4(tCar_spec* c, undefined4 pArg2, br_scalar dt) {
+
+    REC2_THISCALL_UNUSED(pArg2);
+
+    if (c->keys.left) {
+        if (c->turn_speed < 0.f) {
+            c->turn_speed = 0.f;
+        }
+        if (c->collision_info->velocity_car_space.v[2] > 0.f) {
+            c->turn_speed += .25f * dt;
+        } else if ((c->curvature >= 0.f && c->collision_info->omega.v[1] >= -.001f) || c->turn_speed != 0.f) {
+            c->turn_speed += 25.f * dt * (0.05f / (5.f + WORLD_SCALE * BrVector3Length(&c->collision_info->v) )) * .25f;
+        } else {
+            c->turn_speed = 25.f * dt * (.05f / (5.f + WORLD_SCALE * BrVector3Length(&c->collision_info->v)));
+            if (c->collision_info->omega.v[1] < -.01f) {
+                c->turn_speed -= .25f * dt * c->collision_info->omega.v[1];
+            }
+        }
+    }
+    if (c->keys.right) {
+        if (c->turn_speed > 0.f) {
+            c->turn_speed = 0.f;
+        }
+        if (c->collision_info->velocity_car_space.v[2] > 0.f) {
+            c->turn_speed -= .25f * dt;
+        } else if ((c->curvature <= 0.f && c->collision_info->omega.v[1] <= .001f) || c->turn_speed != 0.f) {
+            c->turn_speed -= 25.f * dt * (.05f / (5.f + WORLD_SCALE * BrVector3Length(&c->collision_info->v))) * .25f;
+        } else {
+            c->turn_speed = -25.f * dt * (.05f / (5.f + WORLD_SCALE * BrVector3Length(&c->collision_info->v)));
+            if (c->collision_info->omega.v[1] < -.01f) {
+                c->turn_speed -= .25f * dt * c->collision_info->omega.v[1];
+            }
+        }
+    }
+    if (!c->keys.left && !c->keys.right) {
+        c->turn_speed = 0.f;
+    } else if (fabsf(c->turn_speed) < fabsf(2.f * dt * c->curvature) && c->curvature * c->turn_speed < 0.f) {
+        c->turn_speed = -(2.f * dt * c->curvature);
+    }
+    c->curvature += c->turn_speed;
+    if (c->joystick.left > 0) {
+        c->curvature = (float)c->joystick.left * (float)c->joystick.left / 4294967300.f * c->maxcurve;
+    } else if (c->joystick.right >= 0) {
+        c->curvature = -((float)c->joystick.right * (float)c->joystick.right / 4294967300.f) * c->maxcurve;
+    }
+    if (c->curvature > c->maxcurve) {
+        c->curvature = c->maxcurve;
+    }
+    if (c->curvature < -c->maxcurve) {
+        c->curvature = -c->maxcurve;
+    }
+}
+C2_HOOK_FUNCTION(0x004175e0, ControlCar4)
