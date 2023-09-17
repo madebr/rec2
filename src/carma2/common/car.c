c825f34ce6a1bbcf1f69c446da09cae02eac8d90
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-17 02:58:36 +0200

    Hook FlyCar

--- src/carma2/common/car.c
+++ src/carma2/common/car.c
@@ -34,6 +34,19 @@ int C2_HOOK_FASTCALL CollideCamera2(br_vector3* car_pos, br_vector3* cam_pos, br
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004122b0, CollideCamera2, CollideCamera2_original)
 
+void (C2_HOOK_FAKE_THISCALL * FlyCar_original)(tCar_spec* c, undefined4 pArg2, br_scalar dt);
+void C2_HOOK_FAKE_THISCALL FlyCar(tCar_spec* c, undefined4 pArg2, br_scalar dt) {
+
+    REC2_THISCALL_UNUSED(pArg2);
+
+#if defined(C2_HOOKS_ENABLED)
+    FlyCar_original(c, pArg2, dt);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x0041f4f0, FlyCar, FlyCar_original)
+
 void (C2_HOOK_FASTCALL * PanningExternalCamera_original)(tCar_spec* c, tU32 pTime);
 void C2_HOOK_FASTCALL PanningExternalCamera(tCar_spec* c, tU32 pTime) {
 
