cc00b4165b425868bb865260027aef5667790d84
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-10-29 01:25:52 +0200

    Implement GetNewBoundingBox

--- src/carma2/common/finteray.h
+++ src/carma2/common/finteray.h
@@ -9,4 +9,6 @@ C2_HOOK_VARIABLE_DECLARE(int, gPling_materials);
 
 void C2_HOOK_FASTCALL MultiRayCheckSingleFace(int pNum_rays, tFace_ref* pFace, br_vector3* ray_pos, br_vector3* ray_dir, br_vector3* normal, br_scalar* rt);
 
+void C2_HOOK_FASTCALL GetNewBoundingBox(br_bounds* b2, br_bounds* b1, br_matrix34* m);
+
 #endif
