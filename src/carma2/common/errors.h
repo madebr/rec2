2160e6593cd7058f847b82110ded98a8f8cc4098
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-10-28 23:21:13 +0200

    Use hooked gError_messages

--- src/carma2/common/errors.h
+++ src/carma2/common/errors.h
@@ -5,13 +5,12 @@
 
 #include "c2_hooks.h"
 
-C2_HOOK_VARIABLE_DECLARE(char*, gError_messages);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(const char*, gError_messages, 186);
 C2_HOOK_VARIABLE_DECLARE(int, gError_code);
 
 #if defined(C2_HOOKS_ENABLED)
 #else
 
-extern char* gError_messages[126];
 //extern char* gPalette_copy__errors; // suffix added to avoid duplicate symbol
 //extern int gPixel_buffer_size__errors; // suffix added to avoid duplicate symbol
 //extern int gMouse_was_started__errors; // suffix added to avoid duplicate symbol
