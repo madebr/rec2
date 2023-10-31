35a432caa1a57dce137e5c11fd4ab0e4fd81b64b
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-01 00:55:36 +0100

    Implement InitOpponentsAndDrones

--- src/carma2/include/rec2_types.h
+++ src/carma2/include/rec2_types.h
@@ -2945,7 +2945,9 @@ typedef struct {
     struct br_matrix34 initial_position;
     struct tCar_spec * car;
     undefined field_0xcc[8];
-} tNet_game_player_info;typedef enum {
+} tNet_game_player_info;
+
+typedef enum {
     kDroneType_car = 0,
     kDroneType_plane = 1,
     kDroneType_train = 2,
@@ -2954,7 +2956,7 @@ typedef struct {
 
 typedef struct {
     char name[13];
-    undefined field1_0xd[3];
+    undefined field_0xd[3];
     tDrone_type type;
     float M;
     float speed;
