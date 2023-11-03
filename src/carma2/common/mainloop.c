f932237dc119a072a66cbfd8f80259b9b561e006
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-03 02:58:51 +0100

    Implement ProcessAICars

---
+++ src/carma2/common/mainloop.c
@@ -0,0 +1,27 @@
+#include "mainloop.h"
+
+#include "drone.h"
+#include "loading.h"
+#include "opponent.h"
+#include "powerups.h"
+#include "timers.h"
+#include "utility.h"
+
+void C2_HOOK_FASTCALL ProcessAICars(tU32 pFrame_period) {
+
+    C2V(gTime_stamp_for_this_munging) = GetTotalTime();
+    if (C2V(gTime_stamp_for_this_munging) == 0) {
+        C2V(gTime_stamp_for_this_munging) = PDGetTotalTime();
+    }
+    C2V(gDrone_delta_time) = (float)pFrame_period / 1000.f;
+    C2V(gFrame_period_for_this_munging) = pFrame_period;
+    Timers_Push(TIMER_DRN);
+    ProcessDrones();
+    Timers_Pop(TIMER_DRN);
+    Timers_Push(TIMER_OPP);
+    MungeOpponents();
+    Timers_Pop(TIMER_OPP);
+    C2V(gFirst_drone_processing) = 0;
+    ProcessShitMines(C2V(gTime_stamp_for_this_munging));
+}
+C2_HOOK_FUNCTION(0x00401170, ProcessAICars)
