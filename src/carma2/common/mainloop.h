af57339e0aa96a98edec13cd163226dc19963d82
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-03 02:58:51 +0100

    Implement ProcessAICars

---
+++ src/carma2/common/mainloop.h
@@ -0,0 +1,10 @@
+#ifndef REC2_MAINLOOP_H
+#define REC2_MAINLOOP_H
+
+#include "c2_hooks.h"
+
+#include "rec2_types.h"
+
+void C2_HOOK_FASTCALL ProcessAICars(tU32 pTime);
+
+#endif // REC2_MAINLOOP_H
