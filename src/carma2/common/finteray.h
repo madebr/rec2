c90eb7276b9603cab83f8f30193d2027a0368f08
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-04 15:46:20 +0100

    Implement ActorRayPick2D

--- src/carma2/common/finteray.h
+++ src/carma2/common/finteray.h
@@ -7,6 +7,7 @@
 
 C2_HOOK_VARIABLE_DECLARE(int, gPling_materials);
 C2_HOOK_VARIABLE_DECLARE(int, gTemp_group);
+C2_HOOK_VARIABLE_DECLARE(br_scalar, gNearest_T);
 
 void C2_HOOK_FASTCALL MultiRayCheckSingleFace(int pNum_rays, tFace_ref* pFace, br_vector3* ray_pos, br_vector3* ray_dir, br_vector3* normal, br_scalar* rt);
 
@@ -17,5 +18,7 @@ int C2_HOOK_FASTCALL PickBoundsTestRay__finteray(br_bounds* b, br_vector3* rp, b
 void C2_HOOK_FASTCALL DRVector2AccumulateScale__finteray(br_vector2* a, const br_vector2* b, br_scalar s);
 
 int C2_HOOK_FASTCALL DRModelPick2D__finteray(br_model* model, br_material* material, br_actor* actor, br_vector3* ray_pos, br_vector3* ray_dir, br_scalar t_near, br_scalar t_far, dr_modelpick2d_cbfn* callback, void* arg);
+
+int C2_HOOK_FASTCALL ActorRayPick2D(br_actor* ap, br_vector3* pPosition, br_vector3* pDir, br_model* model, br_material* material, dr_pick2d_cbfn* callback);
 
 #endif
