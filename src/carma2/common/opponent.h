9e398938b46044f4034f115518fa4b98d417b140
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-04 17:06:59 +0100

    Implement PointActorAlongThisBloodyVector

--- src/carma2/common/opponent.h
+++ src/carma2/common/opponent.h
@@ -24,6 +24,8 @@ C2_HOOK_VARIABLE_DECLARE(float, gOpponent_nastyness_frigger);
 C2_HOOK_VARIABLE_DECLARE(br_scalar, gIn_view_distance);
 C2_HOOK_VARIABLE_DECLARE(int, gChallenger_index__opponent);
 
+void C2_HOOK_FASTCALL PointActorAlongThisBloodyVector(br_actor* pThe_actor, br_vector3* pThe_vector);
+
 void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index);
 
 void C2_HOOK_FASTCALL LoadInOppoPaths(FILE* pF);
