66207892cb6781a58087040f62cb4bc2dd3bfe5f
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-04 15:32:05 +0100

    Implement DRModelPick2D__finteray

--- src/carma2/common/finteray.h
+++ src/carma2/common/finteray.h
@@ -6,11 +6,16 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(int, gPling_materials);
+C2_HOOK_VARIABLE_DECLARE(int, gTemp_group);
 
 void C2_HOOK_FASTCALL MultiRayCheckSingleFace(int pNum_rays, tFace_ref* pFace, br_vector3* ray_pos, br_vector3* ray_dir, br_vector3* normal, br_scalar* rt);
 
 void C2_HOOK_FASTCALL GetNewBoundingBox(br_bounds* b2, br_bounds* b1, br_matrix34* m);
 
 int C2_HOOK_FASTCALL PickBoundsTestRay__finteray(br_bounds* b, br_vector3* rp, br_vector3* rd, br_scalar t_near, br_scalar t_far, br_scalar* new_t_near, br_scalar* new_t_far);
+
+void C2_HOOK_FASTCALL DRVector2AccumulateScale__finteray(br_vector2* a, const br_vector2* b, br_scalar s);
+
+int C2_HOOK_FASTCALL DRModelPick2D__finteray(br_model* model, br_material* material, br_actor* actor, br_vector3* ray_pos, br_vector3* ray_dir, br_scalar t_near, br_scalar t_far, dr_modelpick2d_cbfn* callback, void* arg);
 
 #endif
