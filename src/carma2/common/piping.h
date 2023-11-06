7252a5d18fbf973fa7327d0963fd4166e955af21
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 00:59:46 +0100

    Implement DisposeReplay

--- src/carma2/common/piping.h
+++ src/carma2/common/piping.h
@@ -3,6 +3,12 @@
 
 #include "c2_hooks.h"
 
+#include "rec2_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(tPipe_smudge_data*, gSmudge_space);
+
 void C2_HOOK_FASTCALL DisposePiping(void);
+
+void C2_HOOK_FASTCALL DisposeReplay(void);
 
 #endif //REC2_PIPING_H
