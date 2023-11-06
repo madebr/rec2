9169668c1a93f2700e0ace4eb0c68e33ddc34c3d
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-06 20:09:19 +0100

    Implement RestoreStashedSaveGame

--- src/carma2/common/loadsave.c
+++ src/carma2/common/loadsave.c
@@ -1,5 +1,21 @@
 #include "loadsave.h"
 
+#include "globvars.h"
+#include "loading.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT(int, gValid_stashed_save_game, 0x00688ae4);
+C2_HOOK_VARIABLE_IMPLEMENT(tSave_game, gStashed_save_game, 0x00688780);
+
+void C2_HOOK_FASTCALL RestoreStashedSaveGame(void) {
+
+    if (C2V(gValid_stashed_save_game)) {
+        LoadRaces(C2V(gRace_list), &C2V(gNumber_of_races), -1);
+        LoadTheGame(&C2V(gStashed_save_game));
+        C2V(gValid_stashed_save_game) = 0;
+    }
+}
+C2_HOOK_FUNCTION(0x00466e90, RestoreStashedSaveGame)
+
 int (C2_HOOK_FASTCALL * LoadTheGame_original)(tSave_game* pSave_game);
 int C2_HOOK_FASTCALL LoadTheGame(tSave_game* pSave_game) {
 
