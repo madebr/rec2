7d38f7c0527716fbb174daf3772ae872103c79f7
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 00:59:46 +0100

    Implement DisposeReplay

--- src/carma2/common/piping.c
+++ src/carma2/common/piping.c
@@ -1,5 +1,9 @@
 #include "piping.h"
 
+#include <brender/brender.h>
+
+C2_HOOK_VARIABLE_IMPLEMENT(tPipe_smudge_data*, gSmudge_space, 0x00694104);
+
 void (C2_HOOK_FASTCALL * DisposePiping_original)(void);
 void C2_HOOK_FASTCALL DisposePiping(void) {
 
@@ -10,3 +14,13 @@ void C2_HOOK_FASTCALL DisposePiping(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004024a0, DisposePiping, DisposePiping_original)
+
+void C2_HOOK_FASTCALL DisposeReplay(void) {
+
+    DisposePiping();
+    if (C2V(gSmudge_space) != NULL) {
+        BrMemFree(C2V(gSmudge_space));
+        C2V(gSmudge_space) = NULL;
+    }
+}
+C2_HOOK_FUNCTION(0x004c6c60, DisposeReplay)
