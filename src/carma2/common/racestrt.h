71d1cba017cc60175f4376053848ea9d73a95878
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-06 23:25:58 +0100

    Implement SortGridFunction

---
+++ src/carma2/common/racestrt.h
@@ -0,0 +1,8 @@
+#ifndef REC2_RACESTRT_H
+#define REC2_RACESTRT_H
+
+#include "c2_hooks.h"
+
+int C2_HOOK_CDECL SortGridFunction(const void* pFirst_one, const void* pSecond_one);
+
+#endif //REC2_RACESTRT_H
