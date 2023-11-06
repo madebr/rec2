05a1e1380fc7722f69bc5e20e03e0a03ecd66ca5
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-06 21:57:22 +0100

    Implement SelectOpponents

--- src/carma2/common/structur.h
+++ src/carma2/common/structur.h
@@ -3,10 +3,14 @@
 
 #include "c2_hooks.h"
 
+#include "rec2_types.h"
+
 void C2_HOOK_FASTCALL DoProgram(void);
 
 int C2_HOOK_FASTCALL ChooseOpponent(int pNastiness);
 
 int C2_HOOK_FASTCALL NumberOfOpponentsLeft(void);
+
+void C2_HOOK_FASTCALL SelectOpponents(tRace_info* pRace_info);
 
 #endif // REC2_STRUCTUR_H
