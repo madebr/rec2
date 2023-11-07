c7afb7148f6cbf6b9dd1ef258b64b9260b8791dd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-08 00:04:00 +0100

    Implement InitialiseCar

--- src/carma2/common/car.h
+++ src/carma2/common/car.h
@@ -31,6 +31,8 @@ void C2_HOOK_FASTCALL SetInitialPosition(tRace_info* pThe_race, int pCar_index, 
 
 void C2_HOOK_FASTCALL InitialiseCar2(tCar_spec* pCar, int pClear_disabled_flag);
 
+void C2_HOOK_FASTCALL InitialiseCar(tCar_spec* pCar);
+
 void C2_HOOK_FASTCALL SetInitialPositions(tRace_info* pThe_race);
 
 #endif //REC2_CAR_H
