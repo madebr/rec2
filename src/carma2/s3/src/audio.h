e3cce6fb8207c72220d909680b9f0419cd5c7d76
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 03:52:07 +0100

    Implement S3ReleaseSoundSource

--- src/carma2/s3/src/audio.h
+++ src/carma2/s3/src/audio.h
@@ -4,9 +4,13 @@
 #include <s3/s3.h>
 
 C2_HOOK_VARIABLE_DECLARE(int, gS3_enabled);
+C2_HOOK_VARIABLE_DECLARE(tS3_sound_source*, gS3_sound_sources);
+C2_HOOK_VARIABLE_DECLARE(int, gS3_nsound_sources);
 
 int C2_HOOK_FASTCALL S3StopChannel(tS3_channel* chan);
 
 int C2_HOOK_FASTCALL S3StopOutletSound(tS3_outlet* pOutlet);
+
+int C2_HOOK_FASTCALL S3ReleaseSoundSource(tS3_sound_source* src);
 
 #endif // S3_AUDIO_H
