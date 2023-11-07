816503799b490303fc2077fdfb039aaed682a858
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 04:24:10 +0100

    Hook DisposePedestrians

--- src/carma2/common/pedestrn.h
+++ src/carma2/common/pedestrn.h
@@ -30,4 +30,6 @@ void C2_HOOK_FASTCALL MaybeSpawnPedestrian(br_face *pFace, br_model *pModel);
 
 void C2_HOOK_FASTCALL AllocateTrackPedestrians(void);
 
+void C2_HOOK_FASTCALL DisposePedestrians(void);
+
 #endif //REC2_PEDESTRN_H
