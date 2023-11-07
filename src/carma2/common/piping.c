6b91077da5cebe14ca247e8f63d25bbcedf78878
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-08 00:44:00 +0100

    Implement InitLastDamageArrayEtc

--- src/carma2/common/piping.c
+++ src/carma2/common/piping.c
@@ -1,6 +1,11 @@
 #include "piping.h"
 
+#include "globvars.h"
+#include "opponent.h"
+
 #include <brender/brender.h>
+
+#include "rec2_macros.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT(tPipe_smudge_data*, gSmudge_space, 0x00694104);
 
@@ -24,3 +29,35 @@ void C2_HOOK_FASTCALL DisposeReplay(void) {
     }
 }
 C2_HOOK_FUNCTION(0x004c6c60, DisposeReplay)
+
+void C2_HOOK_FASTCALL InitLastDamageArrayEtc(void) {
+    int i;
+    int j;
+    int cat;
+    int car_count;
+    tCar_spec* car;
+
+    C2_HOOK_BUG_ON(REC2_ASIZE(car->frame_start_damage) != 3);
+
+    for (cat = eVehicle_self; cat <= eVehicle_not_really; cat++) {
+        if (cat == eVehicle_self) {
+            car_count = 1;
+        } else {
+            car_count = GetCarCount(cat);
+        }
+        for (i = 0; i < car_count; i++) {
+            if (cat == eVehicle_self) {
+                car = &C2V(gProgram_state).current_car;
+            } else {
+                car = GetCarSpec(cat, i);
+            }
+            if (cat != eVehicle_not_really) {
+                for (j = 0; j < REC2_ASIZE(car->frame_start_damage); j++) {
+                    car->frame_start_damage[j] = 0;
+                }
+            }
+            car->car_ID = (cat << 8) | i;
+        }
+    }
+}
+C2_HOOK_FUNCTION(0x004c6580, InitLastDamageArrayEtc)
