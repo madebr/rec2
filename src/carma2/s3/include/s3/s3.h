ea8e8b8e9a31e5771fcfd83fb13a60b5fe5d31be
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 03:58:01 +0100

    Implement S3StopOutletSound

--- src/carma2/s3/include/s3/s3.h
+++ src/carma2/s3/include/s3/s3.h
@@ -3,6 +3,8 @@
 
 #include "c2_hooks.h"
 
+#include <stdlib.h> /* uintptr_t */
+
 // External typedefs
 
 typedef float tF32;
@@ -20,8 +22,16 @@ typedef struct tS3_descriptor tS3_descriptor;
 typedef struct tS3_outlet tS3_outlet;
 typedef struct tS3_sound_source tS3_sound_source;
 
+/* FIXME: verify elements of tS3_termination_reason */
+typedef enum {
+    eS3_tr_natural = 0,
+    eS3_tr_stopped = 1,
+    eS3_tr_overridden = 2,
+} tS3_termination_reason;
 
-typedef void tS3_sample_filter(tS3_effect_tag, tS3_sound_tag);
+// FIXME: add call convention!!
+typedef void ( * tS3_outlet_callback)(tS3_outlet*, tS3_sound_tag, tS3_termination_reason);
+typedef void ( * tS3_sample_filter)(tS3_effect_tag, tS3_sound_tag);
 
 typedef struct tS3_vector3 {
     tF32 x;
@@ -29,5 +39,44 @@ typedef struct tS3_vector3 {
     tF32 z;
 } tS3_vector3;
 
+typedef struct tS3_channel {
+    int active;
+    tS3_termination_reason termination_reason;
+    int needs_service;
+    int repetitions;
+    int rate;
+    int spatial_sound;
+    uintptr_t mciDevice; /* MCIDEVICE */
+    tS3_sound_tag tag;
+    tS3_type type;
+    int volume_multiplier;
+    float field_0x28;
+    tS3_vector3 position;
+    tS3_vector3 velocity;
+    tS3_vector3 lastpos;
+    int fieldd_0x50;
+    undefined4 field_0x54;
+    float field_0x58;
+    tS3_outlet* owner_outlet;
+    tS3_descriptor* descriptor;
+    tS3_channel* next;
+    char* type_struct_sample;
+    char* type_struct_midi;
+    char* type_struct_cda;
+    tS3_sound_source* sound_source_ptr;
+} tS3_channel;
+
+typedef struct tS3_outlet {
+    int id;
+    int max_channels;
+    int volume;
+    int independent_pitch;
+    tS3_channel* channel_list;
+    tS3_outlet* prev;
+    tS3_outlet* next;
+    tS3_outlet_callback* callback;
+} tS3_outlet;
+
+int C2_HOOK_FASTCALL S3StopOutletSound(tS3_outlet* pOutlet);
 
 #endif
