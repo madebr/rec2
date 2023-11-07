2150c0929073df17e8325a77c375e3780fcca727
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 03:17:45 +0100

    Hook S3StopChannel

--- src/carma2/s3/include/s3/s3.h
+++ src/carma2/s3/include/s3/s3.h
@@ -1,10 +1,11 @@
 #ifndef S3_SOUND_H
 #define S3_SOUND_H
 
+#include "c2_hooks.h"
+
 // External typedefs
 
 typedef float tF32;
-typedef char* tS3_sound_source_ptr;
 typedef int tS3_sound_tag;
 typedef int tS3_sound_id;
 typedef int tS3_type;
@@ -14,7 +15,11 @@ typedef int tS3_effect_tag;
 typedef long tS3_priority;
 typedef long tS3_pitch;
 typedef long tS3_speed;
-typedef char* tS3_outlet_ptr;
+typedef struct tS3_channel tS3_channel;
+typedef struct tS3_descriptor tS3_descriptor;
+typedef struct tS3_outlet tS3_outlet;
+typedef struct tS3_sound_source tS3_sound_source;
+
 
 typedef void tS3_sample_filter(tS3_effect_tag, tS3_sound_tag);
 
@@ -24,40 +29,5 @@ typedef struct tS3_vector3 {
     tF32 z;
 } tS3_vector3;
 
-int S3Init(char* path, int low_memory_mode);
-
-void S3Disable();
-void S3Enable();
-
-void S3Set3DSoundEnvironment(float a1, float a2, float a3);
-tS3_sound_source_ptr S3CreateSoundSourceBR(br_vector3* pPosition, br_vector3* pVelocity, tS3_outlet_ptr pBound_outlet);
-int S3BindAmbientSoundToOutlet(tS3_outlet_ptr pOutlet, int pSound, tS3_sound_source_ptr source, float pMax_distance, int pPeriod, int pRepeats, int pVolume, int pPitch, int pSpeed);
-
-tS3_sound_tag S3StartSound3D(tS3_outlet_ptr pOutlet, tS3_sound_id pSound, tS3_vector3* pInitial_position, tS3_vector3* pInitial_velocity, tS3_repeats pRepeats, tS3_volume pVolume, tS3_pitch pPitch, tS3_speed pSpeed);
-
-tS3_outlet_ptr S3CreateOutlet(int unk1, int pChannel_count);
-void S3DisposeOutlet(tS3_outlet_ptr outlet);
-int S3DisposeDescriptor(tS3_sound_id id);
-int S3DisposeSoundSource(tS3_sound_source_ptr src);
-
-int S3SetTagVolume(tS3_sound_tag pTag, tS3_volume pVolume);
-
-void S3Service(int inside_cockpit, int unk1);
-
-int S3LoadSample(tS3_sound_id id);
-tS3_sound_tag S3StartSound(tS3_outlet_ptr pOutlet, tS3_sound_id pSound);
-tS3_sound_tag S3StartSound2(tS3_outlet_ptr pOutlet, tS3_sound_id pSound, tS3_repeats pRepeats, tS3_volume pLVolume, tS3_volume pRVolume, tS3_pitch pPitch, tS3_speed pSpeed);
-void S3StopAllOutletSounds();
-int S3SoundStillPlaying(tS3_sound_tag pSound);
-int S3StopSound(tS3_sound_tag pSound_tag);
-int S3StopOutletSound(tS3_outlet_ptr pOutlet);
-int S3SetOutletVolume(tS3_outlet_ptr pOutlet, tS3_volume pVolume);
-void S3UpdateSoundSource(tS3_outlet_ptr outlet, tS3_sound_tag tag, tS3_sound_source_ptr src, float pMax_distance_squared, int pPeriod, tS3_repeats pAmbient_repeats, tS3_volume pVolume, int pPitch, tS3_speed pSpeed);
-
-void S3BindListenerPositionBRender(br_vector3* pos);
-void S3BindListenerVelocityBRender(br_vector3* vel);
-void S3BindListenerLeftBRender(br_vector3* left);
-
-int S3IsCDAPlaying2();
 
 #endif
