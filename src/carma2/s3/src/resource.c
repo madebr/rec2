3bfe1a9b4ecbc5d73abcb68b0db26919400218a1
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 03:43:52 +0100

    Implement S3MemFree

--- src/carma2/s3/src/resource.c
+++ src/carma2/s3/src/resource.c
@@ -5,3 +5,9 @@ void* C2_HOOK_FASTCALL S3MemAllocate(br_size_t size, br_uint_8 type) {
     return BrMemAllocate(size, type);
 }
 C2_HOOK_FUNCTION(0x00565af7, S3MemAllocate)
+
+void C2_HOOK_FASTCALL S3MemFree(void* p) {
+
+    BrMemFree(p);
+}
+C2_HOOK_FUNCTION(0x00565b1d, S3MemFree)
