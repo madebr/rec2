a5c5a971549a15ed7328275fef803d31fa634aab
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 04:14:31 +0100

    Hook S3Service

--- src/carma2/s3/include/s3/s3.h
+++ src/carma2/s3/include/s3/s3.h
@@ -107,4 +107,6 @@ int C2_HOOK_FASTCALL S3StopOutletSound(tS3_outlet* pOutlet);
 
 int C2_HOOK_FASTCALL S3ReleaseSoundSource(tS3_sound_source* src);
 
+void C2_HOOK_FASTCALL S3Service(int inside_cockpit, int unk1);
+
 #endif
