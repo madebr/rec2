e52f672a53f27ca4a4fd303881a05d7c52a94aa2
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-07 04:24:10 +0100

    Hook DisposePedestrians

--- src/carma2/common/pedestrn.c
+++ src/carma2/common/pedestrn.c
@@ -617,3 +617,14 @@ void C2_HOOK_FASTCALL AllocateTrackPedestrians(void) {
     }
 }
 C2_HOOK_FUNCTION(0x004d3520, AllocateTrackPedestrians)
+
+void (C2_HOOK_FASTCALL * DisposePedestrians_original)(void);
+void C2_HOOK_FASTCALL DisposePedestrians(void) {
+
+#if defined(C2_HOOKS_ENABLED)
+    DisposePedestrians_original();
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004cb8d0, DisposePedestrians, DisposePedestrians_original)
