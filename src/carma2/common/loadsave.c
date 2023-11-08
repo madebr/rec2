cf73f5c5d5ee4a67deaf08652402a16ee399aebd
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-11-08 03:34:17 +0100

    Hook WriteSaveGame

--- src/carma2/common/loadsave.c
+++ src/carma2/common/loadsave.c
@@ -37,3 +37,14 @@ void C2_HOOK_FASTCALL LoadLastSaveGame(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00491e50, LoadLastSaveGame, LoadLastSaveGame_original)
+
+void (C2_HOOK_FASTCALL * WriteSaveGame_original)(void);
+void C2_HOOK_FASTCALL WriteSaveGame(void) {
+
+#if defined(C2_HOOKS_ENABLED)
+    WriteSaveGame_original();
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00491ac0, WriteSaveGame, WriteSaveGame_original)
