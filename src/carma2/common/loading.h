071a0c8cb3e4f31a5e849ebfb5d39043d9ce4133
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-15 21:58:04 +0100

    Implement DRForEveryArchivedFile

--- src/carma2/common/loading.h
+++ src/carma2/common/loading.h
@@ -162,6 +162,10 @@ void C2_HOOK_FASTCALL TWT_Unmount(tTWTVFS twt);
 
 FILE* C2_HOOK_FASTCALL TWT_fopen(const char* pPath, const char* mode);
 
+void C2_HOOK_FASTCALL DRForEveryArchivedFile(const char* pThe_path, const char* pArchive_name, tPDForEveryFileRecurse_cbfn pAction_routine);
+
+void C2_HOOK_FASTCALL DRForEveryFile(const char* pThe_path, tPDForEveryFileRecurse_cbfn pAction_routine);
+
 tTWTVFS C2_HOOK_FASTCALL TWT_MountEx(const char* path);
 
 void C2_HOOK_FASTCALL TWT_UnmountEx(tTWTVFS twt);
