309aec47c464f9c929061dfa9c8b2136fc2a0961
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-15 14:35:52 +0100

    Implement ReadExplosionInfo

--- src/carma2/common/explosions.h
+++ src/carma2/common/explosions.h
@@ -1,8 +1,13 @@
 #ifndef REC2_EXPLOSIONS_H
 #define REC2_EXPLOSIONS_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
+#include "c2_stdio.h"
 
 void C2_HOOK_FASTCALL InitExplosions(void);
+
+void C2_HOOK_FASTCALL ReadExplosionInfo(FILE* pF, int* pChance_explosion, int* pExplosion_sound_id, tExplosion_animation* pExplosion_groups);
 
 #endif // REC2_EXPLOSIONS_H
