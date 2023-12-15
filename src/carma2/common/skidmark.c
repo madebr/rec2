5c6e3fba1b1f60f1892069fdced4647151c49a67
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-15 15:11:31 +0100

    Hook ReadSlick

--- src/carma2/common/skidmark.c
+++ src/carma2/common/skidmark.c
@@ -10,3 +10,14 @@ void C2_HOOK_FASTCALL InitSkids(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004e9c40, InitSkids, InitSkids_original)
+
+void (C2_HOOK_FASTCALL * ReadSlick_original)(FILE* pF,tSlick_spec* pSlick_spec);
+void C2_HOOK_FASTCALL ReadSlick(FILE* pF, tSlick_spec* pSlick_spec) {
+
+#if defined(C2_HOOKS_ENABLED)
+    ReadSlick_original(pF, pSlick_spec);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004ee5e0, ReadSlick, ReadSlick_original)
--- src/carma2/common/skidmark.h
+++ src/carma2/common/skidmark.h
@@ -1,8 +1,13 @@
 #ifndef REC2_SKIDMARK_H
 #define REC2_SKIDMARK_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
+#include "c2_stdio.h"
 
 void C2_HOOK_FASTCALL InitSkids(void);
+
+void C2_HOOK_FASTCALL ReadSlick(FILE *pF,tSlick_spec *pSlick_spec);
 
 #endif // REC2_SKIDMARK_H
