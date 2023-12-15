718d02fb0387f50ef1a9a9b23e2d0a0b491eeb44
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-15 22:00:01 +0100

    Implement DRPixelmapLoadMany

--- src/carma2/common/loading.h
+++ src/carma2/common/loading.h
@@ -214,5 +214,7 @@ int C2_HOOK_FASTCALL TestForOriginalCarmaCDinDrive(void);
 
 br_pixelmap* C2_HOOK_FASTCALL DRPixelmapLoad(char* pFile_name);
 
+br_uint_32 C2_HOOK_FASTCALL DRPixelmapLoadMany(const char* pFile_name, br_pixelmap** pPixelmaps, br_uint_16 pNum);
+
 #endif //REC2_LOADING_H
 
