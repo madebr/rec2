dc6119e85acfeb5e20d54a276c7740ec01d5b389
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-16 00:47:13 +0100

    Implement ReadSmashableInitialPosition

--- src/carma2/common/smashing.h
+++ src/carma2/common/smashing.h
@@ -5,9 +5,13 @@
 
 #include "c2_hooks.h"
 
+#include "c2_stdio.h"
+
 C2_HOOK_VARIABLE_DECLARE_ARRAY(tSmash_vertex, gSmash_vertices, 200);
 C2_HOOK_VARIABLE_DECLARE_ARRAY(tSmash_quad, gSmash_quads, 50);
 
 void C2_HOOK_FASTCALL InitSmashing(void);
+
+void C2_HOOK_FASTCALL ReadSmashableInitialPosition(FILE* pFile, tSmashable_initial_position_spec*  pInitial_pos);
 
 #endif //REC2_DEPTH_H
