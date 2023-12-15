e686e0ad5b4b6f3b31b4abd2cf3605c72bfb5413
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-16 00:47:13 +0100

    Implement ReadSmashableInitialPosition

--- src/carma2/common/smashing.c
+++ src/carma2/common/smashing.c
@@ -1,9 +1,24 @@
 #include "smashing.h"
 
+#include "loading.h"
+
 #include <brender/brender.h>
+
+#include "rec2_macros.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tSmash_vertex, gSmash_vertices, 200, 0x006b78e0);
 C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tSmash_quad, gSmash_quads, 50, 0x006a80f8);
+
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY_INIT(const char*, gInitial_smashable_position_type_names, 3, 0x0065fed0, {
+    "sphereclumped",
+    "boxclumped",
+    "actorbased",
+});
+
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY_INIT(const char*, gInitial_position_sphere_where, 2, 0x0065fee0, {
+    "impact",
+    "model",
+});
 
 void (C2_HOOK_FASTCALL * InitSmashing_original)(void);
 void C2_HOOK_FASTCALL InitSmashing(void) {
@@ -44,3 +59,24 @@ void C2_HOOK_FASTCALL InitSmashing(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004efe00, InitSmashing, InitSmashing_original)
+
+void C2_HOOK_FASTCALL ReadSmashableInitialPosition(FILE* pFile, tSmashable_initial_position_spec*  pInitial_pos) {
+
+    /* Initial position type */
+    pInitial_pos->type = GetALineAndInterpretCommand(pFile, C2V(gInitial_smashable_position_type_names), REC2_ASIZE(C2V(gInitial_smashable_position_type_names)));
+
+    switch (pInitial_pos->type) {
+        case kInitialSmashablePosition_SphereClumped:
+            /* Sphere radius */
+            pInitial_pos->position.sphere.radius = GetAScalar(pFile);
+            /* Sphere centre */
+            pInitial_pos->position.sphere.where = GetALineAndInterpretCommand(pFile, C2V(gInitial_position_sphere_where), REC2_ASIZE(C2V(gInitial_position_sphere_where)));
+            break;
+        case kInitialSmashablePosition_BoxClumped:
+            GetThreeFloats(pFile, &pInitial_pos->position.box.v[0], &pInitial_pos->position.box.v[1], &pInitial_pos->position.box.v[2]);
+            break;
+        default:
+            break;
+    }
+}
+C2_HOOK_FUNCTION(0x004ee550, ReadSmashableInitialPosition)
