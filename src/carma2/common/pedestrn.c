cb7eb181fa804d429d32aff66ee02f2444513d8c
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-16 00:57:34 +0100

    Implement ScaleModel

--- src/carma2/common/pedestrn.c
+++ src/carma2/common/pedestrn.c
@@ -16,6 +16,20 @@ void C2_HOOK_FAKE_THISCALL ScaleModelXYZ(br_model* pModel, int pArg2, float pX, 
 }
 C2_HOOK_FUNCTION(0x00404290, ScaleModelXYZ)
 
+void C2_HOOK_FAKE_THISCALL ScaleModel(br_model* pModel, int pArg2, float pScale) {
+    int i;
+
+    REC2_THISCALL_UNUSED(pArg2);
+
+    for (i = 0; i < pModel->nvertices; i++) {
+        pModel->vertices[i].p.v[0] *= pScale;
+        pModel->vertices[i].p.v[1] *= pScale;
+        pModel->vertices[i].p.v[2] *= pScale;
+    }
+    BrModelUpdate(pModel, BR_MODU_NORMALS);
+}
+C2_HOOK_FUNCTION(0x004042f0, ScaleModel)
+
 void (C2_HOOK_CDECL * InitPeds_original)(void);
 void C2_HOOK_CDECL InitPeds(void) {
 #if defined(C2_HOOKS_ENABLED)
