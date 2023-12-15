b0b697c92d5c587f1cd1748ef0c2594cc79a0aad
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-15 14:40:15 +0100

    Implement DefaultNetSettings

---
+++ src/carma2/common/newgame.c
@@ -0,0 +1,33 @@
+#include "newgame.h"
+
+#include "globvars.h"
+#include "loading.h"
+#include "utility.h"
+
+#include "rec2_types.h"
+
+#include "rec2_macros.h"
+#include "c2_stdio.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tNet_game_options, gNet_settings, 9, 0x00763960);
+
+void C2_HOOK_FASTCALL DefaultNetSettings(void) {
+    FILE* file;
+    tNet_game_options* pOptions;
+    tPath_name the_path;
+    int i;
+
+    PathCat(the_path, C2V(gApplication_path), "NETDFLT.TXT");
+    file = DRfopen(the_path, "rt");
+    if (file == NULL) {
+        return;
+    }
+    ReadNetworkSettings(file, &C2V(gNet_settings)[0]);
+    DRrewind(file);
+    for (i = 1; i < REC2_ASIZE(C2V(gNet_settings)); i++) {
+        pOptions = &C2V(gNet_settings)[i];
+        ReadNetworkSettings(file, pOptions);
+    }
+    DRfclose(file);
+}
+C2_HOOK_FUNCTION(0x004663e0, DefaultNetSettings)
