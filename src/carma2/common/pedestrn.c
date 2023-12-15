c3bb2489adb08988a48d71f4a8a4e13b4374b92f
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-16 00:56:39 +0100

    Implement ScaleModelXYZ

--- src/carma2/common/pedestrn.c
+++ src/carma2/common/pedestrn.c
@@ -1,5 +1,21 @@
 #include "pedestrn.h"
 
+#include "rec2_macros.h"
+
+void C2_HOOK_FAKE_THISCALL ScaleModelXYZ(br_model* pModel, int pArg2, float pX, float pY, float pZ) {
+    int i;
+
+    REC2_THISCALL_UNUSED(pArg2);
+
+    for (i = 0; i < pModel->nvertices; i++) {
+        pModel->vertices[i].p.v[0] *= pX;
+        pModel->vertices[i].p.v[1] *= pY;
+        pModel->vertices[i].p.v[2] *= pZ;
+    }
+    BrModelUpdate(pModel, BR_MODU_NORMALS);
+}
+C2_HOOK_FUNCTION(0x00404290, ScaleModelXYZ)
+
 void (C2_HOOK_CDECL * InitPeds_original)(void);
 void C2_HOOK_CDECL InitPeds(void) {
 #if defined(C2_HOOKS_ENABLED)
--- src/carma2/common/pedestrn.h
+++ src/carma2/common/pedestrn.h
@@ -3,6 +3,10 @@
 
 #include "c2_hooks.h"
 
+#include <brender/brender.h>
+
+void C2_HOOK_FAKE_THISCALL ScaleModelXYZ(br_model* pModel, int pArg2, float pX, float pY, float pZ);
+
 void C2_HOOK_CDECL InitPeds(void);
 
 #endif //REC2_PEDESTRN_H
