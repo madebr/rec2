813aa26d36fbe3650601b08b3ff592a5c1c5ec94
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-15 14:34:14 +0100

    Implement ReadExplosionAnimation

---
+++ src/carma2/common/animation.h
@@ -0,0 +1,11 @@
+#ifndef REC2_ANIMATION_H
+#define REC2_ANIMATION_H
+
+#include "rec2_types.h"
+
+#include "c2_hooks.h"
+#include "c2_stdio.h"
+
+void C2_HOOK_FASTCALL ReadExplosionAnimation(FILE *pF, tExplosion_animation* pAnimation);
+
+#endif // REC2_ANIMATION_H
