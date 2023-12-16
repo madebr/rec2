e52b40bbb38ac98258dba31fb6cb8464fc1c1df5
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-16 14:51:28 +0100

    Hook NetPlayerStatusChanged

---
+++ src/carma2/common/network.h
@@ -0,0 +1,10 @@
+#ifndef REC2_NETWORK_H
+#define REC2_NETWORK_H
+
+#include "rec2_types.h"
+
+#include "c2_hooks.h"
+
+void C2_HOOK_FASTCALL NetPlayerStatusChanged(tPlayer_status pNew_status);
+
+#endif // REC2_NETWORK_H
