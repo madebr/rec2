87c3e0a6c13535cfa660f750f837e0f41c157a1f
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-16 14:48:31 +0100

    Implement DisableMaterialAdapt

--- src/carma2/common/temp.h
+++ src/carma2/common/temp.h
@@ -11,4 +11,6 @@ br_material* C2_HOOK_FASTCALL LoadTemporaryMaterial(const char* pName);
 
 void C2_HOOK_FASTCALL EnableMaterialAdapt(void);
 
+void C2_HOOK_FASTCALL DisableMaterialAdapt(void);
+
 #endif /* TEMP_H */
