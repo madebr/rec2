371f09fa9a866507e1ac81c046e1d1440f74d5db
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-18 20:35:06 +0100

    Hook CreateActorFromPowerupMap

--- src/carma2/common/powerups.c
+++ src/carma2/common/powerups.c
@@ -14,6 +14,17 @@ void C2_HOOK_FASTCALL InitPowerups(void) {
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004d9ea0, InitPowerups, InitPowerups_original)
 
+br_actor* (C2_HOOK_FASTCALL * CreateActorFromPowerupMap_original)(br_pixelmap* pMap);
+br_actor* C2_HOOK_FASTCALL CreateActorFromPowerupMap(br_pixelmap* pMap) {
+
+#if defined(C2_HOOKS_ENABLED)
+    return CreateActorFromPowerupMap_original(pMap);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004e09d0, CreateActorFromPowerupMap, CreateActorFromPowerupMap_original)
+
 void (C2_HOOK_FASTCALL * LoadPowerups_original)(void);
 void C2_HOOK_FASTCALL LoadPowerups(void) {
 
