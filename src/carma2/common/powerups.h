3ab672964bac9d58e543a4594020ade36244b2ed
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-18 20:35:06 +0100

    Hook CreateActorFromPowerupMap

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -9,6 +9,8 @@ C2_HOOK_VARIABLE_DECLARE_ARRAY(tHeadup_icon, gPickedup_powerups, 5);
 
 void C2_HOOK_FASTCALL InitPowerups(void);
 
+br_actor* C2_HOOK_FASTCALL CreateActorFromPowerupMap(br_pixelmap* pMap);
+
 void C2_HOOK_FASTCALL LoadPowerups(void);
 
 #endif //REC2_POWERUPS_H
