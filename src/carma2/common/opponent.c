6864165f96092c6d7cd685f4dccce2b505df1d97
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-22 04:58:59 +0100

    Implement PointVisibleFromHere

--- src/carma2/common/opponent.c
+++ src/carma2/common/opponent.c
@@ -1,5 +1,6 @@
 #include "opponent.h"
 
+#include "finteray.h"
 #include "globvars.h"
 
 void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index) {
@@ -18,3 +19,20 @@ void C2_HOOK_FASTCALL LoadInOppoPaths(FILE* pF) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004a9610, LoadInOppoPaths, LoadInOppoPaths_original)
+
+int (C2_HOOK_FASTCALL * PointVisibleFromHere_original)(br_vector3* pFrom, br_vector3* pTo);
+int C2_HOOK_FASTCALL PointVisibleFromHere(br_vector3* pFrom, br_vector3* pTo) {
+    br_vector3 from;
+    br_vector3 dir;
+    br_vector3 norm;
+    br_scalar t;
+    br_material* material;
+
+    BrVector3Sub(&dir, pTo, pFrom);
+    dir.v[1] += 0.15f;
+    BrVector3Copy(&from, pFrom);
+    from.v[1] += 0.15f;
+    FindFace(&from, &dir, &norm, &t, &material);
+    return t > 1.0;
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004aa400, PointVisibleFromHere, PointVisibleFromHere_original)
--- src/carma2/common/opponent.h
+++ src/carma2/common/opponent.h
@@ -1,6 +1,8 @@
 #ifndef REC2_OPPONENT_H
 #define REC2_OPPONENT_H
 
+#include <brender/brender.h>
+
 #include "c2_stdio.h"
 
 #include "c2_hooks.h"
@@ -8,5 +10,7 @@
 void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index);
 
 void C2_HOOK_FASTCALL LoadInOppoPaths(FILE* pF);
+
+int C2_HOOK_FASTCALL PointVisibleFromHere(br_vector3* pFrom, br_vector3* pTo);
 
 #endif //REC2_OPPONENT_H
