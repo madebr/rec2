f806402a1dbad943494f0756c11bcf4019671803
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-22 06:47:50 +0100

    Hook CollideCamera2

--- src/carma2/common/car.h
+++ src/carma2/common/car.h
@@ -9,4 +9,6 @@ C2_HOOK_VARIABLE_DECLARE(int, gCar_simplification_level);
 
 void C2_HOOK_FASTCALL SetUpPanningCamera(tCar_spec* c);
 
+int C2_HOOK_FASTCALL CollideCamera2(br_vector3* car_pos, br_vector3* cam_pos, br_vector3* old_camera_pos, int manual_move, tCollision_info *collision_info);
+
 #endif //REC2_CAR_H
