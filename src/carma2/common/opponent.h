51c606eebafa371d1264d0e9d12753551183c088
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-22 04:59:38 +0100

    Hook CalcPlayerConspicuousness

--- src/carma2/common/opponent.h
+++ src/carma2/common/opponent.h
@@ -2,6 +2,7 @@
 #define REC2_OPPONENT_H
 
 #include <brender/brender.h>
+#include "rec2_types.h"
 
 #include "c2_stdio.h"
 
@@ -12,5 +13,7 @@ void C2_HOOK_FASTCALL InitOpponentPsyche(int pOpponent_index);
 void C2_HOOK_FASTCALL LoadInOppoPaths(FILE* pF);
 
 int C2_HOOK_FASTCALL PointVisibleFromHere(br_vector3* pFrom, br_vector3* pTo);
+
+void C2_HOOK_FASTCALL CalcPlayerConspicuousness(tOpponent_spec* pOpponent_spec);
 
 #endif //REC2_OPPONENT_H
