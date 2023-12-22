68ad0e0a96d7fee4f0ac41c436299a6dbd109cd5
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-22 04:57:04 +0100

    Hook SetUpPanningCamera

--- src/carma2/common/car.c
+++ src/carma2/common/car.c
@@ -2,3 +2,14 @@
 
 
 C2_HOOK_VARIABLE_IMPLEMENT(int, gCar_simplification_level, 0x006793d8);
+
+void (C2_HOOK_FASTCALL * SetUpPanningCamera_original)(tCar_spec* c);
+void C2_HOOK_FASTCALL SetUpPanningCamera(tCar_spec* c) {
+
+#if defined(C2_HOOKS_ENABLED)
+    SetUpPanningCamera_original(c);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004105f0, SetUpPanningCamera, SetUpPanningCamera_original)
--- src/carma2/common/car.h
+++ src/carma2/common/car.h
@@ -1,8 +1,12 @@
 #ifndef REC2_CAR_H
 #define REC2_CAR_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
 
 C2_HOOK_VARIABLE_DECLARE(int, gCar_simplification_level);
+
+void C2_HOOK_FASTCALL SetUpPanningCamera(tCar_spec* c);
 
 #endif //REC2_CAR_H
