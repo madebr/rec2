fca4956ef9d7c07e3b802fc5177633c994d7ad45
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-22 06:48:21 +0100

    Hook PanningExternalCamera

--- src/carma2/common/car.h
+++ src/carma2/common/car.h
@@ -11,4 +11,6 @@ void C2_HOOK_FASTCALL SetUpPanningCamera(tCar_spec* c);
 
 int C2_HOOK_FASTCALL CollideCamera2(br_vector3* car_pos, br_vector3* cam_pos, br_vector3* old_camera_pos, int manual_move, tCollision_info *collision_info);
 
+void C2_HOOK_FASTCALL PanningExternalCamera(tCar_spec* c, tU32 pTime);
+
 #endif //REC2_CAR_H
