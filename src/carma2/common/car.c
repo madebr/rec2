da6db355dbe2c5c2ed117048c7a8203d082b9b46
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-22 06:48:21 +0100

    Hook PanningExternalCamera

--- src/carma2/common/car.c
+++ src/carma2/common/car.c
@@ -24,3 +24,14 @@ int C2_HOOK_FASTCALL CollideCamera2(br_vector3* car_pos, br_vector3* cam_pos, br
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004122b0, CollideCamera2, CollideCamera2_original)
+
+void (C2_HOOK_FASTCALL * PanningExternalCamera_original)(tCar_spec* c, tU32 pTime);
+void C2_HOOK_FASTCALL PanningExternalCamera(tCar_spec* c, tU32 pTime) {
+
+#if defined(C2_HOOKS_ENABLED)
+    PanningExternalCamera_original(c, pTime);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004104b0, PanningExternalCamera, PanningExternalCamera_original)
