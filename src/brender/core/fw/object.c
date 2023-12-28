17249f02d15e7b184876e7e889ced3d0ea7b3e05
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-28 15:10:01 +0100

    Implement __br_object_queryMany

--- src/brender/core/fw/object.c
+++ src/brender/core/fw/object.c
@@ -27,10 +27,10 @@ C2_HOOK_FUNCTION_ORIGINAL(0x0052d090, _M_br_object_queryBuffer, _M_br_object_que
 br_error (C2_HOOK_CDECL * _M_br_object_queryMany_original)(br_object* self, br_token_value* tv, void* extra, br_size_t extra_size, br_int_32* pcount);
 br_error C2_HOOK_CDECL _M_br_object_queryMany(br_object* self, br_token_value* tv, void* extra, br_size_t extra_size, br_int_32* pcount) {
 
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     return _M_br_object_queryMany_original(self, tv, extra, extra_size, pcount);
 #else
-#error "Not implemented"
+    return BrTokenValueQueryMany(tv, extra, extra_size, pcount, self, self->dispatch->_templateQuery(self));
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0052d0c0, _M_br_object_queryMany, _M_br_object_queryMany_original)
