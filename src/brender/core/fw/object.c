51d7758acc62702ae48632fcd011581d73c43157
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-28 15:15:16 +0100

    Implement  _M_br_object_queryAllSize

--- src/brender/core/fw/object.c
+++ src/brender/core/fw/object.c
@@ -60,10 +60,10 @@ C2_HOOK_FUNCTION_ORIGINAL(0x0052d120, _M_br_object_queryAll, _M_br_object_queryA
 br_error (C2_HOOK_CDECL * _M_br_object_queryAllSize_original)(br_object* self, br_size_t* psize);
 br_error C2_HOOK_CDECL _M_br_object_queryAllSize(br_object* self, br_size_t* psize) {
 
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     return _M_br_object_queryAllSize_original(self, psize);
 #else
-#error "Not implemented"
+    return BrTokenValueQueryAllSize(psize, self, self->dispatch->_templateQuery(self));
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0052d150, _M_br_object_queryAllSize, _M_br_object_queryAllSize_original)
