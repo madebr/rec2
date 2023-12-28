d6d45b310b72fbfe6a9ea1c04c370d28d688e024
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-28 16:59:50 +0100

    Implement _M_br_object_container_tokensMatch

--- src/brender/core/fw/objectc.c
+++ src/brender/core/fw/objectc.c
@@ -6,6 +6,7 @@
 #include "../std/brstdlib.h"
 #include "fwsetup.h"
 #include "pattern.h"
+#include "tokenval.h"
 
 void* (C2_HOOK_CDECL * BrObjectListAllocate_original)(void* res);
 void* C2_HOOK_CDECL BrObjectListAllocate(void* res) {
@@ -256,14 +257,27 @@ C2_HOOK_FUNCTION_ORIGINAL(0x0052d560, _M_br_object_container_tokensMatchBegin, _
 br_boolean (C2_HOOK_CDECL * _M_br_object_container_tokensMatch_original)(br_object_container* self, br_object* h, void* arg);
 br_boolean C2_HOOK_CDECL _M_br_object_container_tokensMatch(br_object_container* self, br_object* h, void* arg) {
 
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     return _M_br_object_container_tokensMatch_original(self, h, arg);
 #else
     token_match* tm;
     br_size_t s;
     br_int_32 n;
-    LOG_TRACE("(%p, %p, %p)", self, h, arg);
-#error "Not implemented"
+
+    tm = arg;
+    if (arg == NULL) {
+        return 1;
+    }
+    h->dispatch->_queryManySize(h, &s, tm->query);
+    if (s > tm->extra_size) {
+        if (tm->extra != NULL) {
+            BrResFree(tm->extra);
+        }
+        tm->extra = BrResAllocate(tm, s, BR_MEMORY_APPLICATION);
+        tm->extra_size = s;
+    }
+    h->dispatch->_queryMany(h, tm->query, tm->extra, tm->extra_size, &n);
+    return BrTokenValueCompare(tm->original, tm->query);
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0052d5e0, _M_br_object_container_tokensMatch, _M_br_object_container_tokensMatch_original)
