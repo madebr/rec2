fc78c06fe8036bb9489bda4ac8cc4465254d83c8
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-29 21:55:57 +0100

    Implement DumpMatrixFloat

--- src/brender/core/fw/tokenval.c
+++ src/brender/core/fw/tokenval.c
@@ -608,15 +608,24 @@ void C2_HOOK_STDCALLDumpMatrixFixed(br_fixed_ls* xp, int rows, int cols, char* p
     char value[128];
 #error "Not implemented"
 }
+#endif
 
 void C2_HOOK_STDCALL DumpMatrixFloat(br_float* fp, int rows, int cols, char* prefix, char* info_0, char* info_n, br_putline_cbfn* putline, void* arg) {
     int i;
     int j;
     char* cp;
     char value[128];
-#error "Not implemented";
+
+    for (i = 0; i < rows; i++) {
+        cp = value;
+        cp += BrSprintf(value, "%s%s[", prefix, (i == 0) ? info_0 : info_n);
+        for (j = 0; j < cols; j++, fp++) {
+            cp += BrSprintf(cp, "%s%f", (j == 0) ? "" : ",", *fp);
+        }
+        BrStrCpy(cp, "]");
+        putline(value, arg);
+    }
 }
-#endif
 
 void C2_HOOK_STDCALL DumpObject(br_object* h, char* prefix, char* info, br_putline_cbfn* putline, void* arg) {
     char value[128];
