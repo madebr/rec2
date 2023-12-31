9bff96cbaa0a6b5f5e7ba96cfb427eae8a12a011
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-12-31 19:27:11 +0100

    Implement _M_br_device_pixelmap_gen_flush

--- src/brender/core/pixelmap/pmgen.c
+++ src/brender/core/pixelmap/pmgen.c
@@ -235,10 +235,10 @@ C2_HOOK_FUNCTION_ORIGINAL(0x0053d8a0, _M_br_device_pixelmap_gen_copyBits, _M_br_
 
 br_error (C2_HOOK_CDECL * _M_br_device_pixelmap_gen_flush_original)(br_device_pixelmap* self);
 br_error C2_HOOK_CDECL _M_br_device_pixelmap_gen_flush(br_device_pixelmap* self) {
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
 return _M_br_device_pixelmap_gen_flush_original(self);
 #else
-#error "Not implemented"
+    return 0;
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0053d970, _M_br_device_pixelmap_gen_flush, _M_br_device_pixelmap_gen_flush_original)
