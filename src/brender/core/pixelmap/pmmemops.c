ee1801de2fd150128bd14984da5f0667628a07dc
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-01-02 14:02:18 +0100

    Implement pm_mem_fill_colour_rect

--- src/brender/core/pixelmap/pmmemops.c
+++ src/brender/core/pixelmap/pmmemops.c
@@ -62,10 +62,52 @@ C2_HOOK_FUNCTION_ORIGINAL(0x0053e686, pm_mem_fill_colour, pm_mem_fill_colour_ori
 
 void (C2_HOOK_CDECL * pm_mem_fill_colour_rect_original)(br_uint_8* dest, br_uint_32 qual, br_uint_32 width, br_uint_32 height, br_uint_32 stride, br_uint_32 bpp, br_uint_32 colour);
 void C2_HOOK_CDECL pm_mem_fill_colour_rect(br_uint_8* dest, br_uint_32 qual, br_uint_32 width, br_uint_32 height, br_uint_32 stride, br_uint_32 bpp, br_uint_32 colour) {
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     pm_mem_fill_colour_rect_original(dest, qual, width, height, stride, bpp, colour);
 #else
-#error "Not implemented"
+    br_uint_32 x;
+    br_uint_32 y;
+
+    switch (bpp) {
+    case 1:
+        for (y = 0; y < height; y++, dest += stride) {
+            c2_memset(dest, colour, width);
+        }
+        break;
+    case 2:
+        for (y = 0; y < height; y++, dest += stride) {
+            for (x = 0; x < width; x++) {
+                ((br_uint_16*)dest)[x] = colour;
+            }
+        }
+        break;
+    case 3: {
+        br_uint_8 b0, b1, b2;
+
+        b0 = (br_uint_8)((colour >>  0) & 0xff);
+        b1 = (br_uint_8)((colour >>  8) & 0xff);
+        b2 = (br_uint_8)((colour >> 16) & 0xff);
+
+        for (y = 0; y < height; y++, dest += stride) {
+            for (x = 0; x < width; x++) {
+                ((br_uint_8*)dest)[3 * x + 0] = b0;
+                ((br_uint_8*)dest)[3 * x + 1] = b1;
+                ((br_uint_8*)dest)[3 * x + 2] = b2;
+            }
+        }
+        break;
+    }
+    case 4:
+        for (y = 0; y < height; y++, dest += stride) {
+            for (x = 0; x < width; x++) {
+                ((br_uint_32*)dest)[x] = colour;
+            }
+        }
+        break;
+    default:
+        c2_abort();
+        return;
+    }
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0053e9f3, pm_mem_fill_colour_rect, pm_mem_fill_colour_rect_original)
