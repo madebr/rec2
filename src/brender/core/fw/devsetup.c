2583d6c016774a913d00beff7d031f28135d855d
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-01-04 18:34:59 +0100

    Hook a few devsetup functions

--- src/brender/core/fw/devsetup.c
+++ src/brender/core/fw/devsetup.c
@@ -60,3 +60,98 @@ br_error C2_HOOK_CDECL BrDevBeginTV(br_pixelmap** ppm, char* setup_string, br_to
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00528e10, BrDevBeginTV, BrDevBeginTV_original)
+
+br_pixelmap* (C2_HOOK_CDECL * BrDevBeginOld_original)(char* setup_string);
+br_pixelmap* C2_HOOK_CDECL BrDevBeginOld(char* setup_string) {
+#if defined(C2_HOOKS_ENABLED)
+    return BrDevBeginOld_original(setup_string);
+#else
+    br_pixelmap* s;
+
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00529270, BrDevBeginOld, BrDevBeginOld_original)
+
+void (C2_HOOK_CDECL * BrDevEndOld_original)(void);
+void C2_HOOK_CDECL BrDevEndOld(void) {
+#if defined(C2_HOOKS_ENABLED)
+    BrDevEndOld_original();
+#else
+
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x005292c0, BrDevEndOld, BrDevEndOld_original)
+
+void (C2_HOOK_CDECL * BrDevPaletteSetOld_original)(br_pixelmap* pm);
+void C2_HOOK_CDECL BrDevPaletteSetOld(br_pixelmap* pm) {
+#if defined(C2_HOOKS_ENABLED)
+    BrDevPaletteSetOld_original(pm);
+#else
+
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x005292e0, BrDevPaletteSetOld, BrDevPaletteSetOld_original)
+
+void (C2_HOOK_CDECL * BrDevPaletteSetEntryOld_original)(int i, br_colour colour);
+void C2_HOOK_CDECL BrDevPaletteSetEntryOld(int i, br_colour colour) {
+#if defined(C2_HOOKS_ENABLED)
+    BrDevPaletteSetEntryOld_original(i, colour);
+#else
+
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00529300, BrDevPaletteSetEntryOld, BrDevPaletteSetEntryOld_original)
+
+br_error (C2_HOOK_CDECL * BrRendererFacilityFind_original)(br_renderer_facility** prf, br_device_pixelmap* destination, br_token scalar_type);
+br_error C2_HOOK_CDECL BrRendererFacilityFind(br_renderer_facility** prf, br_device_pixelmap* destination, br_token scalar_type) {
+#if defined(C2_HOOKS_ENABLED)
+    return BrRendererFacilityFind_original(prf, destination, scalar_type);
+#else
+    br_renderer_facility* renderer_facility;
+    br_error r;
+    br_output_facility* ot;
+    char object_name[23];
+    char image_name[9];
+    br_boolean scalar_is_valid;
+
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00529320, BrRendererFacilityFind, BrRendererFacilityFind_original)
+
+br_error (C2_HOOK_CDECL * BrPrimitiveLibraryFind_original)(br_primitive_library** ppl, br_device_pixelmap* destination, br_token scalar_type);
+br_error C2_HOOK_CDECL BrPrimitiveLibraryFind(br_primitive_library** ppl, br_device_pixelmap* destination, br_token scalar_type) {
+#if defined(C2_HOOKS_ENABLED)
+    return BrPrimitiveLibraryFind_original(ppl, destination, scalar_type);
+#else
+    br_primitive_library* primitive_library;
+    br_error r;
+    br_output_facility* ot;
+    char object_name[25];
+    char image_name[9];
+    br_boolean scalar_is_valid;
+
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x005294d0, BrPrimitiveLibraryFind, BrPrimitiveLibraryFind_original)
+
+br_error (C2_HOOK_CDECL * BrGeometryFormatFind_original)(br_geometry** pgf, br_renderer* renderer, br_renderer_facility* renderer_facility, br_token scalar_type, br_token format_type);
+br_error C2_HOOK_CDECL BrGeometryFormatFind(br_geometry** pgf, br_renderer* renderer, br_renderer_facility* renderer_facility, br_token scalar_type, br_token format_type) {
+#if defined(C2_HOOKS_ENABLED)
+    return BrGeometryFormatFind_original(pgf, renderer, renderer_facility, scalar_type, format_type);
+#else
+    br_error r;
+    br_geometry* gf;
+    char object_name[21];
+    char image_name[9];
+    br_boolean scalar_is_valid;
+
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00529680, BrGeometryFormatFind, BrGeometryFormatFind_original)
