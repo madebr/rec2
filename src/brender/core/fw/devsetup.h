d9121e1daba0a945dc0483200e2f25ff0ae08b5e
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-01-04 18:34:59 +0100

    Hook a few devsetup functions

--- src/brender/core/fw/devsetup.h
+++ src/brender/core/fw/devsetup.h
@@ -15,4 +15,18 @@ br_error C2_HOOK_CDECL BrDevBeginTV(br_pixelmap** ppm, char* setup_string, br_to
 
 br_error C2_HOOK_CDECL BrDevBegin(br_pixelmap** ppm, char* setup_string);
 
+br_pixelmap* C2_HOOK_CDECL BrDevBeginOld(char* setup_string);
+
+void C2_HOOK_CDECL BrDevEndOld(void);
+
+void C2_HOOK_CDECL BrDevPaletteSetOld(br_pixelmap* pm);
+
+void C2_HOOK_CDECL BrDevPaletteSetEntryOld(int i, br_colour colour);
+
+br_error C2_HOOK_CDECL BrRendererFacilityFind(br_renderer_facility** prf, br_device_pixelmap* destination, br_token scalar_type);
+
+br_error C2_HOOK_CDECL BrPrimitiveLibraryFind(br_primitive_library** ppl, br_device_pixelmap* destination, br_token scalar_type);
+
+br_error C2_HOOK_CDECL BrGeometryFormatFind(br_geometry** pgf, br_renderer* renderer, br_renderer_facility* renderer_facility, br_token scalar_type, br_token format_type);
+
 #endif //REC2_DEVSETUP_H
