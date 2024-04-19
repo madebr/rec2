65a1f9e49c8a88c37b50d2d170b5e0e95e8b6398
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-19 22:40:34 +0200

    Implement HideSkid

--- src/carma2/common/skidmark.h
+++ src/carma2/common/skidmark.h
@@ -8,6 +8,8 @@
 
 void C2_HOOK_FASTCALL InitSkids(void);
 
+void C2_HOOK_FASTCALL HideSkid(int pSkid_num);
+
 void C2_HOOK_FASTCALL ReadSlick(FILE *pF,tSlick_spec *pSlick_spec);
 
 #endif // REC2_SKIDMARK_H
