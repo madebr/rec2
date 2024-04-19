1873f0b58d7ec60c6afd69737c5a0f7b725d856d
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-19 23:34:28 +0200

    Hook InitTrackCrushables

--- src/carma2/common/crush.h
+++ src/carma2/common/crush.h
@@ -16,6 +16,8 @@ C2_HOOK_VARIABLE_DECLARE_ARRAY(const char*, gDetach_type_names, 4);
 
 void C2_HOOK_FASTCALL InitCrush(void);
 
+void C2_HOOK_FASTCALL InitTrackCrushables(void);
+
 void C2_HOOK_FASTCALL LoadGeneralCrushSettings(FILE* file);
 
 br_scalar C2_HOOK_FASTCALL SquaredDistanceFromLineSegment(br_vector3* pP, br_vector3* pA, br_vector3* pB);
