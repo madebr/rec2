9f03cf7f05d37b689d4c61bdba92a57c4a65b39f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-20 19:02:56 +0200

    Hook S3StopSound

--- src/carma2/s3/src/audio.h
+++ src/carma2/s3/src/audio.h
@@ -20,6 +20,8 @@ int C2_HOOK_FASTCALL S3StopOutletSound(tS3_outlet* pOutlet);
 
 int C2_HOOK_FASTCALL S3ReleaseSoundSource(tS3_sound_source* src);
 
+int C2_HOOK_FASTCALL S3StopSound(int pTag);
+
 void C2_HOOK_FASTCALL S3Service(int inside_cockpit, int unk1);
 
 #endif // S3_AUDIO_H
