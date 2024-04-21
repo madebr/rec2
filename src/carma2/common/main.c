d60ae79159ac1401ed5572f81944b6c15cb48cce
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-21 03:15:57 +0200

    Implement QuitGame

--- src/carma2/common/main.c
+++ src/carma2/common/main.c
@@ -6,32 +6,64 @@
 #include "drmem.h"
 #include "errors.h"
 #include "globvars.h"
+#include "globvrpb.h"
+#include "graphics.h"
 #include "init.h"
 #include "input.h"
+#include "loading.h"
+#include "loadsave.h"
 #include "network.h"
+#include "physics.h"
+#include "replay.h"
 #include "sound.h"
 #include "structur.h"
 #include "utility.h"
+#include "world.h"
 
 #include "platform.h"
+
+#include <s3/s3.h>
 
 #include "c2_stdlib.h"
 #include "c2_string.h"
 
 C2_NORETURN_FUNCPTR void (C2_HOOK_FASTCALL * QuitGame_original)(void);
 C2_NORETURN void C2_HOOK_FASTCALL QuitGame(void) {
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     C2_HOOK_START();
     QuitGame_original();
     C2_HOOK_FINISH();
 #else
     SaveOptions();
     if (C2V(gSave_game_out_of_sync)) {
-        SaveGame();
+        WriteSaveGame();
     }
     ActionReplayFinishRecording();
-
-#error "Not implemented"
+    DisableRecursiveCollisions();
+    dr_dprintf("QuitGame() - Point 1");
+    if (C2V(gProgram_state).racing && C2V(gAdditional_actors) != NULL && C2V(gAdditional_actors)->children != NULL) {
+        dr_dprintf("QuitGame() - Point 2");
+        SaveAdditionalStuff();
+        dr_dprintf("QuitGame() - Point 3");
+    }
+    C2V(gProgram_state).racing = 0;
+    SaveOptions();
+    if (C2V(gNet_mode) != eNet_mode_none) {
+        NetLeaveGame(C2V(gCurrent_net_game));
+    }
+    ShutdownNetIfRequired();
+    if (C2V(gSound_available)) {
+        S3StopAll();
+    }
+    if (C2V(gBr_initialized)) {
+        ClearEntireScreen();
+    }
+    PDRevertPalette();
+    StopMusic();
+    if (C2V(gBr_initialized)) {
+        RemoveAllBrenderDevices();
+    }
+    PDShutdownSystem();
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00491f70, QuitGame, QuitGame_original)
