3f32f36f7bbc9913b30e714a8d8e1edccf9e6f72
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-21 17:08:31 +0200

    Use const char * in text headup functions

--- src/carma2/common/displays.h
+++ src/carma2/common/displays.h
@@ -84,11 +84,11 @@ void C2_HOOK_FASTCALL KillOldestQueuedHeadup(void);
 
 void C2_HOOK_FASTCALL ClearQueuedHeadups(void);
 
-int C2_HOOK_FASTCALL NewTextHeadupSlot2(int pSlot_index, int pFlash_rate, int pLifetime, int pFont_index, char* pText, int pQueue_it);
+int C2_HOOK_FASTCALL NewTextHeadupSlot2(int pSlot_index, int pFlash_rate, int pLifetime, int pFont_index, const char* pText, int pQueue_it);
 
-int C2_HOOK_FASTCALL NewTextHeadupSlot(int pSlot_index, int pFlash_rate, int pLifetime, int pFont_index, char* pText);
+int C2_HOOK_FASTCALL NewTextHeadupSlot(int pSlot_index, int pFlash_rate, int pLifetime, int pFont_index, const char* pText);
 
-void C2_HOOK_FASTCALL TransDRPixelmapCleverText(br_pixelmap* pPixelmap, int pX, int pY, tDR_font* pFont, char* pText, int pRight_edge);
+void C2_HOOK_FASTCALL TransDRPixelmapCleverText(br_pixelmap* pPixelmap, int pX, int pY, tDR_font* pFont, const char* pText, int pRight_edge);
 
 void C2_HOOK_STDCALL CreateAccentPolyActor(float pX, float pY, float pWidth, float pHeight);
 
