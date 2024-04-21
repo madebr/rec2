57bc925ffb9197f0e9dfc79c794385b5209a90d0
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-21 20:10:50 +0200

    Hook PositionExternalCamera

--- src/carma2/common/replay.h
+++ src/carma2/common/replay.h
@@ -11,6 +11,8 @@ void C2_HOOK_FASTCALL DefaultQuicktimeSettings(void);
 
 void C2_HOOK_FASTCALL InitializeActionReplayCamera(tActionReplayCameraMode pCamPos);
 
+void C2_HOOK_FASTCALL PositionExternalCamera(tCar_spec* pCar_spec, tU32 pTime);
+
 void C2_HOOK_FASTCALL ActionReplayFinishRecording(void);
 
 #endif //REC2_REPLAY_H
