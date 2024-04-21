f4f42db91b02681dc78abb36a8aed126a5f99d0a
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-21 17:23:01 +0200

    Implement ToggleMiniMap

--- src/carma2/include/rec2_types.h
+++ src/carma2/include/rec2_types.h
@@ -3194,6 +3194,8 @@ enum {
 };
 
 enum {
+    eMiscString_minimap_on = 2,
+    eMiscString_minimap_off = 3,
     eMiscString_checkpoint_finder_turned_off = 254,
     eMiscString_checkpoint_finder_turned_on = 255,
 };
