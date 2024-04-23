29056b0153f9a2e3957819a31d5c1582d2320978
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-23 04:30:33 +0200

    Hook FRONTEND_Quit_Create

--- src/carma2/common/frontend_quit.h
+++ src/carma2/common/frontend_quit.h
@@ -5,6 +5,8 @@
 
 #include "rec2_types.h"
 
+int C2_HOOK_FASTCALL FRONTEND_Quit_Create(tFrontend_spec* pFrontend);
+
 int C2_HOOK_FASTCALL FRONTEND_Quit_Yes(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_QUIT_H */
