05a0598b63e73569613c83b5eb6772abbaff6748
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-23 04:30:33 +0200

    Hook FRONTEND_Quit_Create

--- src/carma2/common/frontend_quit.c
+++ src/carma2/common/frontend_quit.c
@@ -3,6 +3,17 @@
 #include "joystick.h"
 #include "main.h"
 
+int (C2_HOOK_FASTCALL * FRONTEND_Quit_Create_original)(tFrontend_spec* pFrontend);
+int C2_HOOK_FASTCALL FRONTEND_Quit_Create(tFrontend_spec* pFrontend) {
+
+#if defined(C2_HOOKS_ENABLED)
+    return FRONTEND_Quit_Create_original(pFrontend);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00470bb0, FRONTEND_Quit_Create, FRONTEND_Quit_Create_original)
+
 int C2_HOOK_FASTCALL FRONTEND_Quit_Yes(tFrontend_spec* pFrontend) {
 
     StopJoysticks();
