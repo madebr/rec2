4374dc7ac64c4b4ffbffc1f7ec61475dd4cd583b
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-23 04:27:55 +0200

    Hook gFrontend_MAIN

---
+++ src/carma2/common/frontend_main.c
@@ -0,0 +1,7 @@
+#include "frontend_main.h"
+
+#include "frontend.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT_INIT(tFrontend_spec, gFrontend_MAIN, 0x005a80f0, {
+    FIXME TODO
+});
---
+++ src/carma2/common/frontend_main.h
@@ -0,0 +1,10 @@
+#ifndef FRONTEND_MAIN_H
+#define FRONTEND_MAIN_H
+
+#include "c2_hooks.h"
+
+#include "rec2_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_MAIN);
+
+#endif /* FRONTEND_MAIN_H */
