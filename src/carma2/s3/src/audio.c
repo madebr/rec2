9941eb0df49464c7fb5c41d30e846681b8dd6a30
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-26 21:45:43 +0200

    Hook S3CreateOutlet

--- src/carma2/s3/src/audio.c
+++ src/carma2/s3/src/audio.c
@@ -77,6 +77,17 @@ int C2_HOOK_FASTCALL S3StopChannel(tS3_channel* chan) {
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00564bfd, S3StopChannel, S3StopChannel_original)
 
+tS3_outlet* (C2_HOOK_FASTCALL * S3CreateOutlet_original)(int pCount_channels_1, int pCount_channels_2);
+tS3_outlet* C2_HOOK_FASTCALL S3CreateOutlet(int pCount_channels_1, int pCount_channels_2) {
+
+#if defined(C2_HOOKS_ENABLED)
+    return S3CreateOutlet_original(pCount_channels_1, pCount_channels_2);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x005653c7, S3CreateOutlet, S3CreateOutlet_original)
+
 int C2_HOOK_FASTCALL S3StopOutletSound(tS3_outlet* pOutlet) {
     tS3_channel* c;
 
