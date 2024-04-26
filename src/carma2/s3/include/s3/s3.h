850a629de31afcc42830896bf737fff38f86f7a2
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-26 21:58:41 +0200

    Hook S3ReleaseOutlet

--- src/carma2/s3/include/s3/s3.h
+++ src/carma2/s3/include/s3/s3.h
@@ -105,6 +105,8 @@ void C2_HOOK_FASTCALL S3Disable(void);
 
 tS3_outlet* C2_HOOK_FASTCALL S3CreateOutlet(int pCount_channels_1, int pCount_channels_2);
 
+int C2_HOOK_FASTCALL S3ReleaseOutlet(tS3_outlet* pOutlet);
+
 int C2_HOOK_FASTCALL S3SetOutletVolume(tS3_outlet* pOutlet, int pVolume);
 
 void C2_HOOK_FASTCALL S3StopAllOutletSounds(void);
