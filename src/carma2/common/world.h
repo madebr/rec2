e31b95aa94b2eeb5cedeb6248cb24f55e86109c3
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-26 23:28:27 +0200

    Implement LoadTrackSoundGenerators

--- src/carma2/common/world.h
+++ src/carma2/common/world.h
@@ -21,6 +21,9 @@ C2_HOOK_VARIABLE_DECLARE(tSmashable_item_spec*, gTrack_smashable_environment_spe
 C2_HOOK_VARIABLE_DECLARE_ARRAY(tSmashable_item_spec*, gSmashable_track_indexable_triggers, 100);
 C2_HOOK_VARIABLE_DECLARE_ARRAY(tUnknown_0x006a7fc8, gUnknown_0x006a7fc8, 25);
 C2_HOOK_VARIABLE_DECLARE(br_actor*, gAdditional_actors);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(const char*, gSpecial_effects_boundary_choices, 4);
+C2_HOOK_VARIABLE_DECLARE(int, gDefault_engine_noise_index);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(const char*, gSoundGeneratorTypeNames, 3);
 
 tCar_texturing_level C2_HOOK_FASTCALL GetCarTexturingLevel(void);
 
