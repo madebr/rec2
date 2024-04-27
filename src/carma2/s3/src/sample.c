8f1782929bfaf447bbf8027564e7261972dd295c
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-27 16:39:23 +0200

    Hook S3GetBufferDescription

--- src/carma2/s3/src/sample.c
+++ src/carma2/s3/src/sample.c
@@ -1,5 +1,16 @@
 #include "sample.h"
 
+tS3_buffer_desc* (C2_HOOK_FASTCALL * S3GetBufferDescription_original)(int pSample_id);
+tS3_buffer_desc* C2_HOOK_FASTCALL S3GetBufferDescription(int pSample_id) {
+
+#if defined(C2_HOOKS_ENABLED)
+    return S3GetBufferDescription_original(pSample_id);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x005687ff, S3GetBufferDescription, S3GetBufferDescription_original)
+
 tS3_error_codes (C2_HOOK_FASTCALL * S3LoadSample_original)(int pSample_id);
 tS3_error_codes C2_HOOK_FASTCALL S3LoadSample(int pSample_id) {
 
