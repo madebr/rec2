bbae4a4ab9d9337b31e00a0c80bd7cb3f776d0be
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-27 16:39:23 +0200

    Hook S3GetBufferDescription

--- src/carma2/s3/src/sample.h
+++ src/carma2/s3/src/sample.h
@@ -3,6 +3,8 @@
 
 #include <s3/s3.h>
 
+tS3_buffer_desc* C2_HOOK_FASTCALL S3GetBufferDescription(int pSample_id);
+
 tS3_error_codes C2_HOOK_FASTCALL S3LoadSample(int pSample_id);
 
 #endif /* S3_SAMPLE_H */
