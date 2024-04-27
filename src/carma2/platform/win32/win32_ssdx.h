84764a87065fc072709273cf959b35ccc8b89e70
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-27 19:58:59 +0200

    Implement PDS3Init

--- src/carma2/platform/win32/win32_ssdx.h
+++ src/carma2/platform/win32/win32_ssdx.h
@@ -1,6 +1,8 @@
 #ifndef C2_WIN32_SSDX_H
 #define C2_WIN32_SSDX_H
 
+#include "win32_types.h"
+
 #include <dsound.h>
 
 #include "c2_hooks.h"
@@ -9,6 +11,9 @@
 C2_HOOK_VARIABLE_DECLARE(LPDIRECTSOUND, gDirectSound);
 C2_HOOK_VARIABLE_DECLARE(LPDIRECTSOUND, gPD_S3_direct_sound);
 C2_HOOK_VARIABLE_DECLARE_ARRAY(int, gPDS3_volume_factors, 256);
+C2_HOOK_VARIABLE_DECLARE(tPD_S3_config, gPD_S3_config);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(char, gS3_path_separator, 2);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(char, gS3_sound_folder_name, 6);
 
 enum tSSDXFlags {
     SSDX_InitDirectSound = 0x2,
@@ -16,6 +21,8 @@ enum tSSDXFlags {
 
 
 void C2_HOOK_FASTCALL SSDXStart(HWND p_hWnd, int p_DirectDraw, int p_EnumerateDDX, int p_flags);
+
+int C2_HOOK_FASTCALL PDS3Init(void);
 
 int C2_HOOK_FASTCALL PDS3DDXInit(void);
 
