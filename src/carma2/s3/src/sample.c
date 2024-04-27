7b9554e6fbfd5ada8f317f633e35f1fca5809fd0
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-28 00:00:10 +0200

    Implement S3CheckWavHeader

--- src/carma2/s3/src/sample.c
+++ src/carma2/s3/src/sample.c
@@ -101,3 +101,61 @@ void C2_HOOK_CDECL s3_dprintf(const char* pFormat, ...) {
 
 }
 C2_HOOK_FUNCTION(0x00565b46, s3_dprintf);
+
+int (C2_HOOK_FASTCALL * S3CheckWavHeader_original)(tS3_wav_file* pWav_buffer, tS3_wav_chunk_info_header** pWav_format_info_header, void** pSamples, int* pSample_size);
+int C2_HOOK_FASTCALL S3CheckWavHeader(tS3_wav_file* pWav_buffer, tS3_wav_chunk_info_header** pWav_format_info_header, void** pSamples, int* pSample_size) {
+
+#if 0//defined(C2_HOOKS_ENABLED)
+    return S3CheckWavHeader_original(pWav_buffer, pWav_format_info_header, pSamples, pSample_size);
+#else
+    void* chunk_start;
+    void* file_end;
+
+    if (pWav_format_info_header != NULL) {
+        *pWav_format_info_header = NULL;
+    }
+    if (pSamples != NULL) {
+        *pSamples = NULL;
+    }
+    if (pSample_size != NULL) {
+        *pSample_size = 0;
+    }
+    if (c2_strncmp(pWav_buffer->riff_header.magic, "RIFF", 4) != 0 || c2_strncmp(pWav_buffer->wave_magic, "WAVE", 4) != 0) {
+        return 0;
+    }
+    file_end = (br_uint_8*)pWav_buffer->wave_magic + pWav_buffer->riff_header.chunk_size;
+    chunk_start = &pWav_buffer->fmt_chunk;
+    while (chunk_start < file_end) {
+        tS3_riff_chunk_header* chunk_header = chunk_start;
+        if (c2_strncmp(chunk_header->magic, "fmt ", 4) == 0) {
+            tS3_riff_fmt_chunk* fmt_chunk = chunk_start;
+            if (pWav_format_info_header != NULL && *pWav_format_info_header == NULL) {
+                C2_HOOK_STATIC_ASSERT_STRUCT_OFFSET(tS3_wav_chunk_info_header, bits_per_sample, 0xe);
+                if (chunk_header->chunk_size < offsetof(tS3_wav_chunk_info_header, bits_per_sample)) {
+                    return 0;
+                }
+                *pWav_format_info_header = &fmt_chunk->wav_info_header;
+                if ((pSamples == NULL || *pSamples != NULL) && (pSample_size == NULL || *pSample_size != 0)) {
+                    return 1;
+                }
+            }
+        } else if (c2_strncmp(chunk_header->magic, "data", 4) == 0) {
+            tS3_riff_data_chunk* data_chunk = chunk_start;
+            if ((pSamples != NULL && *pSamples == NULL) || (pSample_size != NULL && *pSample_size == 0)) {
+                if (pSamples != NULL) {
+                    *pSamples = data_chunk->data;
+                }
+                if (pSample_size != NULL) {
+                    *pSample_size = data_chunk->header.chunk_size;
+                }
+                if (pWav_format_info_header == NULL || *pWav_format_info_header != NULL) {
+                    return 1;
+                }
+            }
+        }
+        chunk_start = (br_uint_8*)&chunk_header[1] + chunk_header->chunk_size;
+    }
+    return 0;
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00568ee0, S3CheckWavHeader, S3CheckWavHeader_original)
