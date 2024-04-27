28f8d971f55c6e744048aaf3ee4ea2e01e70f661
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-28 00:57:13 +0200

    Implement S3CreateTypeStructs

---
+++ src/carma2/s3/src/s3sound.h
@@ -0,0 +1,10 @@
+#ifndef S3_S3SOUND_H
+#define S3_S3SOUND_H
+
+#include <s3/s3.h>
+
+#include "c2_hooks.h"
+
+int C2_HOOK_FASTCALL S3CreateTypeStructs(tS3_channel* chan);
+
+#endif /* S3_S3SOUND_Hx */
