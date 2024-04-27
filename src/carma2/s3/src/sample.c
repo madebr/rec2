715c5555b3864f7aca302b3b6413089d205d007a
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-27 16:51:48 +0200

    Implement S3GetDescriptorById

--- src/carma2/s3/src/sample.c
+++ src/carma2/s3/src/sample.c
@@ -1,5 +1,7 @@
 #include "sample.h"
 
+C2_HOOK_VARIABLE_IMPLEMENT(tS3_descriptor*, gS3_descriptors, 0x007a0594);
+
 tS3_buffer_desc* (C2_HOOK_FASTCALL * S3GetBufferDescription_original)(int pSample_id);
 tS3_buffer_desc* C2_HOOK_FASTCALL S3GetBufferDescription(int pSample_id) {
 
@@ -21,3 +23,22 @@ tS3_error_codes C2_HOOK_FASTCALL S3LoadSample(int pSample_id) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00568830, S3LoadSample, S3LoadSample_original)
+
+tS3_descriptor* C2_HOOK_FASTCALL S3GetDescriptorByID(int pSample_id) {
+    tS3_descriptor *desc;
+
+    C2_HOOK_STATIC_ASSERT_STRUCT_OFFSET(tS3_descriptor, sample_id, 0x0);
+    C2_HOOK_STATIC_ASSERT_STRUCT_OFFSET(tS3_descriptor, next, 0x2c);
+    C2_HOOK_STATIC_ASSERT_STRUCT_OFFSET(tS3_descriptor, low_memory_alternative, 0x3c);
+
+    for (desc = C2V(gS3_descriptors); desc != NULL; desc = desc->next) {
+        if (desc->sample_id == pSample_id) {
+            if (desc->low_memory_alternative >= 0) {
+                return S3GetDescriptorByID(desc->low_memory_alternative);
+            }
+            return desc;
+        }
+    }
+    return NULL;
+}
+C2_HOOK_FUNCTION(0x00565a3d, S3GetDescriptorByID)
--- src/carma2/s3/src/sample.h
+++ src/carma2/s3/src/sample.h
@@ -3,8 +3,12 @@
 
 #include <s3/s3.h>
 
+C2_HOOK_VARIABLE_DECLARE(tS3_descriptor*, gS3_descriptors);
+
 tS3_buffer_desc* C2_HOOK_FASTCALL S3GetBufferDescription(int pSample_id);
 
 tS3_error_codes C2_HOOK_FASTCALL S3LoadSample(int pSample_id);
+
+tS3_descriptor* C2_HOOK_FASTCALL S3GetDescriptorByID(int pSample_id);
 
 #endif /* S3_SAMPLE_H */
