08dc868106a6701ee0cf5c0d16b1dcd8a7cb36c6
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-27 17:51:03 +0200

    Implement S3LoadSample

--- src/carma2/s3/src/sample.c
+++ src/carma2/s3/src/sample.c
@@ -1,6 +1,14 @@
 #include "sample.h"
 
 #include "audio.h"
+#include "resource.h"
+
+#include "platform.h"
+
+#include <brender/brender.h>
+#include "rec2_types.h"
+
+#include "c2_string.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT(tS3_descriptor*, gS3_descriptors, 0x007a0594);
 
@@ -29,10 +37,43 @@ C2_HOOK_FUNCTION_ORIGINAL(0x005687ff, S3GetBufferDescription, S3GetBufferDescrip
 tS3_error_codes (C2_HOOK_FASTCALL * S3LoadSample_original)(int pSample_id);
 tS3_error_codes C2_HOOK_FASTCALL S3LoadSample(int pSample_id) {
 
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     return S3LoadSample_original(pSample_id);
 #else
-#error "Not implemented"
+    tS3_descriptor* descriptor;
+    tS3_buffer_desc* buffer_description;
+    char path[512];
+
+    if (!C2V(gS3_enabled)) {
+        return eS3_error_none;
+    }
+    descriptor = S3GetDescriptorByID(pSample_id);
+    if (descriptor == NULL) {
+        return eS3_error_bad_id;
+    }
+    if (descriptor->type != 0) {
+        return eS3_error_none;
+    }
+    if (descriptor->buffer_description != NULL) {
+        return eS3_error_none;
+    }
+    path[0] = '\0';
+    c2_strcpy(path, descriptor->path);
+
+    C2_HOOK_BUG_ON(sizeof(tS3_buffer_desc) != 0x18);
+
+    buffer_description = S3MemAllocate(sizeof(tS3_buffer_desc), kMem_S3_sound_header);
+    if (buffer_description == NULL) {
+        return eS3_error_memory;
+    }
+    c2_memset(buffer_description, 0, sizeof(tS3_buffer_desc));
+    descriptor->pd_handle = PDS3BufferWav(path, buffer_description);
+    if (descriptor->pd_handle == NULL) {
+        BrFailure("Cound not load sample:%s", path);
+    }
+    descriptor->effects_enabled = 0;
+    descriptor->buffer_description = buffer_description;
+    return eS3_error_none;
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00568830, S3LoadSample, S3LoadSample_original)
