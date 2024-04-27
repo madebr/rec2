9af6fe369e5fff3012d5c76043282c69b320a711
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-27 21:28:30 +0200

    Implement S3GetFileSize

--- src/carma2/s3/src/resource.c
+++ src/carma2/s3/src/resource.c
@@ -16,3 +16,15 @@ FILE* C2_HOOK_FASTCALL S3_low_memory_fopen(const char* pPath, const char* pMode)
     return NULL;
 }
 C2_HOOK_FUNCTION(0x005186f0, S3_low_memory_fopen)
+
+int C2_HOOK_FASTCALL S3GetFileSize(FILE *pF) {
+    int cur_pos;
+    int pos_end;
+
+    cur_pos = c2_ftell(pF);
+    c2_fseek(pF, 0, SEEK_END);
+    pos_end = c2_ftell(pF);
+    c2_fseek(pF, cur_pos, SEEK_SET);
+    return pos_end;
+}
+C2_HOOK_FUNCTION(0x00568bdc, S3GetFileSize)
