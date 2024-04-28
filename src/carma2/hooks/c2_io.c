beea41374d9438dbc426007ac07468bbd285aafc
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-28 04:26:08 +0200

    Implement _close

--- src/carma2/hooks/c2_io.c
+++ src/carma2/hooks/c2_io.c
@@ -21,4 +21,15 @@ int C2_HOOK_CDECL c2_open(const char *filename, int oflag) {
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00584a00, c2_open, c2_open_original)
 
+int (C2_HOOK_CDECL * c2_close_original)(int fd);
+int C2_HOOK_CDECL c2_close(int fd) {
+
+#if defined(C2_HOOKS_ENABLED)
+    return c2_close_original(fd);
+#else
+    return _close(fd);
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x005805e0, c2_close, c2_close_original)
+
 #endif
