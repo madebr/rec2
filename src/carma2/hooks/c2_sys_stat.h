fdccafe369e41d689e3d022d8c5aa037b2e638d5
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-28 04:16:14 +0200

    Implement _fstat32

---
+++ src/carma2/hooks/c2_sys_stat.h
@@ -0,0 +1,21 @@
+#ifndef REC2_SYS_STAT_H_
+#define REC2_SYS_STAT_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#if defined(C2_HOOKS_ENABLED)
+
+#include "c2_hooks.h"
+
+int C2_HOOK_CDECL c2_fstat32(int fd, struct _stat32 *buffer);
+
+#else
+
+#include <sys/stat.h>
+
+#define c2_fstat32 _fstat32
+
+#endif
+
+#endif //REC2_SYS_STAT_H_
