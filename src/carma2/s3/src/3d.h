d60d0d52c7cdfba3f9ad2889275e5bb74b045e3f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-29 21:55:38 +0200

    Implement FUN_00565e39

--- src/carma2/s3/src/3d.h
+++ src/carma2/s3/src/3d.h
@@ -3,9 +3,18 @@
 
 #include <s3/s3.h>
 
+C2_HOOK_VARIABLE_DECLARE(float, gFLOAT_006b2c6c);
+C2_HOOK_VARIABLE_DECLARE(float, gFLOAT_006b2c4c);
+C2_HOOK_VARIABLE_DECLARE(float, gFLOAT_006b2c68);
+C2_HOOK_VARIABLE_DECLARE(br_vector3, gS3_listener_position_now);
+C2_HOOK_VARIABLE_DECLARE(br_vector3, gS3_listener_vel_now);
+C2_HOOK_VARIABLE_DECLARE(br_vector3, gS3_listener_left_now);
+
 void C2_HOOK_FASTCALL S3StopSoundSource(tS3_sound_source* src);
 
 void C2_HOOK_FASTCALL S3UpdateSoundSource(tS3_outlet* outlet, tS3_sound_tag tag, tS3_sound_source* src, float pMax_distance_squared, int pPeriod, tS3_repeats pAmbient_repeats, tS3_volume pVolume, int pPitch, tS3_speed pSpeed);
+
+int C2_HOOK_STDCALL FUN_00565e39(float a1, float a2, float a3);
 
 int C2_HOOK_STDCALL S3Set3DSoundEnvironment(float a1, float a2, float a3);
 
