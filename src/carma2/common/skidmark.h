40252f7a667bde843c9478c57ddc7424f8cc7261
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-04-30 05:37:39 +0200

    Implement AdjustSkid

--- src/carma2/common/skidmark.h
+++ src/carma2/common/skidmark.h
@@ -14,4 +14,6 @@ void C2_HOOK_FASTCALL HideSkids(void);
 
 void C2_HOOK_FASTCALL ReadSlick(FILE *pF,tSlick_spec *pSlick_spec);
 
+void  C2_HOOK_FASTCALL AdjustSkid(int pSkid_num, br_matrix34* pMatrix, br_material* pMaterial);
+
 #endif // REC2_SKIDMARK_H
