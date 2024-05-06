a53a6386c4dea062a7bd00a935d1a14656b450f0
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-06 18:45:52 +0200

    Implement PratcamEvent

--- src/carma2/common/pratcam.c
+++ src/carma2/common/pratcam.c
@@ -15,3 +15,8 @@ void C2_HOOK_FASTCALL ChangeAmbientPratcam(int pIndex) {
 
 }
 C2_HOOK_FUNCTION(0x004e0e30, ChangeAmbientPratcam)
+
+void C2_HOOK_FASTCALL PratcamEvent(int pIndex) {
+
+}
+C2_HOOK_FUNCTION(0x004e0e40, PratcamEvent)
