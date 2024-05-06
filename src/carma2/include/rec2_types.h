5bc7e3d36ce36094e677f9ef106fcecacf417ce6
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-06 20:15:50 +0200

    Implement IncrementCheckpoint

--- src/carma2/include/rec2_types.h
+++ src/carma2/include/rec2_types.h
@@ -3335,6 +3335,7 @@ enum {
 enum {
     eMiscString_minimap_on = 2,
     eMiscString_minimap_off = 3,
+    eMiscString_final_lap = 42,
     eMiscString_host_abandoned_race = 87,
     eMiscString_recovery_cancelled = 125,
     eMiscString_fox_cant_do_that = 214,
@@ -3608,6 +3609,7 @@ enum {
     eSoundId_OutOfTime2 = 8010,
     eSoundId_RaceComplete = 8011,
     eSoundId_Checkpoint = 8012,
+    eSoundId_FinalLap = 8014,
 };
 
 #endif // REC2_TYPES_H
