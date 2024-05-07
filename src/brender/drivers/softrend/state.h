7099ac45b0415fd692f7d6ab0d88337fd25d28d8
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-07 03:20:00 +0200

    Hook StateInitialise

---
+++ src/brender/drivers/softrend/state.h
@@ -0,0 +1,10 @@
+#ifndef SOFTREND_STATE_H
+#define SOFTREND_STATE_H
+
+#include "types.h"
+
+#include "c2_hooks.h"
+
+int C2_HOOK_STDCALL StateInitialise(soft_state_all* state);
+
+#endif /* SOFTREND_STATE_H */
