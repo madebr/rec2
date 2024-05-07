beda960c97560d5ecd9a24b36b180ce9260b9b83
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-07 23:32:03 +0200

    Implement _M_br_object_softprim_device

--- src/brender/drivers/softrend/object.c
+++ src/brender/drivers/softrend/object.c
@@ -7,3 +7,11 @@ const char* C2_HOOK_CDECL _M_br_softrend_object_identifier(br_softrend_object* s
     return self->identifier;
 }
 C2_HOOK_FUNCTION(0x00540920, _M_br_softrend_object_identifier)
+
+br_device* C2_HOOK_CDECL _M_br_softrend_object_device(br_softrend_object* self) {
+
+    C2_HOOK_STATIC_ASSERT_STRUCT_OFFSET(br_soft_renderer_facility, device, 0x8);
+
+    return (br_device*)self->device;
+}
+C2_HOOK_FUNCTION(0x00540930, _M_br_softrend_object_device)
--- src/brender/drivers/softrend/object.h
+++ src/brender/drivers/softrend/object.h
@@ -7,4 +7,6 @@
 
 const char* C2_HOOK_CDECL _M_br_softrend_object_identifier(br_softrend_object* self);
 
+br_device* C2_HOOK_CDECL _M_br_softrend_object_device(br_softrend_object* self);
+
 #endif /* SOFTREND_OBJECT_H */
