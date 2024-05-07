f40abbf0b48b4df0e454f7f1015dccec47fd3acd
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-07 02:33:21 +0200

    Hook SendGameplayToAllPlayers

--- src/carma2/common/netgame.h
+++ src/carma2/common/netgame.h
@@ -24,4 +24,6 @@ void C2_HOOK_FASTCALL DeclareWinner(int pWinner_index);
 
 void C2_HOOK_FASTCALL SendGameplay(tPlayer_ID pPlayer, tNet_gameplay_mess pMess, int pParam_1, int pParam_2, int pParam_3, int pParam_4);
 
+void C2_HOOK_FASTCALL SendGameplayToAllPlayers(tNet_gameplay_mess pMess, int pParam_1, int pParam_2, int pParam_3, int pParam_4);
+
 #endif // REC2_NETGAME_H
