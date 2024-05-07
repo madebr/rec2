bf3324fdb4a8bc2e77eb53a4a0d6f4c191598028
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-07 04:02:16 +0200

    Hook GeometryV1BucketsAllocate

--- src/brender/drivers/softrend/types.h
+++ src/brender/drivers/softrend/types.h
@@ -3,6 +3,8 @@
 
 #include <brender/brender.h>
 
+typedef struct br_geometry_v1_buckets br_geometry_v1_buckets;
+
 struct device_templates {
     struct br_tv_template *templates[3];
 };
@@ -20,5 +22,14 @@ typedef struct br_soft_device {
 typedef struct soft_state_all {
     br_uint_8 field_0x0[0x788];
 } soft_state_all;
+
+typedef struct br_soft_renderer_facility {
+    br_renderer_facility_dispatch* dispatch;
+    const char* identifier;
+    br_device* device;
+    void* object_list;
+    soft_state_all default_state;
+    br_int_32 num_instances;
+} br_soft_renderer_facility;
 
 #endif /* SOFTREND_TYPES_H */
