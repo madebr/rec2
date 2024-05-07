dce81fc27e6b485b23deffaa4973942946f60aaf
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-07 23:23:44 +0200

    Implement _M_br_object_softprim_identifier

---
+++ src/brender/drivers/softrend/object.h
@@ -0,0 +1,10 @@
+#ifndef SOFTREND_OBJECT_H
+#define SOFTREND_OBJECT_H
+
+#include "types.h"
+
+#include "c2_hooks.h"
+
+const char* C2_HOOK_CDECL _M_br_softrend_object_identifier(br_softrend_object* self);
+
+#endif /* SOFTREND_OBJECT_H */
