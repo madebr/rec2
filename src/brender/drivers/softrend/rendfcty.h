2ada7e5d758a6676b8de060f01ecbabb1ae758b0
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-07 23:17:54 +0200

    Implement _M_br_renderer_facility_soft_free

--- src/brender/drivers/softrend/rendfcty.h
+++ src/brender/drivers/softrend/rendfcty.h
@@ -1,10 +1,14 @@
 #ifndef SOFTREND_FACILITY_H
 #define SOFTREND_FACILITY_H
 
+#include "types.h"
+
 #include <brender/brender.h>
 
 #include "c2_hooks.h"
 
 br_renderer_facility* C2_HOOK_STDCALL RendererFacilitySoftAllocate(br_device* dev, const char* identifier);
+
+void C2_HOOK_CDECL _M_br_renderer_facility_soft_free(br_soft_renderer_facility* self);
 
 #endif //SOFTREND_FACILITY_H
