bf2209b62a2aa616afd576d1f5d46446399026e5
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 00:04:18 +0200

    Implement _M_br_soft_renderer_isType

--- src/brender/drivers/softrend/renderer.h
+++ src/brender/drivers/softrend/renderer.h
@@ -11,4 +11,6 @@ void C2_HOOK_CDECL _M_br_soft_renderer_free(br_soft_renderer* self);
 
 br_token C2_HOOK_CDECL _M_br_soft_renderer_type(br_soft_renderer* self);
 
+int C2_HOOK_CDECL _M_br_soft_renderer_isType(br_soft_renderer* self, br_token t);
+
 #endif /* SOFTREND_RENDERER_H */
