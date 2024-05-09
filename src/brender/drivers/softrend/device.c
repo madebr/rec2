ba2bba2f7435f404067fbffacbe32c681a0ac1e7
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-09 04:11:55 +0200

    Implement _M_br_softrend_device_free

--- src/brender/drivers/softrend/device.c
+++ src/brender/drivers/softrend/device.c
@@ -1,11 +1,45 @@
 #include "device.h"
 
 #include "core/fw/objectc.h"
+#include "core/fw/resource.h"
 #include "core/fw/token.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_INIT(struct br_device_dispatch, softrendr_deviceDispatch, 0x0058bcb0, {
-    FIXME
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    _M_br_softrend_device_free,
+    _M_br_softrend_object_identifier,
+    _M_br_softrend_device_type,
+    _M_br_softrend_device_isType,
+    _M_br_softrend_object_device,
+    _M_br_softrend_device_space,
+    _M_br_softrend_device_templateQuery,
+    _M_br_object_query,
+    _M_br_object_queryBuffer,
+    _M_br_object_queryMany,
+    _M_br_object_queryManySize,
+    _M_br_object_queryAll,
+    _M_br_object_queryAllSize,
+    _M_br_softrend_device_listQuery,
+    _M_br_object_container_tokensMatchBegin,
+    _M_br_object_container_tokensMatch,
+    _M_br_object_container_tokensMatchEnd,
+    _M_br_object_container_addFront,
+    _M_br_object_container_removeFront,
+    _M_br_object_container_remove,
+    _M_br_object_container_find,
+    _M_br_object_container_findMany,
+    _M_br_object_container_count,
 });
+
+void C2_HOOK_CDECL _M_br_softrend_device_free(br_soft_device* self) {
+
+    BrObjectContainerFree((br_object_container *)self, BR_NULL_TOKEN, NULL, NULL);
+    BrResFreeNoCallback(self);
+}
+C2_HOOK_FUNCTION(0x00540880, _M_br_softrend_device_free)
 
 br_device* (C2_HOOK_STDCALL * DeviceSoftAllocate_original)(const char* identifier);
 br_device* C2_HOOK_STDCALL DeviceSoftAllocate(const char* identifier) {
