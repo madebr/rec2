3b2c840b3614931caf0390af0932018564739eda
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-09 04:18:26 +0200

    Implement _M_br_softrend_device_space

--- src/brender/drivers/softrend/device.h
+++ src/brender/drivers/softrend/device.h
@@ -11,6 +11,8 @@ br_token C2_HOOK_CDECL _M_br_softrend_device_type(br_soft_device* self);
 
 int C2_HOOK_CDECL _M_br_softrend_device_isType(br_soft_device* self, br_token t);
 
+br_size_t C2_HOOK_CDECL _M_br_softrend_device_space(br_soft_device* self);
+
 br_device* C2_HOOK_STDCALL DeviceSoftAllocate(const char* identifier);
 
 #endif /* SOFTREND_DEVICE_H */
