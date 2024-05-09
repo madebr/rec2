a7b45d626ceb4d83a1cdfd4140c1f3c8131fe951
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-09 04:15:34 +0200

    Implement _M_br_softrend_device_isType

--- src/brender/drivers/softrend/device.h
+++ src/brender/drivers/softrend/device.h
@@ -9,6 +9,8 @@ void C2_HOOK_CDECL _M_br_softrend_device_free(br_soft_device* self);
 
 br_token C2_HOOK_CDECL _M_br_softrend_device_type(br_soft_device* self);
 
+int C2_HOOK_CDECL _M_br_softrend_device_isType(br_soft_device* self, br_token t);
+
 br_device* C2_HOOK_STDCALL DeviceSoftAllocate(const char* identifier);
 
 #endif /* SOFTREND_DEVICE_H */
