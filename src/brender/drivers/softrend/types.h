98076f111916cfa303623288cfd611e65b8f3a18
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-09 23:55:13 +0200

    Implement RendererSoftAllocate

--- src/brender/drivers/softrend/types.h
+++ src/brender/drivers/softrend/types.h
@@ -5,6 +5,8 @@
 
 typedef struct br_geometry_primitives br_geometry_primitives;
 typedef struct br_geometry_v1_buckets br_geometry_v1_buckets;
+typedef struct br_soft_renderer_state_stored br_soft_renderer_state_stored;
+typedef struct br_soft_renderer br_soft_renderer;
 
 struct device_templates {
     struct br_tv_template* deviceTemplate;
@@ -42,7 +44,9 @@ typedef struct  {
 } br_softrend_object;
 
 typedef struct soft_state_all {
-    br_uint_8 field_0x0[0x788];
+    br_soft_renderer* renderer;
+    br_uint_32 valid;
+    undefined field_0x0[0x780];
 } soft_state_all;
 
 typedef struct br_soft_renderer_facility {
@@ -58,5 +62,21 @@ typedef struct {
     br_object* dest;
     br_primitive_library* prims;
 }  newRendererTokens;
+
+#define MAX_STATE_STACK 32
+
+typedef struct br_soft_renderer {
+    br_renderer_dispatch* dispatch;
+    const char* identifier;
+    br_device* device;
+    void* object_list;
+    soft_state_all state;
+    soft_state_all* default_state;
+    br_soft_renderer_state_stored* last_restored;
+    br_primitive_library* plib;
+    soft_state_all state_stack[MAX_STATE_STACK];
+    br_uint_32 stack_top;
+    br_soft_renderer_facility* renderer_facility;
+} br_soft_renderer;
 
 #endif /* SOFTREND_TYPES_H */
