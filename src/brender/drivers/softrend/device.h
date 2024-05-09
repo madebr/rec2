aabe18b2838f96a80fb23b9371afe503e349ff07
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-09 04:11:55 +0200

    Implement _M_br_softrend_device_free

--- src/brender/drivers/softrend/device.h
+++ src/brender/drivers/softrend/device.h
@@ -5,6 +5,8 @@
 
 #include "c2_hooks.h"
 
+void C2_HOOK_CDECL _M_br_softrend_device_free(br_soft_device* self);
+
 br_device* C2_HOOK_STDCALL DeviceSoftAllocate(const char* identifier);
 
 #endif /* SOFTREND_DEVICE_H */
