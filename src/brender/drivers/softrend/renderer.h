72444c18c24f428651320c37ff4c29f520938674
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 00:00:39 +0200

    Implement _M_br_soft_renderer_free

--- src/brender/drivers/softrend/renderer.h
+++ src/brender/drivers/softrend/renderer.h
@@ -7,4 +7,6 @@
 
 br_renderer* C2_HOOK_STDCALL RendererSoftAllocate(br_device *dev, br_soft_renderer_facility *type, struct br_primitive_library *prims);
 
+void C2_HOOK_CDECL _M_br_soft_renderer_free(br_soft_renderer* self);
+
 #endif /* SOFTREND_RENDERER_H */
