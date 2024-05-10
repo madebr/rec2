c28141b9215389d8a288129112572c5c0198ef83
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 23:25:53 +0200

    Hook StateCopyToStored

--- src/brender/drivers/softrend/sstate.h
+++ src/brender/drivers/softrend/sstate.h
@@ -7,4 +7,6 @@
 
 br_soft_renderer_state_stored* C2_HOOK_STDCALL RendererStateStoredSoftAllocate(br_soft_renderer* renderer, soft_state_all* base_state, br_uint_32 m, br_token_value* tv);
 
+br_error C2_HOOK_STDCALL StateCopyToStored(br_soft_renderer_state_stored* dest, soft_state_all* src, br_uint_32 copy_mask, void* res);
+
 #endif /* SOFTREND_SSTATE_H */
