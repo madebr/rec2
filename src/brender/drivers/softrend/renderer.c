b5138443a297733e982e3c7151f8c4c9bb391aac
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 02:46:08 +0200

    Implement _M_br_soft_renderer_bufferStoredNew

--- src/brender/drivers/softrend/renderer.c
+++ src/brender/drivers/softrend/renderer.c
@@ -255,3 +255,9 @@ br_error C2_HOOK_CDECL _M_br_soft_renderer_stateStoredAvail(br_soft_renderer* se
     return 0x1002;
 }
 C2_HOOK_FUNCTION(0x00540c00, _M_br_soft_renderer_stateStoredAvail)
+
+br_error C2_HOOK_CDECL _M_br_soft_renderer_bufferStoredNew(br_soft_renderer* self, br_buffer_stored** psm, br_token use, br_device_pixelmap* pm, br_token_value* tv) {
+
+    return self->plib->dispatch->_bufferStoredNew(self->plib, psm, use, pm, tv);
+}
+C2_HOOK_FUNCTION(0x00540c10, _M_br_soft_renderer_bufferStoredNew)
--- src/brender/drivers/softrend/renderer.h
+++ src/brender/drivers/softrend/renderer.h
@@ -29,4 +29,6 @@ br_error C2_HOOK_CDECL _M_br_soft_renderer_stateStoredNew(br_soft_renderer* self
 
 br_error C2_HOOK_CDECL _M_br_soft_renderer_stateStoredAvail(br_soft_renderer* self, br_int_32* psize, br_uint_32 mask, br_token_value* tv);
 
+br_error C2_HOOK_CDECL _M_br_soft_renderer_bufferStoredNew(br_soft_renderer* self, br_buffer_stored** psm, br_token use, br_device_pixelmap* pm, br_token_value* tv);
+
 #endif /* SOFTREND_RENDERER_H */
