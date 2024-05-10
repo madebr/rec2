e839b82e1347f48615d69de115fd2b627802d1d6
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 23:34:08 +0200

    Hook StateCopyFromStored

--- src/brender/drivers/softrend/sstate.c
+++ src/brender/drivers/softrend/sstate.c
@@ -22,3 +22,14 @@ br_error C2_HOOK_STDCALL StateCopyToStored(br_soft_renderer_state_stored* dest, 
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00542520, StateCopyToStored, StateCopyToStored_original)
+
+br_error (C2_HOOK_STDCALL * StateCopyFromStored_original)(soft_state_all* dest, br_soft_renderer_state_stored* src, br_uint_32 copy_mask, void* res);
+br_error C2_HOOK_STDCALL StateCopyFromStored(soft_state_all* dest, br_soft_renderer_state_stored* src, br_uint_32 copy_mask, void* res) {
+
+#if defined(C2_HOOKS_ENABLED)
+    return StateCopyFromStored_original(dest, src, copy_mask, res);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00542630, StateCopyFromStored, StateCopyFromStored_original)
