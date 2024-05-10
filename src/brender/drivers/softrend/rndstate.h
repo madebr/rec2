03e539c68c28801dddcc739a7e960cc1bbb50fdb
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 23:47:44 +0200

    Hook _M_br_soft_renderer_boundsTestX

--- src/brender/drivers/softrend/rndstate.h
+++ src/brender/drivers/softrend/rndstate.h
@@ -51,4 +51,6 @@ br_error C2_HOOK_CDECL _M_br_soft_renderer_stateDefault(br_soft_renderer* self, 
 
 br_error C2_HOOK_CDECL _M_br_soft_renderer_boundsTestF(br_soft_renderer* self, br_token* r, br_bounds3_f* bounds);
 
+br_error C2_HOOK_CDECL _M_br_soft_renderer_boundsTestX(br_soft_renderer* self, br_token* r, br_bounds3_x* bounds_in);
+
 #endif /* SOFTREND_RNDSTATE_H */
