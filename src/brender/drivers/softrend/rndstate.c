f194497032d1926a2d65a527b97e80e3ad52415c
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 21:11:07 +0200

    Implement _M_br_soft_renderer_partSetMany

--- src/brender/drivers/softrend/rndstate.c
+++ src/brender/drivers/softrend/rndstate.c
@@ -46,3 +46,35 @@ br_error C2_HOOK_CDECL _M_br_soft_renderer_partSet(br_soft_renderer* self, br_to
     return r;
 }
 C2_HOOK_FUNCTION(0x00541830, _M_br_soft_renderer_partSet)
+
+br_error C2_HOOK_CDECL _M_br_soft_renderer_partSetMany(br_soft_renderer* self, br_token part, br_int_32 index, br_token_value* tv, br_int_32* pcount) {
+    br_error r;
+    soft_state_all* sp = &self->state;
+    br_tv_template* tp;
+    br_uint_32 m = 0;
+    br_int_32 c;
+
+    tp = FindStateTemplate(self, &sp, part, index);
+
+    if (tp != NULL) {
+        r = BrTokenValueSetMany(sp, pcount, &m, tv, tp);
+        if (m != 0) {
+            TemplateActions(sp, part, index, m);
+        }
+    } else {
+        r = CheckPrimitiveState(self);
+        if (r != 0) {
+            return r;
+        }
+        c = 0;
+        C2_HOOK_STATIC_ASSERT_STRUCT_OFFSET(br_primitive_state_dispatch, _partSetMany, 0x48);
+        r = self->state.pstate->dispatch->_partSetMany(self->state.pstate, part, index, tv, &c);
+
+        if (pcount != NULL) {
+            *pcount = c;
+        }
+    }
+
+    return r;
+}
+C2_HOOK_FUNCTION(0x005418f0, _M_br_soft_renderer_partSetMany)
