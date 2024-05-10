085e523e5d303afec8195e599bfad446ca2bfcf3
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-10 03:04:24 +0200

    Implement _M_br_soft_renderer_bufferStoredAvail

--- src/brender/drivers/softrend/renderer.h
+++ src/brender/drivers/softrend/renderer.h
@@ -31,4 +31,6 @@ br_error C2_HOOK_CDECL _M_br_soft_renderer_stateStoredAvail(br_soft_renderer* se
 
 br_error C2_HOOK_CDECL _M_br_soft_renderer_bufferStoredNew(br_soft_renderer* self, br_buffer_stored** psm, br_token use, br_device_pixelmap* pm, br_token_value* tv);
 
+br_error C2_HOOK_CDECL _M_br_soft_renderer_bufferStoredAvail(br_soft_renderer* self, br_int_32* space, br_token use, br_token_value* tv);
+
 #endif /* SOFTREND_RENDERER_H */
