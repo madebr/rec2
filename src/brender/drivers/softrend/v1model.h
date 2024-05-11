0c23b05dcc145d55fc9854083fcdbfd56e7a8e26
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-11 03:43:40 +0200

    Implement _M_br_geometry_v1_model_soft_render

--- src/brender/drivers/softrend/v1model.h
+++ src/brender/drivers/softrend/v1model.h
@@ -7,4 +7,6 @@
 
 br_error C2_HOOK_STDCALL V1Model_Render(br_geometry_v1_model_soft* self, br_renderer* renderer, v11model* model, br_renderer_state_stored* default_state, br_token type, br_boolean on_screen);
 
+br_error C2_HOOK_CDECL _M_br_geometry_v1_model_soft_render(br_geometry_v1_model_soft* self, br_renderer* renderer, v11model* model, br_renderer_state_stored* default_state, br_token type);
+
 #endif /* SOFTREND_V1MODEL_H */
