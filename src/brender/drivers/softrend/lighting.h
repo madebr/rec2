370714f6e756faad17b64c5f8c45f2bd59a7258e
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-13 03:50:33 +0200

    Implement _M_br_geometry_lighting_soft_isType

--- src/brender/drivers/softrend/lighting.h
+++ src/brender/drivers/softrend/lighting.h
@@ -11,4 +11,6 @@ void C2_HOOK_CDECL _M_br_geometry_lighting_soft_free(br_geometry_lighting_soft* 
 
 br_token C2_HOOK_CDECL _M_br_geometry_lighting_soft_type(br_geometry_lighting_soft* self);
 
+br_boolean C2_HOOK_CDECL _M_br_geometry_lighting_soft_isType(br_geometry_lighting_soft* self, br_token t);
+
 #endif /* SOFTREND_LIGHTING_H */
