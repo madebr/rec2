3ca5a8eaedbdcdc42178a83e4a5efb89619cd654
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-13 04:10:34 +0200

    Hook _M_br_geometry_lighting_soft_render

--- src/brender/drivers/softrend/lighting.h
+++ src/brender/drivers/softrend/lighting.h
@@ -17,4 +17,6 @@ br_int_32 C2_HOOK_CDECL _M_br_geometry_lighting_soft_space(br_geometry_lighting_
 
 br_tv_template* C2_HOOK_CDECL _M_br_geometry_lighting_soft_templateQuery(br_geometry_lighting_soft* self);
 
+br_error C2_HOOK_CDECL _M_br_geometry_lighting_soft_render(br_geometry_lighting_soft* self, br_soft_renderer* renderer, br_vector3* points, br_vector3* normals, br_colour* colour_in, br_colour* colour_out, br_uint_16* redirect, int pstride, int nstride, int cinstride, int coutstride, int nvertices);
+
 #endif /* SOFTREND_LIGHTING_H */
