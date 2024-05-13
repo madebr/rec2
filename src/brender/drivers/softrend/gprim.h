26f704cbe48a86646d0deaa4ebc2ffc4caa73d13
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-13 18:46:27 +0200

    Implement _M_br_geometry_primitives_soft_storedNew

--- src/brender/drivers/softrend/gprim.h
+++ src/brender/drivers/softrend/gprim.h
@@ -25,4 +25,6 @@ br_error C2_HOOK_CDECL _M_br_geometry_primitives_soft_renderOnScreen(br_geometry
 
 br_error C2_HOOK_CDECL _M_br_geometry_primitives_soft_renderOnScreenFixedToFloat(br_geometry_primitives_soft* self, br_renderer* renderer, fmt_vertex* vertices, int nvertices, br_token type);
 
+br_error C2_HOOK_CDECL _M_br_geometry_primitives_soft_storedNew(br_geometry_primitives_soft* self, br_renderer *renderer, br_geometry_stored** psg, fmt_vertex* vertices, int nvertices, br_token type, br_token_value* tv);
+
 #endif /* SOFTREND_GPRIM_H */
