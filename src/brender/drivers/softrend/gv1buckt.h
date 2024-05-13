21a2c81d074e73557601255a4aec675c11f4eb59
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-13 02:53:45 +0200

    Implement _M_br_geometry_v1_buckets_soft_space

--- src/brender/drivers/softrend/gv1buckt.h
+++ src/brender/drivers/softrend/gv1buckt.h
@@ -13,4 +13,6 @@ br_token C2_HOOK_CDECL _M_br_geometry_v1_buckets_soft_type(br_geometry_v1_bucket
 
 br_boolean C2_HOOK_CDECL _M_br_geometry_v1_buckets_soft_isType(br_geometry_v1_buckets_soft* self, br_token t);
 
+br_int_32 C2_HOOK_CDECL _M_br_geometry_v1_buckets_soft_space(br_geometry_v1_buckets_soft* self);
+
 #endif /* SOFTREND_GV1BUCKT_H */
