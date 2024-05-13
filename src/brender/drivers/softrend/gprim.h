c64c47215313fce0cf1adbb712da06bf4d007547
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-13 18:10:08 +0200

    Implement _M_br_geometry_primitives_soft_free

--- src/brender/drivers/softrend/gprim.h
+++ src/brender/drivers/softrend/gprim.h
@@ -7,4 +7,6 @@
 
 br_geometry_primitives* C2_HOOK_STDCALL GeometryPrimitivesAllocate(br_soft_renderer_facility* type, const char* id);
 
+void C2_HOOK_CDECL _M_br_geometry_primitives_soft_free(br_geometry_primitives_soft* self);
+
 #endif /* SOFTREND_GPRIM_H */
