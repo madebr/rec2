559343ace2044e13056ca9c5d3e5108814705ab4
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-13 18:33:26 +0200

    Implement _M_br_geometry_primitives_soft_templateQuery

--- src/brender/drivers/softrend/gprim.c
+++ src/brender/drivers/softrend/gprim.c
@@ -1,6 +1,7 @@
 #include "gprim.h"
 
 #include "core/fw/resource.h"
+#include "core/fw/tokenval.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_INIT(const br_geometry_primitives_dispatch, geometryPrimitivesDispatch, 0x0058bfd8, {
     NULL,
@@ -27,6 +28,11 @@ C2_HOOK_VARIABLE_IMPLEMENT_INIT(const br_geometry_primitives_dispatch, geometryP
     _M_br_br_geometry_primitives_soft_storedNew,
     _M_br_br_geometry_primitives_soft_storedNewFixedToFloat,
     _M_br_br_geometry_primitives_soft_storedAvail,
+});
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY_INIT(br_tv_template_entry, geometryPrimitivesTemplateEntries, 3, 0x0058bf90, {
+    { BRT_IDENTIFIER_CSTR,      NULL, offsetof(br_geometry_primitives_dispatch , identifier),           5,  3,  0,  0, },
+    { BRT_RENDERER_FACILITY_O,  NULL, offsetof(br_geometry_primitives_dispatch , renderer_facility),    5,  3,  0,  0, },
+    { BRT_FACILITY_O,           NULL, offsetof(br_geometry_primitives_dispatch , renderer_facility),    1,  3,  0,  0, },
 });
 
 br_geometry_primitives* (C2_HOOK_STDCALL * GeometryPrimitivesAllocate_original)(br_soft_renderer_facility* type, const char* id);
@@ -81,3 +87,16 @@ br_int_32 C2_HOOK_CDECL _M_br_geometry_primitives_soft_space(br_geometry_primiti
     return sizeof(br_geometry_primitives_soft);
 }
 C2_HOOK_FUNCTION(0x00541200, _M_br_geometry_primitives_soft_space)
+
+br_tv_template* C2_HOOK_CDECL _M_br_geometry_primitives_soft_templateQuery(br_geometry_primitives_soft* self) {
+
+    C2_HOOK_STATIC_ASSERT_STRUCT_OFFSET(br_soft_device , templates.geometryPrimitivesTemplate, 0x2c);
+    C2_HOOK_BUG_ON(BR_ASIZE(C2V(geometryPrimitivesTemplateEntries)) != 3);
+    if (self->device->templates.geometryPrimitivesTemplate == NULL) {
+        self->device->templates.geometryPrimitivesTemplate = BrTVTemplateAllocate(self->device,
+            C2V(geometryPrimitivesTemplateEntries),
+            BR_ASIZE(C2V(geometryPrimitivesTemplateEntries)));
+    }
+    return self->device->templates.geometryPrimitivesTemplate;
+}
+C2_HOOK_FUNCTION(0x00541210, _M_br_geometry_primitives_soft_templateQuery)
