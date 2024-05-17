7471a2d31cc597295cb673446247099f3b502f62
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-17 22:14:30 +0200

    Hook ActiveLightsFind

--- src/brender/drivers/softrend/setup.h
+++ src/brender/drivers/softrend/setup.h
@@ -15,4 +15,6 @@ void C2_HOOK_STDCALL StaticCacheUpdate_PerModel(br_soft_renderer *self);
 
 br_error C2_HOOK_STDCALL CacheUpdate(br_soft_renderer* self);
 
+void C2_HOOK_STDCALL ActiveLightsFind(br_soft_renderer* self);
+
 #endif /* SOFTREND_SETUP_H */
