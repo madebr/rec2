167e83dee6a78b55331c11d00d1f3247b689fe0f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-18 01:26:21 +0200

    Implement lightingColourNull

--- src/brender/drivers/softrend/light24.h
+++ src/brender/drivers/softrend/light24.h
@@ -5,6 +5,8 @@
 
 #include "c2_hooks.h"
 
+void C2_HOOK_STDCALL lightingColourNull(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp);
+
 void C2_HOOK_STDCALL ActiveLightAccumulateColourSet(active_light* alp);
 
 #endif /* SOFTREND_LIGHT24_H */
