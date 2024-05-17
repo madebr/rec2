5d1ff109fd96839b7d93f9632004d9afedabce0e
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-17 22:07:58 +0200

    Hook StaticCacheUpdate_PerModel

--- src/brender/drivers/softrend/setup.c
+++ src/brender/drivers/softrend/setup.c
@@ -13,3 +13,14 @@ void C2_HOOK_STDCALL StaticCacheUpdate_PerScene(br_soft_renderer* self) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00543a40, StaticCacheUpdate_PerScene, StaticCacheUpdate_PerScene_original)
+
+void (C2_HOOK_STDCALL * StaticCacheUpdate_PerModel_original)(br_soft_renderer* self);
+void C2_HOOK_STDCALL StaticCacheUpdate_PerModel(br_soft_renderer* self) {
+
+#if defined(C2_HOOKS_ENABLED)
+    StaticCacheUpdate_PerModel_original(self);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00543a80, StaticCacheUpdate_PerModel, StaticCacheUpdate_PerModel_original)
