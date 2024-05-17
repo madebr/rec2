a89ef695cd23dcde0c6be87c44a362c413d5483d
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-17 22:08:21 +0200

    Hook CacheUpdate

--- src/brender/drivers/softrend/setup.c
+++ src/brender/drivers/softrend/setup.c
@@ -24,3 +24,14 @@ void C2_HOOK_STDCALL StaticCacheUpdate_PerModel(br_soft_renderer* self) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00543a80, StaticCacheUpdate_PerModel, StaticCacheUpdate_PerModel_original)
+
+br_error (C2_HOOK_STDCALL * CacheUpdate_original)(br_soft_renderer* self);
+br_error C2_HOOK_STDCALL CacheUpdate(br_soft_renderer* self) {
+
+#if defined(C2_HOOKS_ENABLED)
+    return CacheUpdate_original(self);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00543d80, CacheUpdate, CacheUpdate_original)
