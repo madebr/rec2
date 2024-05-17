b111b717edb2db22378970be114807f1a6a16036
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-18 01:33:32 +0200

    Hook lightingColourLocal2

--- src/brender/drivers/softrend/light24.c
+++ src/brender/drivers/softrend/light24.c
@@ -16,6 +16,17 @@ void C2_HOOK_STDCALL lightingColourDirectSpecular(br_soft_renderer* self, br_vec
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x005491b0, lightingColourDirectSpecular, lightingColourDirectSpecular_original)
 
+void (C2_HOOK_STDCALL * lightingColourLocal2_original)(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp);
+void C2_HOOK_STDCALL lightingColourLocal2(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp) {
+
+#if defined(C2_HOOKS_ENABLED)
+    lightingColourLocal2_original(self, p, n, param_4, alp, comp);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00549380, lightingColourLocal2, lightingColourLocal2_original)
+
 void (C2_HOOK_STDCALL * ActiveLightAccumulateColourSet_original)(active_light* alp);
 void C2_HOOK_STDCALL ActiveLightAccumulateColourSet(active_light* alp) {
 
