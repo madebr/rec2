1d8fd7816d9d2c39f7507ae478b8f3527906c99f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-17 23:35:34 +0200

    Hook ActiveLightAccumulateIndexSet

---
+++ src/brender/drivers/softrend/light8.h
@@ -0,0 +1,10 @@
+#ifndef SOFTREND_LIGHT8_H
+#define SOFTREND_LIGHT8_H
+
+#include "types.h"
+
+#include "c2_hooks.h"
+
+void C2_HOOK_STDCALL ActiveLightAccumulateIndexSet(active_light* alp);
+
+#endif /* SOFTREND_LIGHT8_H */
