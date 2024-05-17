a3dae50e6f235c359f5add024b40a35f716bea17
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-17 22:07:31 +0200

    Hook StaticCacheUpdate_PerScene

--- src/brender/drivers/softrend/setup.c
+++ src/brender/drivers/softrend/setup.c
@@ -1,3 +1,15 @@
 #include "setup.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT(rend_block_soft, rend, 0x0079f980);
+
+
+void (C2_HOOK_STDCALL * StaticCacheUpdate_PerScene_original)(br_soft_renderer* self);
+void C2_HOOK_STDCALL StaticCacheUpdate_PerScene(br_soft_renderer* self) {
+
+#if defined(C2_HOOKS_ENABLED)
+    StaticCacheUpdate_PerScene_original(self);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00543a40, StaticCacheUpdate_PerScene, StaticCacheUpdate_PerScene_original)
