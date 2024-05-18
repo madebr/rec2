63abe01008d03650ff6280d3ff4d441b89a9eaa6
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-18 03:57:52 +0200

    Hook lightingIndexPoint

--- src/brender/drivers/softrend/light8.c
+++ src/brender/drivers/softrend/light8.c
@@ -16,6 +16,17 @@ void C2_HOOK_STDCALL lightingIndexDirect(br_soft_renderer* self, br_vector3* p, 
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0054a7c0, lightingIndexDirect, lightingIndexDirect_original)
 
+void (C2_HOOK_STDCALL * lightingIndexPoint_original)(br_soft_renderer* self, br_vector3* p, br_vector3* n, active_light* alp, br_scalar* comp);
+void C2_HOOK_STDCALL lightingIndexPoint(br_soft_renderer* self, br_vector3* p, br_vector3* n, active_light* alp, br_scalar* comp) {
+
+#if defined(C2_HOOKS_ENABLED)
+    lightingIndexPoint_original(self, p, n, alp, comp);
+#else
+#error "Not implemented"
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x0054a870, lightingIndexPoint, lightingIndexPoint_original)
+
 void (C2_HOOK_STDCALL * ActiveLightAccumulateIndexSet_original)(active_light* alp);
 void C2_HOOK_STDCALL ActiveLightAccumulateIndexSet(active_light* alp) {
 
