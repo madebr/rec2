b379ac1d3768123ea9999812f56d0552a97d9f54
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-18 03:56:26 +0200

    Implement lightingIndexNull

--- src/brender/drivers/softrend/light8.c
+++ src/brender/drivers/softrend/light8.c
@@ -1,5 +1,10 @@
 #include "light8.h"
 
+void C2_HOOK_STDCALL lightingIndexNull(br_soft_renderer* self, br_vector3* p, br_vector3* n, active_light* alp, br_scalar* comp) {
+
+}
+C2_HOOK_FUNCTION(0x0054a7b0, lightingIndexNull)
+
 void (C2_HOOK_STDCALL * ActiveLightAccumulateIndexSet_original)(active_light* alp);
 void C2_HOOK_STDCALL ActiveLightAccumulateIndexSet(active_light* alp) {
 
