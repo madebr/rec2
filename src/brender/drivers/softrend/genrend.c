42c83ef3c7522161dee7252787beaec872feafc9
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-19 21:08:34 +0200

    Implement Vertex_TransformProjectOutcodeBounds

--- src/brender/drivers/softrend/genrend.c
+++ src/brender/drivers/softrend/genrend.c
@@ -1,8 +1,39 @@
 #include "genrend.h"
 
+#include "rend.h"
 #include "setup.h"
 
 #include "core/fw/scratch.h"
+
+void C2_HOOK_CDECL Vertex_TransformProjectOutcodeBounds(br_geometry* self, br_soft_renderer* renderer) {
+    int v;
+    brp_vertex* tvp;
+
+    for (v = 0; v < C2V(rend).nvertices; v++) {
+        tvp = &C2V(rend).temp_vertices[v];
+
+        if (C2V(rend).vertex_counts[v] == 0) {
+            continue;
+        }
+
+        C2_HOOK_BUG_ON((OUTCODE_RIGHT | OUTCODE_N_RIGHT) != 0x20002);
+        C2_HOOK_BUG_ON((OUTCODE_LEFT | OUTCODE_N_LEFT) != 0x10001);
+        C2_HOOK_BUG_ON((OUTCODE_TOP | OUTCODE_N_TOP) != 0x40004);
+        C2_HOOK_BUG_ON((OUTCODE_BOTTOM | OUTCODE_N_BOTTOM) != 0x80008);
+        C2_HOOK_BUG_ON((OUTCODE_HITHER | OUTCODE_N_HITHER) != 0x100010);
+        C2_HOOK_BUG_ON((OUTCODE_YON | OUTCODE_N_YON) != 0x200020);
+        C2_HOOK_BUG_ON((OUTCODE_USER | OUTCODE_N_USER) != 0x400040);
+
+        TRANSFORM_VERTEX((br_vector4*)(tvp->comp + C_X), &C2V(rend).vertices[v].p, &C2V(scache).model_to_screen);
+        OUTCODE_POINT(tvp->flags, (br_vector4 *)(tvp->comp + C_X));
+
+        if (!(tvp->flags & OUTCODES_ALL)) {
+            PROJECT_VERTEX(tvp, tvp->comp[C_X], tvp->comp[C_Y], tvp->comp[C_Z], tvp->comp[C_W]);
+            UPDATE_BOUNDS(tvp);
+        }
+    }
+}
+C2_HOOK_FUNCTION(0x00547810, Vertex_TransformProjectOutcodeBounds)
 
 void C2_HOOK_CDECL ScratchFree(br_geometry* self, br_soft_renderer* renderer) {
 
--- src/brender/include/brender/br_defs.h
+++ src/brender/include/brender/br_defs.h
@@ -13,6 +13,7 @@
 
 #define BR_MAC3(a, b, c, d, e, f) ((a) * (b) + (c) * (d) + (e) * (f))
 #define BR_SQR3(a, b, c) ((a) * (a) + (b) * (b) + (c) * (c))
+#define BR_MAC4(a, b, c, d, e, f, g, h) ((a) * (b) + (c) * (d) + (e) * (f) + (g) * (h))
 
 #define BrAngleToRadian(a) ((br_scalar)((a) * (PI / 32768.0)))      // a * 0.00009587379924285257
 #define BrRadianToAngle(r) ((br_angle)(long)((r) * (32768.0 / PI))) // r * 10430.378350470453
