da851d0a8e90a3dc8f9f7744ba8ccb0daa653328
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-19 18:43:37 +0200

    Implement Vertex_ClearFlags

---
+++ src/brender/drivers/softrend/genrend.h
@@ -0,0 +1,10 @@
+#ifndef SOFTREND_GENREND_H
+#define SOFTREND_GENREND_H
+
+#include "types.h"
+
+#include "c2_hooks.h"
+
+void C2_HOOK_CDECL Vertex_ClearFlags(br_geometry* self, br_soft_renderer* renderer);
+
+#endif /* SOFTREND_GENREND_H */
