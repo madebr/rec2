8bccc1365d7e3902a1509e4ca8c8f95041e08a88
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-19 18:45:32 +0200

    Implement ScratchFree

--- src/brender/drivers/softrend/genrend.c
+++ src/brender/drivers/softrend/genrend.c
@@ -2,6 +2,15 @@
 
 #include "setup.h"
 
+#include "core/fw/scratch.h"
+
+void C2_HOOK_CDECL ScratchFree(br_geometry* self, br_soft_renderer* renderer) {
+
+    BrScratchFree(C2V(rend).scratch);
+    C2V(rend).scratch = NULL;
+}
+C2_HOOK_FUNCTION(0x005476a0, ScratchFree)
+
 void C2_HOOK_CDECL Vertex_ClearFlags(br_geometry* self, br_soft_renderer* renderer) {
     int v;
 
