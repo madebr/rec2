d49972946d6468988a229cc576859b845482dff4
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-19 21:08:34 +0200

    Implement Vertex_TransformProjectOutcodeBounds

--- src/brender/drivers/softrend/types.h
+++ src/brender/drivers/softrend/types.h
@@ -112,15 +112,42 @@ typedef struct {
     br_timestamp timestamp;
 } soft_state_cull;
 
+enum {
+    ENBL_BOUNDS_BIT = 4,
+};
+
+enum {
+    ENBL_BOUNDS = (1 << ENBL_BOUNDS_BIT),
+};
+
 #define TFF_REVERSED 1
 #define TFF_CLIPPED  2
 #define TFF_VISIBLE  4
 
-#define TVDIR_FRONT	0x80000000
-#define TVDIR_BACK	0x40000000
+#define TVDIR_FRONT 0x80000000
+#define TVDIR_BACK  0x40000000
 
-#define OUTCODES_ALL		0x00000FFF
-#define OUTCODES_NOT		0x0FFF0000
+#define OUTCODE_LEFT        0x00000001
+#define OUTCODE_RIGHT       0x00000002
+#define OUTCODE_TOP         0x00000004
+#define OUTCODE_BOTTOM      0x00000008
+#define OUTCODE_HITHER      0x00000010
+#define OUTCODE_YON         0x00000020
+
+#define OUTCODE_USER        0x00000040
+
+#define OUTCODES_ALL        0x00000fff
+
+#define OUTCODE_N_LEFT      0x00010000
+#define OUTCODE_N_RIGHT     0x00020000
+#define OUTCODE_N_TOP       0x00040000
+#define OUTCODE_N_BOTTOM    0x00080000
+#define OUTCODE_N_HITHER    0x00100000
+#define OUTCODE_N_YON       0x00200000
+
+#define OUTCODE_N_USER      0x00400000
+
+#define OUTCODES_NOT        0x0fff0000
 
 typedef struct {
     br_uint_16 codes;
