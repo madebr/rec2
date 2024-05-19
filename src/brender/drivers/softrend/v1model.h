8b7d9a291dfeb81e380491f9d8bc7382eb5bd557
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-19 17:43:09 +0200

    Implement V1Face_Render

--- src/brender/drivers/softrend/v1model.h
+++ src/brender/drivers/softrend/v1model.h
@@ -29,6 +29,8 @@ void C2_HOOK_CDECL V1Face_OS_CullTwoSided(br_geometry* self, br_soft_renderer* r
 
 void C2_HOOK_CDECL V1Face_Outcode(br_geometry* self, br_soft_renderer* renderer);
 
+void C2_HOOK_CDECL V1Face_Render(br_geometry* self, br_soft_renderer* renderer);
+
 br_error C2_HOOK_STDCALL V1Model_Render(br_geometry_v1_model_soft* self, br_renderer* renderer, v11model* model, br_renderer_state_stored* default_state, br_token type, br_boolean on_screen);
 
 br_error C2_HOOK_CDECL _M_br_geometry_v1_model_soft_render(br_geometry_v1_model_soft* self, br_renderer* renderer, v11model* model, br_renderer_state_stored* default_state, br_token type);
