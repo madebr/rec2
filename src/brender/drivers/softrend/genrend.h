3c74f8a6dc1d32a658e66f6dd3b622c2efeeed9c
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-19 22:10:10 +0200

    Implement Vertex_OS_TransformProjectGeom

--- src/brender/drivers/softrend/genrend.h
+++ src/brender/drivers/softrend/genrend.h
@@ -19,6 +19,8 @@ void C2_HOOK_CDECL Vertex_OS_TransformProjectSurf(br_geometry* self, br_soft_ren
 
 void C2_HOOK_CDECL Vertex_OS_TransformProjectBoundsGeom(br_geometry* self, br_soft_renderer* renderer);
 
+void C2_HOOK_CDECL Vertex_OS_TransformProjectGeom(br_geometry* self, br_soft_renderer* renderer);
+
 void C2_HOOK_CDECL ScratchFree(br_geometry* self, br_soft_renderer* renderer);
 
 void C2_HOOK_CDECL Vertex_ClearFlags(br_geometry* self, br_soft_renderer* renderer);
