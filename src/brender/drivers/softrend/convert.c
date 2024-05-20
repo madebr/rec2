7e46a3df188e61444e59ddd1eadc2f0633239923
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-20 22:36:30 +0200

    Implement RenderConvert1

--- src/brender/drivers/softrend/convert.c
+++ src/brender/drivers/softrend/convert.c
@@ -57,3 +57,48 @@ void C2_HOOK_STDCALL ConvertVertex(brp_vertex* dest, brp_vertex* src) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x005460e0, ConvertVertex, ConvertVertex_original)
+
+void C2_HOOK_CDECL RenderConvert1(brp_block* block, brp_vertex* v0) {
+    int c;
+    br_uint_32 m;
+    brp_vertex outv[1];
+
+    m = C2V(rend).block->convert_mask_f;
+    if (m != 0) {
+        for (c = 0; m != 0; c++, m >>= 1) {
+            if (m & 1) {
+                outv[0].comp_f[c] = FCONV(v0->comp[c]);
+            }
+        }
+    }
+
+    m = C2V(rend).block->convert_mask_x;
+    if (m != 0) {
+        for (c = 0; m != 0; c++, m >>= 1) {
+            if (m & 1) {
+                outv[0].comp_x[c] = XCONV(v0->comp[c]);
+            }
+        }
+    }
+
+    m = C2V(rend).block->convert_mask_i;
+    if (m != 0) {
+        for (c = 0; m != 0; c++, m >>= 1) {
+            if (m & 1) {
+                outv[0].comp_i[c] = ICONV(v0->comp[c]);
+            }
+        }
+    }
+
+    m = 0x0000ffff ^ (C2V(rend).block->convert_mask_i | C2V(rend).block->convert_mask_x | C2V(rend).block->convert_mask_f);
+    if (m != 0) {
+        for (c = 0; m != 0; c++, m >>= 1) {
+            if (m & 1) {
+                outv[0].comp_i[c] = v0->comp_i[c];
+            }
+        }
+    }
+
+    block->chain->render(block->chain, outv);
+}
+C2_HOOK_FUNCTION(0x005461b0, RenderConvert1)
