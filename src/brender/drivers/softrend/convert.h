2185db2ca27f270b65f056d4f252fc90aae113ba
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-20 22:41:28 +0200

    Implement RenderConvert2

--- src/brender/drivers/softrend/convert.h
+++ src/brender/drivers/softrend/convert.h
@@ -9,4 +9,6 @@ void C2_HOOK_STDCALL ConvertVertex(brp_vertex* dest, brp_vertex* src);
 
 void C2_HOOK_CDECL RenderConvert1(brp_block* block, brp_vertex* v0);
 
+void C2_HOOK_CDECL RenderConvert2(brp_block* block, brp_vertex* v0, brp_vertex* v1);
+
 #endif /* SOFTREND_CONVERT_H */
