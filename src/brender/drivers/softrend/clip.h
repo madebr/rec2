e89fa4f70375d90f780d5f22aa4afe28915a0604
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-21 00:19:59 +0200

    Implement ClipFaceToMinus1

--- src/brender/drivers/softrend/clip.h
+++ src/brender/drivers/softrend/clip.h
@@ -5,6 +5,10 @@
 
 #include "c2_hooks.h"
 
+int ClipFaceToPlane(brp_vertex* vp, brp_vertex* verts_out, int num_in, br_vector4* plane, int cmask);
+
 int C2_HOOK_STDCALL ClipFaceToPlus1(brp_vertex* vp, brp_vertex* verts_out, int num_in, int axis, int cmask);
+
+int C2_HOOK_STDCALL ClipFaceToMinus1(brp_vertex* vp, brp_vertex* verts_out, int num_in, int axis, int cmask);
 
 #endif /* SOFTREND_CLIP_H */
