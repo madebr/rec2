6df2312b7eb8dbd506100dda5d7391f3d87058a4
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-21 03:52:16 +0200

    Implement OpTriangleTwoSidedConstantSurf

--- src/brender/drivers/softrend/faceops.h
+++ src/brender/drivers/softrend/faceops.h
@@ -11,4 +11,6 @@ void C2_HOOK_CDECL OpTriangleClipConstantSurf(brp_block* block, brp_vertex* v0, 
 
 void C2_HOOK_CDECL OpTriangleConstantSurf(brp_block* block, brp_vertex* v0, brp_vertex* v1, brp_vertex* v2, v11face* face, br_uint_16* fp_vertices);
 
+void C2_HOOK_CDECL OpTriangleTwoSidedConstantSurf(brp_block* block, brp_vertex* v0, brp_vertex* v1, brp_vertex* v2, v11face* face, temp_face_soft* tfp);
+
 #endif
