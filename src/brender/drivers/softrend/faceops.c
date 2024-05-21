0a889dbdb594085c07b8e77be68d0afe42781635
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-21 03:28:08 +0200

    Implement OpTriangleClipConstantSurf

--- src/brender/drivers/softrend/faceops.c
+++ src/brender/drivers/softrend/faceops.c
@@ -22,3 +22,41 @@ void C2_HOOK_CDECL OpTriangleClip(brp_block* block, brp_vertex* v0, brp_vertex* 
     }
 }
 C2_HOOK_FUNCTION(0x00545120, OpTriangleClip)
+
+void C2_HOOK_CDECL OpTriangleClipConstantSurf(brp_block* block, brp_vertex* v0, brp_vertex* v1, brp_vertex* v2, v11face* face, temp_face_soft* tfp) {
+    brp_vertex clip_in[3];
+    int nclipped,i;
+    brp_vertex* clipped;
+    fmt_vertex* vp;
+    br_colour colour = C2V(scache).colour;
+    br_vector3 rev_normal;
+
+    clip_in[0] = *v0;
+    clip_in[1] = *v1;
+    clip_in[2] = *v2;
+
+    clipped = FaceClip(C2V(rend).renderer, clip_in, C2V(rend).renderer->state.cache.clip_slots, tfp->codes, 3, &nclipped);
+    if (clipped != NULL) {
+        vp = &C2V(rend).vertices[face->vertices[0]];
+
+        if (C2V(rend).renderer->state.surface.colour_source == BRT_GEOMETRY) {
+            colour = C2V(rend).face_colours[face - C2V(rend).faces];
+        }
+        if (tfp->flag & TFF_REVERSED) {
+            BrVector3Negate(&rev_normal, &face->eqn);
+
+            for (i = 0; i < C2V(rend).renderer->state.cache.nconstant_fns; i++) {
+                C2V(rend).renderer->state.cache.constant_fns[i]((br_renderer*)C2V(rend).renderer, &vp->p, &vp->map, &rev_normal, colour, clipped->comp);
+            }
+
+        } else {
+            for (i = 0; i < C2V(rend).renderer->state.cache.nconstant_fns; i++) {
+                C2V(rend).renderer->state.cache.constant_fns[i]((br_renderer*)C2V(rend).renderer, &vp->p, &vp->map, (br_vector3*)&face->eqn, colour, clipped->comp);
+            }
+        }
+
+        /* FIXME: are last argument types correct? */
+        ClippedRenderTriangles(C2V(rend).renderer, block->chain, clipped, nclipped, face->vertices, &tfp->codes);
+    }
+}
+C2_HOOK_FUNCTION(0x005451c0, OpTriangleClipConstantSurf)
