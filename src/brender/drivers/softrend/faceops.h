d084836ac44c14af0a47e6c85b275798ce37ec13
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-05-21 03:28:08 +0200

    Implement OpTriangleClipConstantSurf

--- src/brender/drivers/softrend/faceops.h
+++ src/brender/drivers/softrend/faceops.h
@@ -7,4 +7,6 @@
 
 void C2_HOOK_CDECL OpTriangleClip(brp_block* block, brp_vertex* v0, brp_vertex* v1, brp_vertex* v2, br_uint_16* fp_vertices, br_uint_16* fp_edges);
 
+void C2_HOOK_CDECL OpTriangleClipConstantSurf(brp_block* block, brp_vertex* v0, brp_vertex* v1, brp_vertex* v2, v11face* face, temp_face_soft* tfp);
+
 #endif
