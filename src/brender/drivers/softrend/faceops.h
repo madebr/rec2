d6bfdabe2a1c7282c55277d4f1f31cc3ce099b8b
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-06-09 02:23:18 +0200

    Implement OpTriangleSubdivideOnScreen

--- src/brender/drivers/softrend/faceops.h
+++ src/brender/drivers/softrend/faceops.h
@@ -43,4 +43,6 @@ void C2_HOOK_STDCALL triangleSubdivideCheck(int depth, brp_block* block, brp_ver
 
 void C2_HOOK_CDECL OpTriangleSubdivide(brp_block* block, brp_vertex* v0, brp_vertex* v1, brp_vertex* v2, br_uint_16* fp_vertices, br_uint_16* fp_edges);
 
+void C2_HOOK_CDECL OpTriangleSubdivideOnScreen(brp_block* block, brp_vertex* v0, brp_vertex* v1, brp_vertex* v2, br_uint_16* fp_vertices, br_uint_16* fp_edges);
+
 #endif
