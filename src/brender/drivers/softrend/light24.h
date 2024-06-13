77ea6ce08c22d6f21b8a95f47d733bb1596642c9
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-06-13 04:46:21 +0200

    Implement SurfaceColourZero

--- src/brender/drivers/softrend/light24.h
+++ src/brender/drivers/softrend/light24.h
@@ -5,6 +5,8 @@
 
 #include "c2_hooks.h"
 
+void C2_HOOK_CDECL SurfaceColourZero(br_soft_renderer* self, br_vector3* p, br_vector2* map, br_vector3* n, br_colour colour, br_scalar* comp);
+
 void C2_HOOK_STDCALL lightingColourNull(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp);
 
 void C2_HOOK_STDCALL lightingColourDirectSpecular(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp);
