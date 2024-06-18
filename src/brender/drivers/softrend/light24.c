2fe7572694a2e6a59275440ee83cf64c0a50a2e3
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-06-18 21:39:02 +0200

    Implement lightingColourPointSpecular

--- src/brender/drivers/softrend/light24.c
+++ src/brender/drivers/softrend/light24.c
@@ -153,13 +153,46 @@ void C2_HOOK_STDCALL lightingColourLocal1(br_soft_renderer* self, br_vector3* p,
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00549510, lightingColourLocal1, lightingColourLocal1_original)
 
-void (C2_HOOK_STDCALL * lightingColourPointSpecular_original)(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp);
-void C2_HOOK_STDCALL lightingColourPointSpecular(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp) {
+void (C2_HOOK_STDCALL * lightingColourPointSpecular_original)(br_soft_renderer* self, br_vector3* p, br_vector3* n, br_colour colour, active_light* alp, br_scalar* comp);
+void C2_HOOK_STDCALL lightingColourPointSpecular(br_soft_renderer* self, br_vector3* p, br_vector3* n, br_colour colour, active_light* alp, br_scalar* comp) {
 
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     lightingColourPointSpecular_original(self, p, n, param_4, alp, comp);
 #else
-#error "Not implemented"
+    br_scalar dot;
+    br_vector3 dirn;
+    br_scalar red, grn, blu;
+
+    BrVector3Sub(&dirn, &alp->position, p);
+    BrVector3Normalise(&dirn, &dirn);
+
+    dot = BrVector3Dot(&dirn, n);
+    if (dot <= 0.f) {
+        return;
+    }
+    red = BrFixedToScalar(BR_RED(colour) << 8) * self->state.surface.kd * dot;
+    grn = BrFixedToScalar(BR_GRN(colour) << 8) * self->state.surface.kd * dot;
+    blu = BrFixedToScalar(BR_BLU(colour) << 8) * self->state.surface.kd * dot;
+
+    if (self->state.surface.ks != 0.f) {
+        br_vector3 tmp;
+
+        BrVector3Scale(&tmp, n, 2 * dot);
+        BrVector3Sub(&tmp, &tmp, &dirn);
+        dot = BrVector3Dot(&tmp , &C2V(rend).eye_l);
+        if (dot > SPECULARPOW_CUTOFF) {
+            br_scalar specular;
+
+            specular = SPECULAR_POWER(self->state.surface.ks);
+            red += specular;
+            grn += specular;
+            blu += specular;
+        }
+    }
+
+    comp[C_R] += red * alp->intensity * BrFixedToFloat(BR_RED(alp->s->colour) << 8);
+    comp[C_G] += grn * alp->intensity * BrFixedToFloat(BR_GRN(alp->s->colour) << 8);
+    comp[C_B] += blu * alp->intensity * BrFixedToFloat(BR_BLU(alp->s->colour) << 8);
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x00549630, lightingColourPointSpecular, lightingColourPointSpecular_original)
