c28fe116c2d911a623f3f63d9dc145c43139bdd9
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-06-18 21:39:02 +0200

    Implement lightingColourPointSpecular

--- src/brender/drivers/softrend/light24.h
+++ src/brender/drivers/softrend/light24.h
@@ -19,7 +19,7 @@ void C2_HOOK_STDCALL lightingColourLocal2(br_soft_renderer* self, br_vector3* p,
 
 void C2_HOOK_STDCALL lightingColourLocal1(br_soft_renderer* self, br_vector3* p, br_vector3* n, br_colour colour, active_light* alp, br_scalar* comp);
 
-void C2_HOOK_STDCALL lightingColourPointSpecular(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp);
+void C2_HOOK_STDCALL lightingColourPointSpecular(br_soft_renderer* self, br_vector3* p, br_vector3* n, br_colour colour, active_light* alp, br_scalar* comp);
 
 void C2_HOOK_STDCALL lightingColourPointAttnSpecular(br_soft_renderer* self, br_vector3* p, br_vector3* n, undefined4 param_4, active_light* alp, br_scalar* comp);
 
