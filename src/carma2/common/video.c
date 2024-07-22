6667672454179a5c800ed22ffb2aa64725d7e383
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-07-22 14:51:17 +0200

    Rename VideoInit -> InitQuickTimeStuff

--- src/carma2/common/video.c
+++ src/carma2/common/video.c
@@ -1,11 +1,11 @@
 #include "video.h"
 
-void (C2_HOOK_CDECL * VideoInit_original)(void);
-void C2_HOOK_CDECL VideoInit(void) {
+void (C2_HOOK_CDECL * InitQuickTimeStuff_original)(void);
+void C2_HOOK_CDECL InitQuickTimeStuff(void) {
 #if defined(C2_HOOKS_ENABLED)
-    VideoInit_original();
+    InitQuickTimeStuff_original();
 #else
 #error "not implemented"
 #endif
 }
-C2_HOOK_FUNCTION_ORIGINAL(0x004e1700, VideoInit, VideoInit_original)
+C2_HOOK_FUNCTION_ORIGINAL(0x004e1700, InitQuickTimeStuff, InitQuickTimeStuff_original)
