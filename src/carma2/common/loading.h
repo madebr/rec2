cf78d97fbb4b5bee2a3cae5c44e0bac761bae920
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-16 02:18:19 +0200

    Rename DRfgets -> PFfgets

--- src/carma2/common/loading.h
+++ src/carma2/common/loading.h
@@ -226,7 +226,7 @@ int C2_HOOK_FASTCALL DRfgetc2(FILE* pFile);
 
 int C2_HOOK_FASTCALL DRungetc(int ch, FILE* file);
 
-char* C2_HOOK_FASTCALL DRfgets(char* buffer, br_size_t size, FILE* pFile);
+char* C2_HOOK_FASTCALL PFfgets(char* buffer, br_size_t size, FILE* pFile);
 
 int C2_HOOK_FASTCALL DRfseek(FILE* pF, int offset, int whence);
 
