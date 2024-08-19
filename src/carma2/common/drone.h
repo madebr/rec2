b65ad61d676f455d3eee6760da0ca67fb498489c
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-19 17:59:05 +0200

    Implement AddDroneToPHIL and ReallyAddDroneToPHIL

--- src/carma2/common/drone.h
+++ src/carma2/common/drone.h
@@ -41,6 +41,10 @@ void C2_HOOK_FASTCALL CrappyLittleVector3DPrintf(const char* pMessage, br_vector
 
 int C2_HOOK_FASTCALL CheckDroneInSensiblePlaceBeforeStartingToProcessTheCuntingThing(tDrone_spec* pDrone);
 
+int C2_HOOK_FASTCALL ReallyAddDroneToPHIL(tDrone_spec* pDrone);
+
+int C2_HOOK_FASTCALL AddDroneToPHIL(tDrone_spec* pDrone);
+
 void C2_HOOK_FASTCALL StartProcessingThisDrone(tDrone_spec* pDrone);
 
 void C2_HOOK_FASTCALL InitDrones(void);
