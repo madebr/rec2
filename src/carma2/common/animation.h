36e22a8bbe48bd540e92da0a3def99b0d196d4bf
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-20 00:14:11 +0200

    Rename ReadExplosionAnimation -> ReadExplosion

--- src/carma2/common/animation.h
+++ src/carma2/common/animation.h
@@ -6,6 +6,6 @@
 #include "c2_hooks.h"
 #include "c2_stdio.h"
 
-void C2_HOOK_FASTCALL ReadExplosionAnimation(FILE *pF, tExplosion_animation* pAnimation);
+void C2_HOOK_FASTCALL ReadExplosion(FILE *pF, tExplosion_animation* pAnimation);
 
 #endif // REC2_ANIMATION_H
