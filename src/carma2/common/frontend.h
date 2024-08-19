3977789e4b22237ffc08ead404599e2846b10daf
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-19 03:42:19 +0200

    Rename FRONTEND_RenderItems -> FRONTEND_DrawMenu

--- src/carma2/common/frontend.h
+++ src/carma2/common/frontend.h
@@ -84,7 +84,7 @@ int C2_HOOK_FASTCALL FRONTEND_Default_Destroy(tFrontend_spec* pFrontend);
 
 int C2_HOOK_FASTCALL FRONTEND_Default_Tick(tFrontend_spec* pFrontend);
 
-void C2_HOOK_FASTCALL FRONTEND_RenderItems(tFrontend_spec* pFrontend);
+void C2_HOOK_FASTCALL FRONTEND_DrawMenu(tFrontend_spec* pFrontend);
 
 void C2_HOOK_FASTCALL FRONTEND_DoMouse(void);
 
