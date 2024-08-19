0e511c8c4dd11be02c84dcab7f4e6f9a001d6470
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-19 21:26:57 +0200

    Rename SetMaterialTrackLighting -> SmoothificateWorldMaterial + Implement SmoothlySetWorldMaterialFlags

--- src/carma2/common/loading.h
+++ src/carma2/common/loading.h
@@ -392,7 +392,11 @@ int C2_HOOK_CDECL ActorModelAttachCrushData(br_actor* pActor, void* pData);
 
 void C2_HOOK_FASTCALL AttachCrushDataToActorModels(br_actor* pActor, tCar_spec* pCar_spec);
 
-void C2_HOOK_FASTCALL SetMaterialTrackLighting(br_material* pMaterial);
+void C2_HOOK_FASTCALL SmoothificatePowerupMaterial(br_material* pMaterial);
+
+void C2_HOOK_FASTCALL SmoothificateWorldMaterial(br_material* pMaterial);
+
+void C2_HOOK_FASTCALL SmoothlySetWorldMaterialFlags(tBrender_storage* pStorage);
 
 void C2_HOOK_FASTCALL LoadTrackMaterials(tBrender_storage* pStorage, const char* pPath);
 
