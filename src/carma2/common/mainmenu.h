7487d7eddecfda12bf6eb0231ade392af7727ad3
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-20 17:30:51 +0200

    Implement DoOptionsMenu

--- src/carma2/common/mainmenu.h
+++ src/carma2/common/mainmenu.h
@@ -1,10 +1,14 @@
 #ifndef REC2_MAINMENU_H
 #define REC2_MAINMENU_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
 
 int C2_HOOK_FASTCALL DoVerifyQuit(int pReplace_background);
 
 void C2_HOOK_FASTCALL DoMainScreen(void);
+
+void C2_HOOK_FASTCALL DoOptionsMenu(void);
 
 #endif //REC2_MAINMENU_H
