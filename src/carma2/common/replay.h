65666ce0f673a73ed8fd2c3eeb3e599b6fb2e003
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-20 15:13:47 +0200

    Rename InitializeActionReplayCamera -> SetCameraType

--- src/carma2/common/replay.h
+++ src/carma2/common/replay.h
@@ -9,7 +9,7 @@ C2_HOOK_VARIABLE_DECLARE(tActionReplayCameraMode, gAction_replay_camera_mode);
 
 void C2_HOOK_FASTCALL SetQuickTimeDefaults(void);
 
-void C2_HOOK_FASTCALL InitializeActionReplayCamera(tActionReplayCameraMode pCamPos);
+void C2_HOOK_FASTCALL SetCameraType(tActionReplayCameraMode pCamPos);
 
 void C2_HOOK_FASTCALL PositionExternalCamera(tCar_spec* pCar_spec, tU32 pTime);
 
