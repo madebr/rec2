97dc5d0bf40ae57aaa43ad3ccdeabc5467b1387f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-20 22:09:43 +0200

    Hook ProcessOilSpills

--- src/carma2/common/oil.h
+++ src/carma2/common/oil.h
@@ -1,10 +1,14 @@
 #ifndef REC2_OIL_H
 #define REC2_OIL_H
 
+#include "rec2_types.h"
+
 #include "c2_hooks.h"
 
 void C2_HOOK_FASTCALL InitOilSpills(void);
 
 void C2_HOOK_FASTCALL ResetOilSpills(void);
+
+void C2_HOOK_FASTCALL ProcessOilSpills(tU32 pFrame_period);
 
 #endif //REC2_OIL_H
