611b0bdecb9e447e7f4b0db4898940ffe6308d91
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-24 03:25:17 +0200

    Rename ProcessSmashableActorModel -> SetSmashableModel

--- src/carma2/common/brucetrk.h
+++ src/carma2/common/brucetrk.h
@@ -18,7 +18,7 @@ void C2_HOOK_FASTCALL GetModelTextureArea(br_model* pModel, int* pArea_1, int* p
 
 void C2_HOOK_FASTCALL FixModelPointer(br_model* pModel, br_uint_16 pFlags);
 
-void C2_HOOK_FASTCALL ProcessSmashableActorModel(br_actor* pActor);
+void C2_HOOK_FASTCALL SetSmashableModel(br_actor* pActor);
 
 intptr_t C2_HOOK_CDECL ProcessModelsCB(br_actor* pActor, void* data);
 
