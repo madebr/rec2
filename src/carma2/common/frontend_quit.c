043f368772bfc15719113b4ba887c0ea16423b6d
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-28 01:48:17 +0200

    Rename FRONTEND_Quit_Create -> Generic_Infunc

--- src/carma2/common/frontend_quit.c
+++ src/carma2/common/frontend_quit.c
@@ -8,7 +8,7 @@
 C2_HOOK_VARIABLE_IMPLEMENT_INIT(tFrontend_spec, gFrontend_QUIT, 0x00649df0, {
     "Quit",                         /* name */
     3,                              /* count_items */
-    FRONTEND_Quit_Create,           /* init */
+    Generic_Infunc,                 /* init */
     FRONTEND_Default_Destroy,       /* destroy */
     FRONTEND_Default_Tick,          /* tick */
     &gFrontend_MAIN,                /* previous */
@@ -113,16 +113,16 @@ C2_HOOK_VARIABLE_IMPLEMENT_INIT(tFrontend_spec, gFrontend_QUIT, 0x00649df0, {
     0,
 });
 
-int (C2_HOOK_FASTCALL * FRONTEND_Quit_Create_original)(tFrontend_spec* pFrontend);
-int C2_HOOK_FASTCALL FRONTEND_Quit_Create(tFrontend_spec* pFrontend) {
+int (C2_HOOK_FASTCALL * Generic_Infunc_original)(tFrontend_spec* pFrontend);
+int C2_HOOK_FASTCALL Generic_Infunc(tFrontend_spec* pFrontend) {
 
 #if defined(C2_HOOKS_ENABLED)
-    return FRONTEND_Quit_Create_original(pFrontend);
+    return Generic_Infunc_original(pFrontend);
 #else
 #error "Not implemented"
 #endif
 }
-C2_HOOK_FUNCTION_ORIGINAL(0x00470bb0, FRONTEND_Quit_Create, FRONTEND_Quit_Create_original)
+C2_HOOK_FUNCTION_ORIGINAL(0x00470bb0, Generic_Infunc, Generic_Infunc_original)
 
 int C2_HOOK_FASTCALL FRONTEND_Quit_Yes(tFrontend_spec* pFrontend) {
 
