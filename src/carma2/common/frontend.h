092594b65b2196703718f8a7df866d4734bcb0bb
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-28 15:34:23 +0200

    Implement Generic_Infunc

--- src/carma2/common/frontend.h
+++ src/carma2/common/frontend.h
@@ -32,6 +32,8 @@ C2_HOOK_VARIABLE_DECLARE(int, gINT_0059b0d8);
 C2_HOOK_VARIABLE_DECLARE(int, gFrontend_selected_item_index);
 C2_HOOK_VARIABLE_DECLARE(tU32, gFrontend_last_scroll);
 C2_HOOK_VARIABLE_DECLARE(tU32, gFrontend_time_last_input);
+C2_HOOK_VARIABLE_DECLARE(tStruct_00686508*, PTR_00686508);
+C2_HOOK_VARIABLE_DECLARE(tFrontend_scroller_callback*, gCurrent_frontend_scrollbars);
 
 
 void C2_HOOK_FASTCALL IString_Load(void);
