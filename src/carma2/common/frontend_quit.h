5fc78424d572d646aa0f72ee82134dec18bf3437
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-08-28 02:01:45 +0200

    Rename FRONTEND_Quit_Yes -> Options_Quit

--- src/carma2/common/frontend_quit.h
+++ src/carma2/common/frontend_quit.h
@@ -9,6 +9,6 @@ C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_QUIT);
 
 int C2_HOOK_FASTCALL Generic_Infunc(tFrontend_spec* pFrontend);
 
-int C2_HOOK_FASTCALL FRONTEND_Quit_Yes(tFrontend_spec* pFrontend);
+int C2_HOOK_FASTCALL Options_Quit(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_QUIT_H */
