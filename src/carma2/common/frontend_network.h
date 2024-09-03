2b6af6b55f0337d666456411b0f2d4703aa78659
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-03 03:52:45 +0200

    Implement NetRaceUp

--- src/carma2/common/frontend_network.h
+++ src/carma2/common/frontend_network.h
@@ -8,6 +8,8 @@
 C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_NETWORK);
 C2_HOOK_VARIABLE_DECLARE(tNet_game_type, gFrontend_game_type);
 C2_HOOK_VARIABLE_DECLARE(tNet_game_options, gFrontend_net_options);
+C2_HOOK_VARIABLE_DECLARE(tU32, gFrontend_net_current_roll);
+C2_HOOK_VARIABLE_DECLARE(tU32, gFrontend_net_last_roll);
 
 void C2_HOOK_FASTCALL RefreshNetRacesScroller(tFrontend_spec* pFrontend);
 
@@ -20,5 +22,7 @@ int C2_HOOK_FASTCALL NetGameTypeUp(tFrontend_spec* pFrontend);
 int C2_HOOK_FASTCALL NetSetRaceType(tFrontend_spec* pFrontend);
 
 int C2_HOOK_FASTCALL NetGameTypeDn(tFrontend_spec* pFrontend);
+
+int C2_HOOK_FASTCALL NetRaceUp(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_NETWORK_H */
