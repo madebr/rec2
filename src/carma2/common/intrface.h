fd32f9652bf5049c5c0914f644c12d9fc2048d90
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-03 23:11:18 +0200

    Implement ClearAlwaysTyping

--- src/carma2/common/intrface.h
+++ src/carma2/common/intrface.h
@@ -5,8 +5,12 @@
 
 #include "rec2_types.h"
 
+C2_HOOK_VARIABLE_DECLARE(int, gAlways_typing);
+
 int C2_HOOK_FASTCALL DoInterfaceScreen(const tInterface_spec* pSpec, int pOptions, int pCurrent_choice);
 
 void C2_HOOK_FASTCALL LoadMenuSettings(tFrontend_spec* pFrontend);
+
+void C2_HOOK_FASTCALL ClearAlwaysTyping(void);
 
 #endif //REC2_INTRFACE_H
