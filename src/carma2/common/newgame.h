049af41e2965b0315a5bfec61b703f3c6ae91b6a
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-03 02:43:32 +0200

    Implement SetOptions

--- src/carma2/common/newgame.h
+++ src/carma2/common/newgame.h
@@ -6,7 +6,10 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE_ARRAY(tNet_game_options, gNet_settings, 9);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(int, gNet_grid_starts, 10);
 
 void C2_HOOK_FASTCALL DefaultNetSettings(void);
+
+void C2_HOOK_FASTCALL SetOptions(tNet_game_type pGame_type, tNet_game_options* pGame_options);
 
 #endif //REC2_NEWGAME_H
