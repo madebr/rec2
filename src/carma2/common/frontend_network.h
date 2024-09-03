35818d106917001d135e46c4259355cca3c58234
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-03 03:26:44 +0200

    Implement NetSetRaceType

--- src/carma2/common/frontend_network.h
+++ src/carma2/common/frontend_network.h
@@ -6,11 +6,17 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_NETWORK);
+C2_HOOK_VARIABLE_DECLARE(tNet_game_type, gFrontend_game_type);
+C2_HOOK_VARIABLE_DECLARE(tNet_game_options, gFrontend_net_options);
 
 void C2_HOOK_FASTCALL RefreshNetRacesScroller(tFrontend_spec* pFrontend);
+
+void C2_HOOK_FASTCALL UpdateNetTrackScroller(tFrontend_spec* pFrontend);
 
 void C2_HOOK_FASTCALL UpdateNetGameTypeScroller(tFrontend_spec* pFrontend);
 
 int C2_HOOK_FASTCALL NetGameTypeUp(tFrontend_spec* pFrontend);
+
+int C2_HOOK_FASTCALL NetSetRaceType(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_NETWORK_H */
