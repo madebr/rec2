72aaa378185b477dd24e8b94a8d95d2ea313305a
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-03 02:43:32 +0200

    Implement SetOptions

--- src/carma2/common/newgame.c
+++ src/carma2/common/newgame.c
@@ -10,6 +10,9 @@
 #include "c2_stdio.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tNet_game_options, gNet_settings, 9, 0x00763960);
+C2_HOOK_VARIABLE_IMPLEMENT_ARRAY_INIT(int, gNet_grid_starts, 10, 0x0059c7e8, {
+    1, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+});
 
 void C2_HOOK_FASTCALL DefaultNetSettings(void) {
     FILE* file;
@@ -31,3 +34,21 @@ void C2_HOOK_FASTCALL DefaultNetSettings(void) {
     PFfclose(file);
 }
 C2_HOOK_FUNCTION(0x004663e0, DefaultNetSettings)
+
+void C2_HOOK_FASTCALL SetOptions(tNet_game_type pGame_type, tNet_game_options* pGame_options) {
+
+    pGame_options->show_players_on_map = C2V(gNet_settings)[0].show_players_on_map;
+    pGame_options->enable_text_messages = C2V(gNet_settings)[0].enable_text_messages;
+    pGame_options->show_powerups_on_map = C2V(gNet_settings)[0].show_powerups_on_map;
+    pGame_options->powerup_respawn = C2V(gNet_settings)[0].powerup_respawn;
+    pGame_options->open_game = C2V(gNet_settings)[pGame_type + 1].open_game;
+    pGame_options->starting_credits = C2V(gNet_settings)[0].starting_credits;
+    pGame_options->grid_start = C2V(gNet_settings)[pGame_type + 1].grid_start || C2V(gNet_grid_starts)[pGame_type + 1];
+    pGame_options->waste_to_transfer = C2V(gNet_settings)[pGame_type + 1].waste_to_transfer;
+    pGame_options->random_car_choice = C2V(gNet_settings)[pGame_type + 1].random_car_choice;
+    pGame_options->race_sequence_type = C2V(gNet_settings)[0].race_sequence_type;
+    pGame_options->car_choice = C2V(gNet_settings)[0].car_choice;
+    pGame_options->starting_target = C2V(gNet_settings)[pGame_type + 1].starting_target;
+
+}
+C2_HOOK_FUNCTION(0x00468140, SetOptions)
