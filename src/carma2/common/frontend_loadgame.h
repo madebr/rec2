ad6b30f636aae6afd69d28908efbfb19c41a9115
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-08 01:42:29 +0200

    Implement LoadSlot6

--- src/carma2/common/frontend_loadgame.h
+++ src/carma2/common/frontend_loadgame.h
@@ -30,4 +30,6 @@ int C2_HOOK_FASTCALL LoadSlot4(tFrontend_spec* pFrontend);
 
 int C2_HOOK_FASTCALL LoadSlot5(tFrontend_spec* pFrontend);
 
+int C2_HOOK_FASTCALL LoadSlot6(tFrontend_spec* pFrontend);
+
 #endif /* FRONTEND_LOADGAME_H */
