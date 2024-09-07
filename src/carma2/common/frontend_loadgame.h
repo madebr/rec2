031a50a5001e8fab0e7c1b0124569ae521b722de
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-08 01:06:16 +0200

    Implement LoadGameUpdateFunc

--- src/carma2/common/frontend_loadgame.h
+++ src/carma2/common/frontend_loadgame.h
@@ -8,10 +8,14 @@
 C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_LOAD_GAME);
 C2_HOOK_VARIABLE_DECLARE(int, gFrontend_count_saved_games);
 C2_HOOK_VARIABLE_DECLARE(int, gFrontend_load_game_index_top);
+C2_HOOK_VARIABLE_DECLARE(int, gFrontend_mouse_down);
+C2_HOOK_VARIABLE_DECLARE(int, gPrev_frontend_mouse_down);
 
 
 int C2_HOOK_FASTCALL LoadGameInFunc(tFrontend_spec* pFrontend);
 
 int C2_HOOK_FASTCALL LoadGameOutFunc(tFrontend_spec* pFrontend);
+
+int C2_HOOK_FASTCALL LoadGameUpdateFunc(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_LOADGAME_H */
