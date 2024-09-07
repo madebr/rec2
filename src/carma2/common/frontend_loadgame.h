bbfaf852f7a70e0ba648370ef54b9901ecc1f72c
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-07 19:08:50 +0200

    Implement LoadGameInFunc

--- src/carma2/common/frontend_loadgame.h
+++ src/carma2/common/frontend_loadgame.h
@@ -6,5 +6,10 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_LOAD_GAME);
+C2_HOOK_VARIABLE_DECLARE(int, gFrontend_count_saved_games);
+C2_HOOK_VARIABLE_DECLARE(int, gFrontend_load_game_index_top);
+
+
+int C2_HOOK_FASTCALL LoadGameInFunc(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_LOADGAME_H */
