19973967735506ec4b858c42735edb55d7109f94
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-08 04:54:18 +0200

    Implement NetSummary_Infunc

--- src/carma2/common/frontend_networksummary.h
+++ src/carma2/common/frontend_networksummary.h
@@ -6,5 +6,11 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_NETWORK_SUMMARY);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(int, gPlayer_lookup_netsummary, 14);
+
+
+void C2_HOOK_FASTCALL NetworkSummarySetup(tFrontend_spec* pFrontend);
+
+int C2_HOOK_FASTCALL NetSummary_Infunc(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_NETWORKSUMMARY_H */
