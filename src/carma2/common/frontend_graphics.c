dca7e1dc8719bf29e02ed932679d8e323255b22b
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-09 02:28:33 +0200

    Implement GetGraphicsSettingsData

---
+++ src/carma2/common/frontend_graphics.c
@@ -0,0 +1,144 @@
+#include "frontend_credits.h"
+
+#include "brucetrk.h"
+#include "controls.h"
+#include "depth.h"
+#include "frontend.h"
+#include "globvars.h"
+#include "graphics.h"
+#include "loading.h"
+#include "world.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT_INIT(tFrontend_spec, gFrontend_GRAPHICS, 0x0063e528, {
+    FIXME TODO
+});
+
+
+void C2_HOOK_FASTCALL GetGraphicsSettingsData(tFrontend_spec* pFrontend) {
+    float yon_factor;
+
+    switch (GetCarSimplificationLevel()) {
+    case 0:
+        SelectThisItemIn(pFrontend, 1, 3);
+        break;
+    case 1:
+        SelectThisItemIn(pFrontend, 1, 4);
+        break;
+    case 2:
+        SelectThisItemIn(pFrontend, 1, 5);
+        break;
+    case 3:
+        SelectThisItemIn(pFrontend, 1, 6);
+        break;
+    }
+
+    switch ((int)((35.f - GetYon()) / 5.f)) {
+    case 0:
+        SelectThisItemIn(pFrontend, 2, 8);
+        break;
+    case 1:
+        SelectThisItemIn(pFrontend, 2, 9);
+        break;
+    case 2:
+        SelectThisItemIn(pFrontend, 2, 10);
+        break;
+    default:
+        SelectThisItemIn(pFrontend, 2, 11);
+        break;
+    }
+
+    yon_factor = GetYonFactor();
+    if (yon_factor >= 1.f) {
+        SelectThisItemIn(pFrontend, 3, 13);
+    } else if (yon_factor >= 0.5f) {
+        SelectThisItemIn(pFrontend, 3, 14);
+    } else if (yon_factor >= 0.25f) {
+        SelectThisItemIn(pFrontend, 3, 15);
+    } else {
+        SelectThisItemIn(pFrontend, 3, 16);
+    }
+
+    switch (GetShadowLevel()) {
+    default:
+        SelectThisItemIn(pFrontend, 4, 18);
+        break;
+    case 0:
+        SelectThisItemIn(pFrontend, 4, 20);
+        break;
+    case 1:
+        SelectThisItemIn(pFrontend, 4, 19);
+        break;
+    }
+
+    switch (GetSoundDetailLevel()) {
+    case 0:
+        SelectThisItemIn(pFrontend, 5, 24);
+        break;
+    case 1:
+        SelectThisItemIn(pFrontend, 5, 23);
+        break;
+    case 2:
+        SelectThisItemIn(pFrontend, 5, 22);
+        break;
+    }
+
+    switch (GetHowMuchBloodAndSnotToSmearAbout()) {
+    case 0:
+        SelectThisItemIn(pFrontend, 6, 26);
+        break;
+    case 1:
+        SelectThisItemIn(pFrontend, 6, 27);
+        break;
+    case 2:
+        SelectThisItemIn(pFrontend, 6, 28);
+        break;
+    }
+
+    if (GetSkyTextureOn()) {
+        SelectThisItemIn(pFrontend, 7, 30);
+    } else {
+        SelectThisItemIn(pFrontend, 7, 31);
+    }
+
+    if (GetDepthCueingOn()) {
+        SelectThisItemIn(pFrontend, 8, 33);
+    } else {
+        SelectThisItemIn(pFrontend, 8, 34);
+    }
+
+    if (GetDronesOn()) {
+        SelectThisItemIn(pFrontend, 9, 36);
+    } else {
+        SelectThisItemIn(pFrontend, 9, 37);
+    }
+
+    if (GetAnimalsOn()) {
+        SelectThisItemIn(pFrontend, 10, 39);
+    } else {
+        SelectThisItemIn(pFrontend, 10, 40);
+    }
+
+    if (IsItOkayToFireHorribleBallsOfNastyNapalmDeathAtPerfectlyInnocentPassersByAndByInnocentIDoMeanInTheBiblicalSense()) {
+        SelectThisItemIn(pFrontend, 11, 42);
+    } else {
+        SelectThisItemIn(pFrontend, 11, 43);
+    }
+
+    if (IsItReallyOKThatWeDontMakeAnyEffortToProtectAnySadFuckersOutThereThatDontWishToSeeInnocentPeopleBlownToBitsByHighExplosiveMinesAndShells()) {
+        SelectThisItemIn(pFrontend, 12, 45);
+    } else {
+        SelectThisItemIn(pFrontend, 12, 46);
+    }
+
+    if (C2V(gAuto_load)) {
+        SelectThisItemIn(pFrontend, 13, 48);
+    } else {
+        SelectThisItemIn(pFrontend, 13, 49);
+    }
+    if (C2V(gAmbient_sound)) {
+        SelectThisItemIn(pFrontend, 14, 51);
+    } else {
+        SelectThisItemIn(pFrontend, 14, 52);
+    }
+}
+C2_HOOK_FUNCTION(0x004b4090, GetGraphicsSettingsData)
