8cb69c4ca91bdc5de3e8aa9cedd308cfb548cb88
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-09 02:28:33 +0200

    Implement GetGraphicsSettingsData

---
+++ src/carma2/common/frontend_graphics.h
@@ -0,0 +1,13 @@
+#ifndef FRONTEND_GRAPHICS_H
+#define FRONTEND_GRAPHICS_H
+
+#include "c2_hooks.h"
+
+#include "rec2_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_GRAPHICS);
+
+
+void C2_HOOK_FASTCALL GetGraphicsSettingsData(tFrontend_spec* pFrontend);
+
+#endif /* FRONTEND_GRAPHICS_H */
