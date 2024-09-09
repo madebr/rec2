5bf2bf7fc57a879ca2fb4168e4511e7b315cf29d
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-09 03:02:04 +0200

    Implement SetGraphicsSettingsData

--- src/carma2/common/frontend_graphics.c
+++ src/carma2/common/frontend_graphics.c
@@ -151,3 +151,162 @@ int C2_HOOK_FASTCALL Graphics_Infunc(tFrontend_spec* pFrontend) {
     return 1;
 }
 C2_HOOK_FUNCTION(0x004747e0, Graphics_Infunc)
+
+void C2_HOOK_FASTCALL SetGraphicsSettingsData(tFrontend_spec* pFrontend) {
+
+    switch (WhichItemIsSelectedIn(pFrontend, 1)) {
+    case 3:
+        SetCarSimplificationLevel(0);
+        break;
+    case 4:
+        SetCarSimplificationLevel(1);
+        break;
+    case 5:
+        SetCarSimplificationLevel(2);
+        break;
+    case 6:
+        SetCarSimplificationLevel(3);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 2)) {
+    case 8:
+        SetYon(35.f);
+        break;
+    case 9:
+        SetYon(30.f);
+        break;
+    case 10:
+        SetYon(25.f);
+        break;
+    case 11:
+        SetYon(20.f);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 3)) {
+    case 13:
+        SetYonFactor(1.0f);
+        break;
+    case 14:
+        SetYonFactor(0.5f);
+        break;
+    case 15:
+        SetYonFactor(0.25f);
+        break;
+    case 16:
+        SetYonFactor(0.125f);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 4)) {
+    case 18:
+        SetShadowLevel(3);
+        break;
+    case 19:
+        SetShadowLevel(1);
+        break;
+    case 20:
+        SetShadowLevel(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 5)) {
+    case 22:
+        SetSoundDetailLevel(2);
+        break;
+    case 23:
+        SetSoundDetailLevel(1);
+        break;
+    case 24:
+        SetSoundDetailLevel(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 6)) {
+    case 26:
+        SetGoreLevel(0);
+        break;
+    case 27:
+        SetGoreLevel(1);
+        break;
+    case 28:
+        SetGoreLevel(2);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 7)) {
+    case 30:
+        SetSkyTextureOn(1);
+        break;
+    case 31:
+        SetSkyTextureOn(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 8)) {
+    case 33:
+        SetDepthCueingOn(1);
+        break;
+    case 34:
+        SetDepthCueingOn(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 9)) {
+    case 36:
+        SetDronesOn(1);
+        break;
+    case 37:
+        SetDronesOn(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 10)) {
+    case 39:
+        SetAnimalsOn(1);
+        break;
+    case 40:
+        SetAnimalsOn(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 11)) {
+    case 42:
+        SetFlameThrowerOn(1);
+        break;
+    case 43:
+        SetFlameThrowerOn(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 12)) {
+    case 45:
+        SetMinesOn(1);
+        break;
+    case 46:
+        SetMinesOn(0);
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 13)) {
+    case 48:
+        C2V(gAuto_load) = 1;
+        break;
+    case 49:
+        C2V(gAuto_load) = 0;
+        break;
+    }
+
+    switch (WhichItemIsSelectedIn(pFrontend, 14)) {
+    case 51:
+        C2V(gAmbient_sound) = 1;
+        break;
+    case 52:
+        C2V(gAmbient_sound) = 0;
+        break;
+    }
+
+    SaveOptions();
+}
+C2_HOOK_FUNCTION(0x004b42c0, SetGraphicsSettingsData)
