1b724782076a2f11e799a921e53ec796ab11a4d5
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-11 01:09:28 +0200

    Implement Controls_KeyDown

--- src/carma2/common/frontend_controls.h
+++ src/carma2/common/frontend_controls.h
@@ -32,4 +32,6 @@ int C2_HOOK_FASTCALL Controls_SlotActivated(tFrontend_spec* pFrontend);
 
 int C2_HOOK_FASTCALL Controls_KeyUp(tFrontend_spec* pFrontend);
 
+int C2_HOOK_FASTCALL Controls_KeyDown(tFrontend_spec* pFrontend);
+
 #endif /* FRONTEND_CONTROLS_H */
