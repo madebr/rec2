f081fd16861e1b01c02377f5bbcf22c65663dc9c
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-10 02:36:00 +0200

    Implement ActorPick2D

--- src/carma2/common/raycast.h
+++ src/carma2/common/raycast.h
@@ -3,12 +3,14 @@
 
 #include "c2_hooks.h"
 
-#include <brender/brender.h>
+#include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(br_actor*, gY_picking_camera);
 
 void C2_HOOK_FASTCALL InitRayCasting(void);
 
 int C2_HOOK_FASTCALL PickBoundsTestRay__raycast(br_bounds* b, br_vector3* rp, br_vector3* rd, br_scalar t_near, br_scalar t_far, br_scalar* new_t_near, br_scalar* new_t_far);
+
+int C2_HOOK_FASTCALL ActorPick2D(br_actor* ap, br_model* model, br_material* material, dr_pick2d_cbfn* callback, void* arg);
 
 #endif //REC2_RAYCAST_H
