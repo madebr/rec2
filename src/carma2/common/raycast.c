a8622fa40f8c305218ad4dc8b1cd8ba232462b94
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-10 02:35:21 +0200

    Implement PickBoundsTestRay__raycast

--- src/carma2/common/raycast.c
+++ src/carma2/common/raycast.c
@@ -31,3 +31,48 @@ void C2_HOOK_FASTCALL InitRayCasting(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004e3660, InitRayCasting, InitRayCasting_original)
+
+int C2_HOOK_FASTCALL PickBoundsTestRay__raycast(br_bounds* b, br_vector3* rp, br_vector3* rd, br_scalar t_near, br_scalar t_far, br_scalar* new_t_near, br_scalar* new_t_far) {
+    int i;
+    float s;
+    float t;
+
+    for (i = 0; i < 3; i++) {
+        if (rd->v[i] > 2.384186e-07f) {
+            s = (1.0f / rd->v[i]) * (rp->v[i] - b->max.v[i]);
+            if (s > BR_SCALAR_MAX) {
+                t_near = BR_SCALAR_MAX;
+            } else if (s > t_near) {
+                t_near = s;
+            }
+            t = (1.0f / rd->v[i]) * (rp->v[i] - b->min.v[i]);
+            if (t < BR_SCALAR_MIN) {
+                t_far = BR_SCALAR_MIN;
+            } else if (t < t_far) {
+                t_far = t;
+            }
+        } else if (rd->v[i] < -2.384186e-07f) {
+            s = (1.0f / rd->v[i]) * (rp->v[i] - b->max.v[i]);
+            if (s < BR_SCALAR_MIN) {
+                t_far = BR_SCALAR_MIN;
+            } else if (s < t_far) {
+                t_far = s;
+            }
+            t = (1.0f / rd->v[i]) * (rp->v[i] - b->min.v[i]);
+            if (t > BR_SCALAR_MAX) {
+                t_near = BR_SCALAR_MAX;
+            } else if (t > t_near) {
+                t_near = t;
+            }
+        } else if (rp->v[i] > b->max.v[i] || rp->v[i] < b->min.v[i]) {
+            return 0;
+        }
+    }
+    if (t_far < t_near) {
+        return 0;
+    }
+    *new_t_near = t_near;
+    *new_t_far = t_far;
+    return 1;
+}
+C2_HOOK_FUNCTION(0x004e3a30, PickBoundsTestRay__raycast)
