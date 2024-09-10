f042ebe5bd76f87ab49803441792bc61ba4b1ea8
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-10 02:35:21 +0200

    Implement PickBoundsTestRay__raycast

--- src/carma2/common/raycast.h
+++ src/carma2/common/raycast.h
@@ -9,4 +9,6 @@ C2_HOOK_VARIABLE_DECLARE(br_actor*, gY_picking_camera);
 
 void C2_HOOK_FASTCALL InitRayCasting(void);
 
+int C2_HOOK_FASTCALL PickBoundsTestRay__raycast(br_bounds* b, br_vector3* rp, br_vector3* rd, br_scalar t_near, br_scalar t_far, br_scalar* new_t_near, br_scalar* new_t_far);
+
 #endif //REC2_RAYCAST_H
