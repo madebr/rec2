0df56498e80fbf7606d13679e4977588d83ad23b
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-10 22:22:32 +0200

    Implement DisplayJoystickSettings

--- src/carma2/common/frontend_controls.h
+++ src/carma2/common/frontend_controls.h
@@ -6,5 +6,12 @@
 #include "rec2_types.h"
 
 C2_HOOK_VARIABLE_DECLARE(tFrontend_spec, gFrontend_CONTROLS);
+C2_HOOK_VARIABLE_DECLARE(tFrontend_slider, gControls_slider_1);
+C2_HOOK_VARIABLE_DECLARE(tFrontend_slider, gControls_slider_2);
+C2_HOOK_VARIABLE_DECLARE(tFrontend_slider, gControls_slider_3);
+C2_HOOK_VARIABLE_DECLARE_ARRAY(int, gFrontend_controls_indices, 15);
+
+
+void C2_HOOK_FASTCALL DisplayJoystickSettings(tFrontend_spec *pFrontend);
 
 #endif /* FRONTEND_CONTROLS_H */
