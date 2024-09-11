9c201b62b4bb6189f273f410e96eaebde95eb544
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-11 04:00:54 +0200

    Implement Controls_Ok

--- src/carma2/common/frontend_controls.h
+++ src/carma2/common/frontend_controls.h
@@ -10,6 +10,7 @@ C2_HOOK_VARIABLE_DECLARE(tFrontend_slider, gControls_slider_1);
 C2_HOOK_VARIABLE_DECLARE(tFrontend_slider, gControls_slider_2);
 C2_HOOK_VARIABLE_DECLARE(tFrontend_slider, gControls_slider_3);
 C2_HOOK_VARIABLE_DECLARE_ARRAY(int, gFrontend_controls_indices, 15);
+C2_HOOK_VARIABLE_DECLARE(int, gINT_00688748);
 
 
 void C2_HOOK_FASTCALL DisplayJoystickSettings(tFrontend_spec *pFrontend);
@@ -35,5 +36,7 @@ int C2_HOOK_FASTCALL Controls_SlotActivated(tFrontend_spec* pFrontend);
 int C2_HOOK_FASTCALL Controls_KeyUp(tFrontend_spec* pFrontend);
 
 int C2_HOOK_FASTCALL Controls_KeyDown(tFrontend_spec* pFrontend);
+
+int C2_HOOK_FASTCALL Controls_Ok(tFrontend_spec* pFrontend);
 
 #endif /* FRONTEND_CONTROLS_H */
