599e9fd7cb76d1f52e88326e0100fb1de073c1bf
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-13 01:14:25 +0200

    Implement UpdateFramePeriod

--- src/carma2/common/mainloop.c
+++ src/carma2/common/mainloop.c
@@ -2,12 +2,19 @@
 
 #include "drone.h"
 #include "globvars.h"
+#include "globvrpb.h"
 #include "loading.h"
 #include "opponent.h"
 #include "powerups.h"
 #include "sound.h"
 #include "timers.h"
 #include "utility.h"
+
+C2_HOOK_VARIABLE_IMPLEMENT(tU32, gLast_tick_count, 0x0079efb0);
+C2_HOOK_VARIABLE_IMPLEMENT(tU32, gOld_camera_time, 0x0068c814);
+C2_HOOK_VARIABLE_IMPLEMENT(tU32, gActual_last_tick_count, 0x0068c820);
+C2_HOOK_VARIABLE_IMPLEMENT(tU32, gAverage_frame_period, 0x0068c824);
+
 
 void C2_HOOK_FASTCALL MungeAIWorld(tU32 pFrame_period) {
 
@@ -59,3 +66,68 @@ tRace_result C2_HOOK_FASTCALL DoRace(void) {
     return result;
 }
 C2_HOOK_FUNCTION(0x00492950, DoRace)
+
+void C2_HOOK_FASTCALL UpdateFramePeriod(tU32* pCamera_period) {
+    tU32 new_tick_count;
+    tU32 new_camera_tick_count;
+    int error;
+
+    if (C2V(gAction_replay_mode)) {
+        tU32 last_tick_count;
+
+        last_tick_count = C2V(gLast_tick_count);
+        C2V(gLast_tick_count) = C2V(gLast_replay_frame_time);
+        C2V(gFrame_period) = abs((int)(C2V(gLast_replay_frame_time) - last_tick_count));
+        new_camera_tick_count = PDGetTotalTime();
+        new_tick_count = GetTotalTime();
+        if (C2V(gOld_camera_time) != 0) {
+            *pCamera_period = new_camera_tick_count - C2V(gOld_camera_time);
+        } else {
+            *pCamera_period = 0;
+        }
+        C2V(gOld_camera_time) = new_camera_tick_count;
+        if (C2V(gFrame_period) != 0) {
+            *pCamera_period = C2V(gFrame_period);
+        }
+    } else {
+        int new_period;
+        new_tick_count = PDGetTotalTime();
+        C2V(gLast_tick_count) += C2V(gFrame_period);
+        error = new_tick_count - C2V(gLast_tick_count);
+        C2V(gFrame_period) = new_tick_count - C2V(gActual_last_tick_count);
+        if (C2V(gFrame_period) > 500 && new_tick_count - C2V(gRace_start) > 2000) {
+            C2V(gFrame_period) = C2V(gAverage_frame_period) / 10;
+            C2V(gLast_tick_count) = new_tick_count;
+            if (C2V(gNet_mode) != eNet_mode_none && C2V(gNet_mode) != eNet_mode_host) {
+                if (C2V(gNet_mode) == eNet_mode_client) {
+                    C2V(gProgram_state).current_car.collision_info->field_0x49c = 0;
+                }
+            }
+        }
+        C2V(gAverage_frame_period) = 9 * C2V(gAverage_frame_period) / 10 + C2V(gFrame_period);
+        if (new_tick_count - C2V(gRace_start) > 2000) {
+            C2V(gFrame_period) = C2V(gAverage_frame_period) / 10;
+        }
+        new_period = C2V(gFrame_period) + error;
+        if (new_period > 0) {
+            C2V(gFrame_period) = new_period;
+        } else {
+            C2V(gLast_tick_count) = new_tick_count;
+        }
+        *pCamera_period = C2V(gFrame_period);
+        C2V(gActual_last_tick_count) = new_tick_count;
+    }
+    if (C2V(gFrame_period) < 10) {
+        C2V(gLast_tick_count) = 10;
+        C2V(gLast_tick_count) = new_tick_count;
+    } else if (C2V(gFrame_period) > 1000) {
+        C2V(gFrame_period) = 1000;
+        C2V(gLast_tick_count) = new_tick_count;
+    }
+    if (*pCamera_period < 10) {
+        *pCamera_period = 10;
+    } else if (*pCamera_period > 1000) {
+        *pCamera_period = 1000;
+    }
+}
+C2_HOOK_FUNCTION(0x00492680, UpdateFramePeriod)
