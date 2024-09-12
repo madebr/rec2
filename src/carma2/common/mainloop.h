baf718d742b5f4068d8b08d7cf0f18e8e807b15b
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-13 01:14:25 +0200

    Implement UpdateFramePeriod

--- src/carma2/common/mainloop.h
+++ src/carma2/common/mainloop.h
@@ -5,6 +5,12 @@
 
 #include "rec2_types.h"
 
+C2_HOOK_VARIABLE_DECLARE(tU32, gLast_tick_count);
+C2_HOOK_VARIABLE_DECLARE(tU32, gOld_camera_time);
+C2_HOOK_VARIABLE_DECLARE(tU32, gActual_last_tick_count);
+C2_HOOK_VARIABLE_DECLARE(tU32, gAverage_frame_period);
+
+
 void C2_HOOK_FASTCALL MungeAIWorld(tU32 pTime);
 
 void C2_HOOK_FASTCALL CalculateCameraStuff(tU32 pCamera_period);
@@ -12,5 +18,7 @@ void C2_HOOK_FASTCALL CalculateCameraStuff(tU32 pCamera_period);
 tRace_result C2_HOOK_FASTCALL MainGameLoop(void);
 
 tRace_result C2_HOOK_FASTCALL DoRace(void);
+
+void C2_HOOK_FASTCALL UpdateFramePeriod(tU32* pCamera_period);
 
 #endif // REC2_MAINLOOP_H
