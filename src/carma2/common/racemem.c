d7e6121115bf3f202264e064daeddf535bd1f528
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-21 21:49:20 +0200

    Implement GetRuntimeVariable

--- src/carma2/common/racemem.c
+++ src/carma2/common/racemem.c
@@ -1,5 +1,7 @@
 #include "racemem.h"
 
+#include "rec2_macros.h"
+
 #include "c2_string.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_ARRAY(tU8, gRace_memory, 1000, 0x006ba640);
@@ -9,3 +11,12 @@ void C2_HOOK_FASTCALL ClearAllRuntimeVariables(void) {
     c2_memset(C2V(gRace_memory), 0, sizeof(C2V(gRace_memory)));
 }
 C2_HOOK_FUNCTION(0x004e74b0, ClearAllRuntimeVariables)
+
+int C2_HOOK_FASTCALL GetRuntimeVariable(int pAddress) {
+
+    if (pAddress >= 0 && pAddress < REC2_ASIZE(C2V(gRace_memory))) {
+        return C2V(gRace_memory)[pAddress];
+    }
+    return 0;
+}
+C2_HOOK_FUNCTION(0x004e7440, GetRuntimeVariable)
--- src/carma2/common/racemem.h
+++ src/carma2/common/racemem.h
@@ -9,4 +9,6 @@ C2_HOOK_VARIABLE_DECLARE_ARRAY(tU8, gRace_memory, 1000);
 
 void C2_HOOK_FASTCALL ClearAllRuntimeVariables(void);
 
+int C2_HOOK_FASTCALL GetRuntimeVariable(int pAddress);
+
 #endif /* C2_RACEMEM_H */
