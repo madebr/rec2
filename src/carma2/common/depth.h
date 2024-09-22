91b483d13225479fdbd4bdb23bb8b3f98b84848f
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-22 02:19:20 +0200

    Implement SwitchCarModels

--- src/carma2/common/depth.h
+++ src/carma2/common/depth.h
@@ -27,6 +27,8 @@ C2_HOOK_VARIABLE_DECLARE(br_model*, gSky_model);
 
 intptr_t C2_HOOK_CDECL SwitchCarModel(br_actor* pActor, void* pData);
 
+void C2_HOOK_FASTCALL SwitchCarModels(tCar_spec* pCar, int pIndex);
+
 void C2_HOOK_FASTCALL InstantDepthChange(tDepth_effect_type pType, br_pixelmap* pSky_texture, int pStart, int pEnd, int pRed, int pGreen, int pBlue, int pParam_8);
 
 int C2_HOOK_FASTCALL GetSkyTextureOn(void);
