209a2994b84a32666b3b93b6244f84f3bea85630
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-27 02:25:25 +0200

    Implement PointOutOfSight

--- src/carma2/common/graphics.h
+++ src/carma2/common/graphics.h
@@ -78,6 +78,7 @@ C2_HOOK_VARIABLE_DECLARE(int, gMouse_in_use);
 C2_HOOK_VARIABLE_DECLARE(int, gCurrent_cursor_index);
 C2_HOOK_VARIABLE_DECLARE(float, gCamera_to_horiz_angle);
 C2_HOOK_VARIABLE_DECLARE(float, gYon_squared);
+C2_HOOK_VARIABLE_DECLARE(int, gMirror_on__graphics);
 
 void C2_HOOK_FASTCALL ClearWobbles(void);
 
