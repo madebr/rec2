c961ec036038372daf240d24628d8bef45c8b2a0
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-29 03:45:16 +0200

    Implement UnlockInterfaceStuff

--- src/carma2/common/loading.h
+++ src/carma2/common/loading.h
@@ -473,6 +473,8 @@ void C2_HOOK_FASTCALL LoadAIWorldForRace(tRace_info* pRace_info);
 
 void C2_HOOK_FASTCALL LoadInterfaceStuff(int pWithin_race);
 
+void C2_HOOK_FASTCALL UnlockInterfaceStuff(void);
+
 void C2_HOOK_FASTCALL InitInterfaceLoadState(void);
 
 void C2_HOOK_FASTCALL InitialiseAIWorldForRace(tRace_info* pRace_info);
