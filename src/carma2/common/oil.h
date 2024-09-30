4918457151fd437a209f0ce476a97b2cbe0b22e1
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-09-30 19:53:26 +0200

    Implement ProcessOilSpills

--- src/carma2/common/oil.h
+++ src/carma2/common/oil.h
@@ -5,11 +5,19 @@
 
 #include "c2_hooks.h"
 
+C2_HOOK_VARIABLE_DECLARE(int, gNext_oil_pixie);
+
 void C2_HOOK_FASTCALL InitOilSpills(void);
 
 void C2_HOOK_FASTCALL ResetOilSpills(void);
 
 void C2_HOOK_FASTCALL SetInitialOilStuff(tOil_spill_info* pOil, br_model* pModel);
+
+void C2_HOOK_FASTCALL MungeOilsHeightAeGround(tOil_spill_info* pThe_spill);
+
+int C2_HOOK_FASTCALL OKToSpillOil(tOil_spill_info* pOil);
+
+void C2_HOOK_FASTCALL MungeOilsHeightAboveGround(tOil_spill_info* pOil);
 
 void C2_HOOK_FASTCALL ProcessOilSpills(tU32 pFrame_period);
 
