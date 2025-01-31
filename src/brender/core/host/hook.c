a8f9b726e41ac5f537dd24d36554095763917885
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-31 20:20:55 +0100

    Prefix apply host/hook, HostExceptionHook, HostExceptionUnhook

--- src/brender/core/host/hook.c
+++ src/brender/core/host/hook.c
@@ -1,18 +1,8 @@
 #include "hook.h"
 
+#include "core/host/prot.h"
+
 #include "c2_stdlib.h"
-
-br_error C2_HOOK_CDECL HostExceptionGet(br_uint_8 exception, br_uint_32 *offp, br_uint_16 *selp) {
-    // Not supported on Windows
-    c2_abort();
-}
-C2_HOOK_FUNCTION(0x0053fdcd, HostExceptionGet)
-
-br_error C2_HOOK_CDECL HostExceptionSet(br_uint_8 exception, br_uint_32 off, br_uint_16 sel) {
-    // Not supported on Windows
-    c2_abort();
-}
-C2_HOOK_FUNCTION(0x0053fdec, HostExceptionSet)
 
 br_error C2_HOOK_CDECL HostExceptionHook(host_exception_hook *h, br_uint_8 exception, br_uint_32 off, br_uint_16 sel) {
     br_error r;
@@ -53,4 +43,4 @@ br_error C2_HOOK_CDECL HostExceptionUnhook(host_exception_hook *h) {
     h->active = 0;
     return 0x1002;
 }
-C2_HOOK_FUNCTION(0x00540360 ,HostExceptionUnhook)
+C2_HOOK_FUNCTION(0x00540360, HostExceptionUnhook)
