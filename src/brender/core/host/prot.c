46cbb0318021559efdc61011bcf42c7e937903f7
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-31 20:21:29 +0100

    Implement HostInterruptHook

--- src/brender/core/host/prot.c
+++ src/brender/core/host/prot.c
@@ -2,6 +2,16 @@
 
 #include "c2_stdlib.h"
 
+br_error C2_HOOK_CDECL HostInterruptGet(br_uint_8 vector, br_uint_32 *offp, br_uint_16 *selp) {
+    // Not supported on Windows
+    c2_abort();
+}
+
+br_error C2_HOOK_CDECL HostInterruptSet(br_uint_8 vector, br_uint_32 off, br_uint_16 sel) {
+    // Not supported on Windows
+    c2_abort();
+}
+
 br_error C2_HOOK_CDECL HostExceptionGet(br_uint_8 exception, br_uint_32 *offp, br_uint_16 *selp) {
     // Not supported on Windows
     c2_abort();
