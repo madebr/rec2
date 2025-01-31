58aa5c05a8837514d700860f09258c851b5d006b
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-31 18:47:12 +0100

    Hook HostExceptionSet

--- src/brender/core/host/hook.c
+++ src/brender/core/host/hook.c
@@ -7,3 +7,9 @@ br_error C2_HOOK_CDECL HostExceptionGet(br_uint_8 exception, br_uint_32 *offp, b
     c2_abort();
 }
 C2_HOOK_FUNCTION(0x0053fdcd, HostExceptionGet)
+
+br_error C2_HOOK_CDECL HostExceptionSet(br_uint_8 exception, br_uint_32 off, br_uint_16 sel) {
+    // Not supported on Windows
+    c2_abort();
+}
+C2_HOOK_FUNCTION(0x0053fdec, HostExceptionSet)
