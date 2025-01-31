5b0686be2af79720206c7a3e2b71048b1f4f3b96
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-31 18:25:15 +0100

    Implement HostSelectorAllocate

--- src/brender/core/host/memmgmt.c
+++ src/brender/core/host/memmgmt.c
@@ -9,3 +9,8 @@ br_error C2_HOOK_CDECL HostUnlock(br_uint_32 offset, br_uint_16 sel, br_size_t s
     return 0x1002;
 }
 C2_HOOK_FUNCTION(0x0053fcd0, HostUnlock)
+
+br_error C2_HOOK_CDECL HostSelectorAllocate(br_uint_16 *selp) {
+    return 0x1002;
+}
+C2_HOOK_FUNCTION(0x0053fce0, HostSelectorAllocate)
