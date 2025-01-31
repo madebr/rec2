d7e0fb44dacffabcb149aefe8fd2a0ff94c10769
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-31 18:34:23 +0100

    Implement HostSelectorBaseQuery

--- src/brender/core/host/memmgmt.h
+++ src/brender/core/host/memmgmt.h
@@ -17,4 +17,6 @@ br_error C2_HOOK_CDECL HostSelectorBaseSet(br_uint_16 sel, br_uint_32 base);
 
 br_error C2_HOOK_CDECL HostSelectorLimitSet(br_uint_16 sel, br_size_t limit);
 
+br_error C2_HOOK_CDECL HostSelectorBaseQuery(br_uint_32 *basep, br_uint_16 sel);
+
 #endif // REC2_MEMMGMT_H
