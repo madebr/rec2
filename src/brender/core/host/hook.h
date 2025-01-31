df817ee3462c578c272dbca8743c4dc0a12edb07
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-31 18:57:13 +0100

    Implement HostExceptionUnhook

--- src/brender/core/host/hook.h
+++ src/brender/core/host/hook.h
@@ -11,4 +11,6 @@ br_error C2_HOOK_CDECL HostExceptionSet(br_uint_8 exception, br_uint_32 off, br_
 
 br_error C2_HOOK_CDECL HostExceptionHook(host_exception_hook *h, br_uint_8 exception, br_uint_32 off, br_uint_16 sel);
 
+br_error C2_HOOK_CDECL HostExceptionUnhook(host_exception_hook *h);
+
 #endif // REC2_HOOK_H
