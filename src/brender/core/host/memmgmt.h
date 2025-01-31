0a37934d98883431412344d52353d93bff75d008
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-31 18:10:28 +0100

    Implement HostLock

---
+++ src/brender/core/host/memmgmt.h
@@ -0,0 +1,10 @@
+#ifndef REC2_MEMMGMT_H
+#define REC2_MEMMGMT_H
+
+#include "brender/br_types.h"
+
+#include "c2_hooks.h"
+
+br_error C2_HOOK_CDECL HostLock(br_uint_32 offset, br_uint_16 sel, br_size_t size);
+
+#endif // REC2_MEMMGMT_H
