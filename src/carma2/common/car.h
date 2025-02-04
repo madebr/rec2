230b877c6288c2ee0943662866e9f84278e69b1d
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-04 19:58:49 +0100

    Hook GetFrictionFromFace

--- src/carma2/common/car.h
+++ src/carma2/common/car.h
@@ -77,4 +77,6 @@ tCar_spec* C2_HOOK_FASTCALL DoPullActorFromWorld(br_actor* actor);
 
 tCar_spec* C2_HOOK_FASTCALL PullActorFromWorld(br_actor* actor);
 
+float C2_HOOK_FASTCALL GetFrictionFromFace(void *arg1);
+
 #endif //REC2_CAR_H
