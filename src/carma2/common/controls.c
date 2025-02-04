02e4587e99660590d589aae68f2a3dd4c56c5b2a
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-04 22:39:45 +0100

    Implement GotPowerup3

--- src/carma2/common/controls.c
+++ src/carma2/common/controls.c
@@ -2378,3 +2378,9 @@ void C2_HOOK_FASTCALL GotPowerup2(void) {
     GotPowerupN(2);
 }
 C2_HOOK_FUNCTION(0x004dbe70, GotPowerup2)
+
+void C2_HOOK_FASTCALL GotPowerup3(void) {
+
+    GotPowerupN(3);
+}
+C2_HOOK_FUNCTION(0x004dbed0, GotPowerup3)
