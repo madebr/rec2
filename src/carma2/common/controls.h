17f2d1a6244800a2d548da9baf2ae1607aa26176
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-04 22:18:02 +0100

    Hook PrevPedCam

--- src/carma2/common/controls.h
+++ src/carma2/common/controls.h
@@ -217,6 +217,8 @@ void C2_HOOK_FASTCALL ResetMan(void);
 
 void C2_HOOK_FASTCALL NextPedCam(void);
 
+void C2_HOOK_FASTCALL PrevPedCam(void);
+
 void C2_HOOK_FASTCALL FindNearestPed(void);
 
 #endif //REC2_CONTROLS_H
