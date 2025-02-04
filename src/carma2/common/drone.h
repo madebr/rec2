044e30693b32293d599a77204e77dd54ca08b0bb
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-04 22:52:27 +0100

    Hook DroneStateFuncControlledMovement

--- src/carma2/common/drone.h
+++ src/carma2/common/drone.h
@@ -65,4 +65,6 @@ void C2_HOOK_FASTCALL ProcessDrones(void);
 
 void C2_HOOK_FASTCALL DroneStateFuncReset(tDrone_spec* pDrone, tDroneStateFuncState state);
 
+void C2_HOOK_FASTCALL DroneStateFuncControlledMovement(tDrone_spec* pDrone, tDroneStateFuncState state);
+
 #endif //REC2_DRONE_H
