e8e63ec8d55874fffe1c1cc2bb1a56d80aed18ab
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-05 03:16:56 +0100

    Fix Hook a few Frontend structs

--- src/carma2/common/frontend_credits.c
+++ src/carma2/common/frontend_credits.c
@@ -2,6 +2,7 @@
 
 #include "errors.h"
 #include "frontend.h"
+#include "frontend_main.h"
 #include "globvars.h"
 #include "intrface.h"
 #include "loading.h"
@@ -14,7 +15,22 @@
 #include "c2_string.h"
 
 C2_HOOK_VARIABLE_IMPLEMENT_INIT(tFrontend_spec, gFrontend_CREDITS, 0x005cab48, {
-    FIXME TODO
+    "Credits",
+    0,
+    1,
+    CreditsScreenInfunc,
+    CreditsScreenOutfunc,
+    NULL,
+    &C2V(gFrontend_MAIN),
+    0,
+    0,
+    0,
+    11,
+    0,
+    0,
+    {
+        { 0x401, temp, &C2V(gFrontend_MAIN), 0, 17, 18, 0, 0, 0, 0, 1, 1 },
+    },
 });
 
 
