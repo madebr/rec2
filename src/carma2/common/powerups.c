ace37d56b45b57c6225a79f813ad6bc2bd30d170
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 04:22:53 +0100

    Hook TurnOffCloaking

--- src/carma2/common/powerups.c
+++ src/carma2/common/powerups.c
@@ -1944,3 +1944,13 @@ void C2_HOOK_FASTCALL ResetPissed(tPowerup* powerup, tCar_spec* car) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004df300, ResetPissed, ResetPissed_original)
+
+void (C2_HOOK_FASTCALL * TurnOffCloaking_original)(tPowerup* powerup, tCar_spec* car);
+void C2_HOOK_FASTCALL TurnOffCloaking(tPowerup* powerup, tCar_spec* car) {
+#if defined(C2_HOOKS_ENABLED)
+    TurnOffCloaking_original(powerup, car);
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004e0d50, TurnOffCloaking, TurnOffCloaking_original)
