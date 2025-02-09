bc62a24d31ab6bd9c116c627cac9aa7f17bb2eda
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 04:05:38 +0100

    Hook GainAPO

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -153,4 +153,6 @@ int C2_HOOK_FASTCALL SetPissedPeds(tPowerup* powerup, tCar_spec* car);
 
 int C2_HOOK_FASTCALL RepulsePedestrians(tPowerup* powerup, tCar_spec* car);
 
+int C2_HOOK_FASTCALL GainAPO(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
