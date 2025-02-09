9617597a2d4c9823914de43e75801c38d89ca0f7
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 03:44:37 +0100

    Hook SetPedExplode

--- src/carma2/common/powerups.c
+++ src/carma2/common/powerups.c
@@ -1094,3 +1094,13 @@ int C2_HOOK_FASTCALL SetPedSize(tPowerup* powerup, tCar_spec* car) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004dc750, SetPedSize, SetPedSize_original);
+
+int (C2_HOOK_FASTCALL * SetPedExplode_original)(tPowerup* powerup, tCar_spec* car);
+int C2_HOOK_FASTCALL SetPedExplode(tPowerup* powerup, tCar_spec* car) {
+#if defined(C2_HOOKS_ENABLED)
+    return SetPedExplode_original(powerup, car);
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004dc7d0, SetPedExplode, SetPedExplode_original);
