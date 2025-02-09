f43a452f4be7c783f7880ad2e38c56af82982745
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 04:02:15 +0100

    Hook SetPedBrittle

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -139,4 +139,6 @@ int C2_HOOK_FASTCALL AnnihilatePeds(tPowerup* powerup, tCar_spec* car);
 
 int C2_HOOK_FASTCALL RepulseOpponents(tPowerup* powerup, tCar_spec* car);
 
+int C2_HOOK_FASTCALL SetPedBrittle(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
