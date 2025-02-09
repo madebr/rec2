fabb365236d741d54402937226ea2582c80c8350
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 03:53:40 +0100

    Hook SetSuicidalPedestrians

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -113,4 +113,6 @@ int C2_HOOK_FASTCALL SetImmortalPeds(tPowerup* powerup, tCar_spec* car);
 
 int C2_HOOK_FASTCALL SetStupidPedestrians(tPowerup* powerup, tCar_spec* car);
 
+int C2_HOOK_FASTCALL SetSuicidalPedestrians(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
