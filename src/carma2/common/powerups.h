7731192336bfa181a7fe57c34c168ee72f887b11
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 04:16:48 +0100

    HOok HidePedestrians

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -217,4 +217,6 @@ void C2_HOOK_FASTCALL ResetMassMultiplier(tPowerup* powerup, tCar_spec* car);
 
 void C2_HOOK_FASTCALL ResetInstantHandbrake(tPowerup* powerup, tCar_spec* car);
 
+void C2_HOOK_FASTCALL HidePedestrians(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
