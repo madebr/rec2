9c95bce2cb9463beb5388f05fa85a34168ffaf46
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 03:56:19 +0100

    Hook SetPedHeadSize

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -127,4 +127,6 @@ int C2_HOOK_FASTCALL HitMine(tPowerup* powerup, tCar_spec* car);
 
 int C2_HOOK_FASTCALL SetElectroBastard(tPowerup* powerup, tCar_spec* car);
 
+int C2_HOOK_FASTCALL SetPedHeadSize(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
