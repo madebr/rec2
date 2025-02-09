25460ba40e786d8b4a68c12bd3284ec83bb0926a
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 23:58:10 +0100

    Implement InitQuickTimeStuff

--- src/carma2/common/video.c
+++ src/carma2/common/video.c
@@ -1,11 +1,24 @@
 #include "video.h"
 
+#include "globvars.h"
+
+#include <Movies.h>
+#include <QTML.h>
+
+C2_HOOK_VARIABLE_IMPLEMENT(int, gQuickTime_initialized, 0x006a0c24);
+
 void (C2_HOOK_CDECL * InitQuickTimeStuff_original)(void);
 void C2_HOOK_CDECL InitQuickTimeStuff(void) {
-#if defined(C2_HOOKS_ENABLED)
+#if 0//defined(C2_HOOKS_ENABLED)
     InitQuickTimeStuff_original();
 #else
-    NOT_IMPLEMENTED();
+    C2V(gQuickTime_initialized) = 0;
+    if (InitializeQTML(0) == noErr && EnterMovies() == noErr) {
+        C2V(gQuickTime_initialized) = 1;
+    }
+    C2V(gQuick_time_temp_path)[0] = '\0';
+    C2V(gQuick_time_movie_path_stub)[0] = '\0';
+    C2V(gQuick_time_banner_texture_name)[0] = '\0';
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004e1700, InitQuickTimeStuff, InitQuickTimeStuff_original)
