1a46659506cf030f86abd26919827b86146fa8a6
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 04:12:37 +0100

    Hook ResetGravity

--- src/carma2/common/powerups.c
+++ src/carma2/common/powerups.c
@@ -1714,3 +1714,13 @@ void C2_HOOK_FASTCALL ResetCopsSpeed(tPowerup* powerup, tCar_spec* car) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004de710, ResetCopsSpeed, ResetCopsSpeed_original)
+
+void (C2_HOOK_FASTCALL * ResetGravity_original)(tPowerup* powerup, tCar_spec* car);
+void C2_HOOK_FASTCALL ResetGravity(tPowerup* powerup, tCar_spec* car) {
+#if defined(C2_HOOKS_ENABLED)
+    ResetGravity_original(powerup, car);
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004de720, ResetGravity, ResetGravity_original)
