e5c55676dd9cd5386aa71b9704a5a34b6b1c336c
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 03:55:27 +0100

    Hook HitMine

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -123,4 +123,6 @@ int C2_HOOK_FASTCALL SetInstantHandbrake(tPowerup* powerup, tCar_spec* car);
 
 int C2_HOOK_FASTCALL ShowPedestrians(tPowerup* powerup, tCar_spec* car);
 
+int C2_HOOK_FASTCALL HitMine(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
