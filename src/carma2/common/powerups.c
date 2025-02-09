28862235f19eb1ab2b43a3d91353c4c195829666
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 04:22:06 +0100

    Hook ResetLowGravityPeds

--- src/carma2/common/powerups.c
+++ src/carma2/common/powerups.c
@@ -1914,3 +1914,13 @@ void C2_HOOK_FASTCALL ResetPanickingPeds(tPowerup* powerup, tCar_spec* car) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004de970, ResetPanickingPeds, ResetPanickingPeds_original)
+
+void (C2_HOOK_FASTCALL * ResetLowGravityPeds_original)(tPowerup* powerup, tCar_spec* car);
+void C2_HOOK_FASTCALL ResetLowGravityPeds(tPowerup* powerup, tCar_spec* car) {
+#if defined(C2_HOOKS_ENABLED)
+    ResetLowGravityPeds_original(powerup, car);
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004de980, ResetLowGravityPeds, ResetLowGravityPeds_original)
