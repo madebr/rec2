0bce98667646e340a7754c44f26ade5d6becf9af
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 03:44:37 +0100

    Hook SetPedExplode

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -69,4 +69,6 @@ int C2_HOOK_FASTCALL SetPedSpeed(tPowerup* powerup, tCar_spec* car);
 
 int C2_HOOK_FASTCALL SetPedSize(tPowerup* powerup, tCar_spec* car);
 
+int C2_HOOK_FASTCALL SetPedExplode(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
