70343b68eaa6ba3e2ffde0900e5ee005ba557743
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-09 04:21:49 +0100

    Hook ResetPanickingPeds

--- src/carma2/common/powerups.h
+++ src/carma2/common/powerups.h
@@ -231,4 +231,6 @@ void C2_HOOK_FASTCALL ResetGhostPeds(tPowerup* powerup, tCar_spec* car);
 
 void C2_HOOK_FASTCALL ResetDancingPeds(tPowerup* powerup, tCar_spec* car);
 
+void C2_HOOK_FASTCALL ResetPanickingPeds(tPowerup* powerup, tCar_spec* car);
+
 #endif //REC2_POWERUPS_H
