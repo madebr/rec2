77b2168b2faa2465f2ebfd9061984ab1a84aeb79
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-10 20:24:18 +0100

    Implement DRStdioAdvance

--- src/carma2/common/drfile.h
+++ src/carma2/common/drfile.h
@@ -24,6 +24,8 @@ br_size_t C2_HOOK_CDECL DRStdioWrite(void* buf, br_size_t size, unsigned int n, 
 
 br_size_t C2_HOOK_CDECL DRStdioGetLine(char* buf, br_size_t buf_len, void* f);
 
+void C2_HOOK_CDECL DRStdioAdvance(br_size_t count, void *f);
+
 void C2_HOOK_FASTCALL InstallDRFileCalls(void);
 
 #endif // REC2_DRFILE_H
