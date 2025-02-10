6398a66b193529cdacaab61d674aaa1b5e52ce22
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-10 03:22:23 +0100

    Implement S3MIDILoadSong

--- src/carma2/s3/include/s3/s3.h
+++ src/carma2/s3/include/s3/s3.h
@@ -61,6 +61,7 @@ typedef enum tS3_error_codes {
     eS3_error_load_sound = 8,
     eS3_error_channel_alloc = 10,
     eS3_error_function_failed = 12,
+    eS3_error_load_song = 13,
     eS3_error_nonexistant_source = 19,
 } tS3_error_codes;
 
