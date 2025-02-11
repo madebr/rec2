ea459f57b3ed97be0c9f58ce69d9a5379c918620
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-11 20:50:14 +0100

    Implement S3CopyS3Vector3

--- src/carma2/s3/src/3d.h
+++ src/carma2/s3/src/3d.h
@@ -20,6 +20,8 @@ int C2_HOOK_STDCALL S3Set3DSoundEnvironment(float a1, float a2, float a3);
 
 void C2_HOOK_FASTCALL S3CopyBrVector3(br_vector3* pDest, br_vector3* pSrc);
 
+void C2_HOOK_FASTCALL S3CopyS3Vector3(tS3_vector3* pDest, tS3_vector3* pSrc);
+
 void C2_HOOK_FASTCALL S3BindListenerPositionBRender(br_vector3* pos);
 
 void C2_HOOK_FASTCALL S3BindListenerVelocityBRender(br_vector3* vel);
