c5ba542aa7e0f63af9bb62353b37aba60a31e650
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-13 04:05:44 +0100

    Fix Implement PDGetJoy1Y

--- src/carma2/platform/platform.h
+++ src/carma2/platform/platform.h
@@ -116,6 +116,8 @@ tU32 C2_HOOK_FASTCALL PDGetJoy1Button4(void);
 
 int C2_HOOK_FASTCALL PDGetJoy1X(void);
 
+int C2_HOOK_FASTCALL PDGetJoy1Y(void);
+
 int C2_HOOK_FASTCALL PDS3Init(void);
 
 void C2_HOOK_FASTCALL PDExtractFilename(char* pDest, const char* pPath);
