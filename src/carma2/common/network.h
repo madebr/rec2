dd62b79a5044eb05e14c9d7eb8b9c988a123ae62
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-14 00:29:05 +0100

    Hook CheckForDisappearees

--- src/carma2/common/network.h
+++ src/carma2/common/network.h
@@ -47,6 +47,8 @@ void C2_HOOK_FASTCALL NetSendHeadupToAllPlayers(char* pMessage);
 
 void C2_HOOK_FASTCALL DoNextJoinPoll(void);
 
+void C2_HOOK_FASTCALL CheckForDisappearees(void);
+
 void C2_HOOK_FASTCALL NetService(int pIn_race);
 
 void C2_HOOK_FASTCALL DisableNetService(void);
