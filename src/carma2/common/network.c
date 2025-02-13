b7d823cd49bd0710a98b9bc0dc657e017dde778e
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-14 00:29:05 +0100

    Hook CheckForDisappearees

--- src/carma2/common/network.c
+++ src/carma2/common/network.c
@@ -168,6 +168,17 @@ void C2_HOOK_FASTCALL DoNextJoinPoll(void) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0049ec70, DoNextjoinPoll, DoNextjoinPoll_original)
+
+void (C2_HOOK_FASTCALL * CheckForDisappearees_original)(void);
+void C2_HOOK_FASTCALL CheckForDisappearees(void) {
+
+#if defined(C2_HOOKS_ENABLED)
+    CheckForDisappearees_original();
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004a4da0, CheckForDisappearees, CheckForDisappearees_original);
 void (C2_HOOK_FASTCALL * NetService_original)(int pIn_race);
 void C2_HOOK_FASTCALL NetService(int pIn_race) {
 #if defined(C2_HOOKS_ENABLED)
