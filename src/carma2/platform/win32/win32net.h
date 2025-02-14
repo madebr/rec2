8fcbafbf9e07de30db3925394fe7b2992769960d
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-14 03:29:02 +0100

    Implement PDNetLeaveGame

--- src/carma2/platform/win32/win32net.h
+++ src/carma2/platform/win32/win32net.h
@@ -21,4 +21,6 @@ int C2_HOOK_FASTCALL PDNetJoinGame(tNet_game_details* pGame_details, const char*
 
 void C2_HOOK_FASTCALL PDNetHostFinishGame(void);
 
+void C2_HOOK_FASTCALL PDNetLeaveGame(void);
+
 #endif // C2_WIN32_NET_H
