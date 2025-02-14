49b6dc676765481b4174e334c752d7220558516b
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-14 03:59:20 +0100

    Hook ReceivedMessage

--- src/carma2/common/network.h
+++ src/carma2/common/network.h
@@ -114,6 +114,8 @@ void C2_HOOK_FASTCALL NetGuaranteedSendMessageToHost(tNet_game_details* pDetails
 
 tNet_message* C2_HOOK_FASTCALL NetGetNextMessage(tNet_game_details* pDetails, void** pSender_address);
 
+void C2_HOOK_FASTCALL ReceivedMessage(tNet_message* pMessage, void* pSender_address, tU32 pReceive_time);
+
 void C2_HOOK_FASTCALL NetReceiveAndProcessMessages(void);
 
 int C2_HOOK_FASTCALL NetGuaranteedSendMessageToAllPlayers(tNet_game_details* pDetails, tNet_message* pMessage, undefined4 *pArg3);
