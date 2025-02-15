43d7dd1ba2ca955ed8c20b32e39639f6cfe1598e
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-15 22:53:18 +0100

    Implement c2_tolower

--- src/carma2/hooks/c2_ctype.c
+++ src/carma2/hooks/c2_ctype.c
@@ -2,6 +2,11 @@
 
 #include <ctype.h>
 
+int C2_HOOK_CDECL c2_tolower(int character) {
+    return tolower(character);
+}
+C2_HOOK_FUNCTION(0x00576860, c2_tolower);
+
 int C2_HOOK_CDECL c2_toupper(int character) {
     return toupper(character);
 }
