9435d5ec8f09518cb273eb3a79196c9015a79e46
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-02-27 22:43:52 +0100

    Add pre-built smackw32 import library

--- scripts/regenerate_implibs.sh
+++ scripts/regenerate_implibs.sh
@@ -4,8 +4,16 @@ root=`cd -P -- "\`dirname -- "$0"\`/.." && printf '%s\n' "\`pwd -P\`"`
 
 echo "root=${root}"
 
+cd "${root}/external/iforce2/lib/x86"
 i686-w64-mingw32-dlltool \
-  --input-def "${root}/external/iforce2/lib/x86/iforce2.def" \
-  --output-lib "${root}/external/iforce2/lib/x86/iforce2.lib"  \
+  --input-def "iforce2.def" \
+  --output-lib "iforce2.lib"  \
+  --add-stdcall-underscore \
+  --deterministic-libraries
+
+cd "${root}/external/smackw32/lib/x86"
+i686-w64-mingw32-dlltool \
+  --input-def "smackw32.def" \
+  --output-lib "smackw32.lib"  \
   --add-stdcall-underscore \
   --deterministic-libraries
