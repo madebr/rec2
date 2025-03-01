859d5e6156c045482f9d465a7e13a3a5b317d4c8
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-03-01 20:33:32 +0100

    Let BrDevBegin accept const strings

--- src/brender/core/fw/devsetup.h
+++ src/brender/core/fw/devsetup.h
@@ -9,13 +9,13 @@ br_pixelmap* C2_HOOK_CDECL BrDevLastBeginQuery(void);
 
 void C2_HOOK_CDECL BrDevLastBeginSet(br_pixelmap* pm);
 
-br_error C2_HOOK_CDECL BrDevBeginVar(br_pixelmap** ppm, char* setup_string, ...);
+br_error C2_HOOK_CDECL BrDevBeginVar(br_pixelmap** ppm, const char* setup_string, ...);
 
-br_error C2_HOOK_CDECL BrDevBeginTV(br_pixelmap** ppm, char* setup_string, br_token_value* setup_tv);
+br_error C2_HOOK_CDECL BrDevBeginTV(br_pixelmap** ppm, const char* setup_string, br_token_value* setup_tv);
 
-br_error C2_HOOK_CDECL BrDevBegin(br_pixelmap** ppm, char* setup_string);
+br_error C2_HOOK_CDECL BrDevBegin(br_pixelmap** ppm, const char* setup_string);
 
-br_pixelmap* C2_HOOK_CDECL BrDevBeginOld(char* setup_string);
+br_pixelmap* C2_HOOK_CDECL BrDevBeginOld(const char* setup_string);
 
 void C2_HOOK_CDECL BrDevEndOld(void);
 
