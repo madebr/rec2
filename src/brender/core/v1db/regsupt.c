f8470becae211add15eb0e6d7ffd9efd19582f01
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-03-02 03:34:04 +0100

    Hook BrMapFind

--- src/brender/core/v1db/regsupt.c
+++ src/brender/core/v1db/regsupt.c
@@ -9,3 +9,13 @@ br_material* C2_HOOK_CDECL BrMaterialFind(const char* pattern) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0051eea0, BrMaterialFind, BrMaterialFind_original)
+
+br_pixelmap* (C2_HOOK_CDECL * BrMapFind_original)(char* pattern);
+br_pixelmap* C2_HOOK_CDECL BrMapFind(char* pattern) {
+#if defined(C2_HOOKS_ENABLED)
+    return BrMapFind_original(pattern);
+#else
+    return BrRegistryFind(&C2V(v1db).reg_textures, pattern);
+#endif
+}
+C2_HOOK_FUNCTION(0x0051eff0, BrMapFind)
