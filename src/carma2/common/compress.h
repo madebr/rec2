705871f67f77b1a7b429b4d151d6a6ff79bdbc81
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-03-19 00:31:34 +0100

    Implement ExpandMatrix34

--- src/carma2/common/compress.h
+++ src/carma2/common/compress.h
@@ -15,4 +15,6 @@ void C2_HOOK_FASTCALL ExpandVector3(br_vector3* pDest, const tCompressed_vector3
 
 void C2_HOOK_FASTCALL CompressMatrix34(tCompressed_matrix3* pCompressed_matrix3, int* pInactive, const br_matrix34* pMatrix);
 
+void C2_HOOK_FASTCALL ExpandMatrix34(br_matrix34* pMat, const tCompressed_matrix3* pCompressed, int pInactive);
+
 #endif //REC2_COMPRESS_H
