bb301b20050c62509f9088e7a8f7514dcfc22f46
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-07-18 00:11:07 +0200

    Hook ProcessThisDrone

--- src/carma2/common/drone.c
+++ src/carma2/common/drone.c
@@ -466,6 +466,17 @@ void C2_HOOK_FASTCALL MakeAISimpleEditSectionHere(br_model* pModel, int pVert_in
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004020e0, MakeAISimpleEditSectionHere, MakeAISimpleEditSectionHere_original)
 
+void (C2_HOOK_FASTCALL * ProcessThisDrone_original)(int pIndex);
+void C2_HOOK_FASTCALL ProcessThisDrone(int pIndex) {
+
+#if defined(C2_HOOKS_ENABLED)
+    ProcessThisDrone_original(pIndex);
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x00451ca0, ProcessThisDrone, ProcessThisDrone_original)
+
 void (C2_HOOK_FASTCALL * ProcessDrones_original)(void);
 void C2_HOOK_FASTCALL ProcessDrones(void) {
 
