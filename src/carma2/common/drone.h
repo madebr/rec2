e1f51015c6280529f765876169cad4e94d65fbdf
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-07-17 23:38:10 +0200

    Implement DroneTrainWithinProcessingDistance

--- src/carma2/common/drone.h
+++ src/carma2/common/drone.h
@@ -97,6 +97,8 @@ int C2_HOOK_FASTCALL DroneCarWithinProcessingDistance(const br_vector3* pPos);
 
 int C2_HOOK_FASTCALL DronePlaneWithinProcessingDistance(const br_vector3* pPos);
 
+int C2_HOOK_FASTCALL DroneTrainWithinProcessingDistance(const br_vector3* pPos);
+
 void C2_HOOK_FASTCALL CalcRenderBoundsCentre(void);
 
 void C2_HOOK_FASTCALL PipeDroneMatrix(tDrone_spec* pDrone);
