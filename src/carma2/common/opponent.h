96f8e631e5c926024e709486e9ecf34c4167ca9d
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-07-20 03:05:19 +0200

    Hook ProcessGetNearPlayer

--- src/carma2/common/opponent.h
+++ src/carma2/common/opponent.h
@@ -150,6 +150,8 @@ int C2_HOOK_FASTCALL AddToOpponentsProjectedRoute(tOpponent_spec* pOpponent_spec
 
 void C2_HOOK_FASTCALL TopUpRandomRoute(tOpponent_spec* pOpponent_spec, int pSections_to_add);
 
+void C2_HOOK_FASTCALL ProcessGetNearPlayer(tOpponent_spec* pOpponent_spec, tProcess_objective_command pCommand);
+
 void C2_HOOK_FASTCALL ProcessCurrentObjective(tOpponent_spec* pOpponent_spec, tProcess_objective_command pCommand);
 
 void C2_HOOK_FASTCALL TeleportOpponentToNearestSafeLocation(tOpponent_spec* pOpponent_spec);
