6b81b375c6129b36fb2ac1d8e88049bb4e5f097d
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-07-22 16:18:59 +0200

    Implement DoNotDprintf_opponent

---
+++ src/carma2/common/oppoproc.h
@@ -0,0 +1,12 @@
+#ifndef REC2_OPPOPROC_H
+#define REC2_OPPOPROC_H
+
+#include <brender/brender.h>
+#include "rec2_types.h"
+
+#include "c2_hooks.h"
+
+
+void C2_HOOK_CDECL DoNotDprintf_oppoproc(const char* pMessage, ...);
+
+#endif //REC2_OPPONENT_H
