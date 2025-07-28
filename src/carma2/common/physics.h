e3cc6607a7ab637a89ec4218cbcd6e9b1796b8ef
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-07-28 18:02:26 +0200

    Implement TimeToSendData

--- src/carma2/common/physics.h
+++ src/carma2/common/physics.h
@@ -128,4 +128,6 @@ int C2_HOOK_FASTCALL PHILAddObject(tCollision_info* pObject);
 
 void C2_HOOK_FASTCALL PHILDoPhysics(tPhysics_callbacks* pCallbacks, tU32 pLast_tick_time, tU32 pFrame_period);
 
+int C2_HOOK_FASTCALL TimeToSendData(void);
+
 #endif //REC2_PHYSICS_H
