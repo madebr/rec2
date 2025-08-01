2fdbd7bd8bae18113cabccaf33810335c4eedb22
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-08-01 19:57:34 +0200

    Implement MakeModelMaterialsDoubleSided

--- src/carma2/common/flap.h
+++ src/carma2/common/flap.h
@@ -9,6 +9,8 @@ void C2_HOOK_FASTCALL DoDetaching(void);
 
 void C2_HOOK_FASTCALL SetBitForDetachment(br_actor* pActor, tCar_spec* pCar, float pArg3, int pArg4);
 
+void C2_HOOK_FASTCALL MakeModelMaterialsDoubleSided(br_model* pModel);
+
 void C2_HOOK_FASTCALL DoFlapping(void);
 
 void C2_HOOK_FASTCALL DoFullyDetaching(void);
