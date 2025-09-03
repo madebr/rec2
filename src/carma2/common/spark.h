8622bff6e242ea01304e0e0634c68d110eb77ce7
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-09-03 03:38:53 +0200

    Hook CreateSmokeColumn2

--- src/carma2/common/spark.h
+++ src/carma2/common/spark.h
@@ -88,4 +88,6 @@ void C2_HOOK_FASTCALL CreatePuffOfSmoke(br_vector3* pPos, br_vector3* pV, float 
 
 void C2_HOOK_FASTCALL ClearSplashes(tCollision_info* pObject);
 
+void C2_HOOK_FASTCALL CreateSmokeColumn2(undefined4 pArg1, br_actor* pActor, tCar_spec* pCar, tCollision_info* pObject, undefined4 pArg5, undefined4 pArg6, undefined4 pArg7);
+
 #endif //REC2_SPARK_H
