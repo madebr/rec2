50c3b5158d77e5c456b474a56c6072cfff4e2cdb
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-10-09 21:28:40 +0200

    Hook GeneralCreateSplash

--- src/carma2/common/spark.c
+++ src/carma2/common/spark.c
@@ -706,3 +706,14 @@ void C2_HOOK_FASTCALL MungeSmokeColumn(tU32 pTime) {
 #endif
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x004fba40, MungeSmokeColumn, MungeSmokeColumn_original)
+
+void (C2_HOOK_FASTCALL * GeneralCreateSplash_original)(tCollision_info* pObject, br_vector3* pVel,br_vector3* pOmega, br_vector3* pNormal, float pWater_density, br_bounds3* pBounds, br_matrix34* pMat, tU32 pTime);
+void C2_HOOK_FASTCALL GeneralCreateSplash(tCollision_info* pObject, br_vector3* pVel,br_vector3* pOmega, br_vector3* pNormal, float pWater_density, br_bounds3* pBounds, br_matrix34* pMat, tU32 pTime) {
+
+#if defined(C2_OOKS_ENABLED)
+    GeneralCreateSplash_original(pObject, pVel, pOmega, pNormal, pWater_density, pBounds, pMat, pTime);
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004fcbb0, GeneralCreateSplash, GeneralCreateSplash_original)
