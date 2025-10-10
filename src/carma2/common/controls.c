444778e8eb50311d0315e0fc8300ea4702610f86
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-10-10 04:48:24 +0200

    Hook MungeRepulseRays

--- src/carma2/common/controls.c
+++ src/carma2/common/controls.c
@@ -3055,6 +3055,17 @@ void C2_HOOK_FASTCALL ViewOpponent(void) {
 }
 C2_HOOK_FUNCTION_ORIGINAL(0x0040e590, ViewOpponent, ViewOpponent_original)
 
+void (C2_HOOK_FASTCALL * MungeRepulseRays_original)(void);
+void C2_HOOK_FASTCALL MungeRepulseRays(void) {
+
+#if defined(C2_HOOKS_ENABLED)
+    MungeRepulseRays_original();
+#else
+    NOT_IMPLEMENTED();
+#endif
+}
+C2_HOOK_FUNCTION_ORIGINAL(0x004db8e0, MungeRepulseRays, MungeRepulseRays_original)
+
 void C2_HOOK_FASTCALL GotPowerupN(int pN) {
     int modifiers;
 
--- src/carma2/common/controls.h
+++ src/carma2/common/controls.h
@@ -254,6 +254,8 @@ void C2_HOOK_FASTCALL LoseDosh(void);
 
 void C2_HOOK_FASTCALL ViewOpponent(void);
 
+void C2_HOOK_FASTCALL MungeRepulseRays(void);
+
 void C2_HOOK_FASTCALL GotPowerupN(int pN);
 
 void C2_HOOK_FASTCALL GotPowerup0(void);
