bf600703c72555747d7e77e47adbb44139515260
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-01-23 04:32:27 +0100

    Implement MovieRecordFrame

--- src/carma2/common/video.h
+++ src/carma2/common/video.h
@@ -3,6 +3,12 @@
 
 #include "c2_hooks.h"
 
+#include "rec2_types.h"
+
+C2_HOOK_VARIABLE_DECLARE(int, gRecording_paused);
+C2_HOOK_VARIABLE_DECLARE(tU32, gRecording_time);
+C2_HOOK_VARIABLE_DECLARE(int, gRecording_mouse_disabled);
+
 void C2_HOOK_CDECL InitQuickTimeStuff(void);
 
 void C2_HOOK_FASTCALL KillStatusMessage(void);
@@ -28,5 +34,11 @@ int C2_HOOK_FASTCALL InitMovie(void);
 int C2_HOOK_FASTCALL CreateMovie(void);
 
 int C2_HOOK_FASTCALL GenerateOneMovieFrame(void);
+
+void C2_HOOK_FASTCALL TrapS3SoundOutput(void);
+
+void C2_HOOK_FASTCALL FreeS3SoundOutput(void);
+
+int C2_HOOK_FASTCALL MovieRecordFrame(br_pixelmap *pScreen, int pFrame_period);
 
 #endif //REC2_VIDEO_H
