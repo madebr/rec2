36eb5061fdf1955da4cee62e831f901a9334827f
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-06-05 00:48:43 +0200

    Match DefaultNetSettings

--- src/carma2/newcommon/71-newgame.c
+++ src/carma2/newcommon/71-newgame.c
@@ -1,5 +1,13 @@
 #include "71-newgame.h"
 
+#include "08-loading1.h"
+#include "10-loading2.h"
+#include "41-utility.h"
+#include "70-packfile.h"
+#include "globvars.h"
+
+#include "rec2_macros.h"
+
 // GLOBAL: CARMA2_HW 0x00763960
 tNet_game_options gNet_settings[9];
 
@@ -15,9 +23,25 @@ tNet_game_options gNet_settings[9];
 
 // SetNetAvailability
 
-// STUB: CARMA2_HW 0x004663e0
+// FUNCTION: CARMA2_HW 0x004663e0
 void C2_HOOK_FASTCALL DefaultNetSettings(void) {
-    NOT_IMPLEMENTED();
+    FILE* file;
+    tNet_game_options* pOptions;
+    tPath_name the_path;
+    int i;
+
+    PathCat(the_path, gApplication_path, "NETDEFLT.TXT");
+    file = DRfopen(the_path, "rt");
+    if (file == NULL) {
+        return;
+    }
+    ReadNetworkSettings(file, &gNet_settings[0]);
+    PFrewind(file);
+    for (i = 1; i < (int)REC2_ASIZE(gNet_settings); i++) {
+        pOptions = &gNet_settings[i];
+        ReadNetworkSettings(file, pOptions);
+    }
+    PFfclose(file);
 }
 
 // DisposeJoinableGame
