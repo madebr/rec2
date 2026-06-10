0b60547e47ade9172d42f602276cb9f4872946f9
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-06-10 14:33:51 +0200

    Match DRLoadShadeTable

--- src/carma2/newcommon/63-loading3.h
+++ src/carma2/newcommon/63-loading3.h
@@ -24,7 +24,7 @@ extern br_pixelmap* C2_HOOK_FASTCALL LoadPixelmap(const char* pPath_name);
 
 extern void C2_HOOK_FASTCALL DRLoadPalette(const char* pPath_name);
 
-// DRLoadShadeTable
+extern void C2_HOOK_FASTCALL DRLoadShadeTable(const char* pPath_name);
 
 // DRLoadMaterials
 
