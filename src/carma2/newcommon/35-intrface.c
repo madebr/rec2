a427383d925dea5502b45a1d3b4cb6f8b7a8c194
Anonymous Maarten <anonymous.maarten@gmail.com>
2026-06-23 20:05:33 +0200

    Match ChoiceDisabled

--- src/carma2/newcommon/35-intrface.c
+++ src/carma2/newcommon/35-intrface.c
@@ -3,6 +3,12 @@
 // GLOBAL: CARMA2_HW 0x0068c1f0
 int gAlways_typing;
 
+// GLOBAL: CARMA2_HW 0x0068c1ec
+int gDisabled_count;
+
+// GLOBAL: CARMA2_HW 0x0068c1f8
+int gDisabled_choices[10];
+
 // FUNCTION: CARMA2_HW 0x00484620
 void C2_HOOK_FASTCALL SetAlwaysTyping(void) {
 
@@ -15,11 +21,54 @@ void C2_HOOK_FASTCALL ClearAlwaysTyping(void) {
     gAlways_typing = 0;
 }
 
-// ChoiceDisabled
+int C2_HOOK_FASTCALL ChoiceDisabled(int pChoice) {
+    int i;
+
+    for (i = 0; i < gDisabled_count; ++i) {
+        if (gDisabled_choices[i] == pChoice) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 // ResetInterfaceTimeout
 
-// ChangeSelection
+// FUNCTION: CARMA2_HW 0x00484dd0
+void C2_HOOK_FASTCALL ChangeSelection(const tInterface_spec* pSpec, int* pOld_selection, int* pNew_selection, int pMode, int pSkip_disabled) {
+
+    if (ChoiceDisabled(*pNew_selection)) {
+        if (pSkip_disabled) {
+            if (*pOld_selection < *pNew_selection) {
+                do {
+                    *pNew_selection += 1;
+                    if (*pNew_selection < pSpec->move_up_min[pMode]) {
+                        *pNew_selection = pSpec->move_up_max[pMode];
+                    }
+                    if (*pNew_selection > pSpec->move_up_max[pMode]) {
+                        *pNew_selection = pSpec->move_up_min[pMode];
+                    }
+                } while (*pNew_selection != *pOld_selection && ChoiceDisabled(*pNew_selection));
+            } else {
+                do {
+                    *pNew_selection -= 1;
+                    if (*pNew_selection < pSpec->move_up_min[pMode]) {
+                        *pNew_selection = pSpec->move_up_max[pMode];
+                    }
+                    if (*pNew_selection > pSpec->move_up_max[pMode]) {
+                        *pNew_selection = pSpec->move_up_min[pMode];
+                    }
+                } while (*pNew_selection != *pOld_selection && ChoiceDisabled(*pNew_selection));
+            }
+        } else {
+            *pNew_selection = *pOld_selection;
+        }
+    }
+
+    if (*pOld_selection != *pNew_selection) {
+        *pOld_selection = *pNew_selection;
+    }
+}
 
 // RecopyAreas
 
--- src/carma2/newcommon/35-intrface.h
+++ src/carma2/newcommon/35-intrface.h
@@ -2,16 +2,17 @@
 #define GUARD_35_INTRFACE_H
 
 #include "c2_hooks.h"
+#include "rec2_types.h"
 
 extern void C2_HOOK_FASTCALL SetAlwaysTyping(void);
 
 extern void C2_HOOK_FASTCALL ClearAlwaysTyping(void);
 
-// ChoiceDisabled
+extern int C2_HOOK_FASTCALL ChoiceDisabled(int pChoice);
 
 // ResetInterfaceTimeout
 
-// ChangeSelection
+extern void C2_HOOK_FASTCALL ChangeSelection(const tInterface_spec* pSpec, int* pOld_selection, int* pNew_selection, int pMode, int pSkip_disabled);
 
 // RecopyAreas
 
